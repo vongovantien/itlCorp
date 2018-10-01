1cffdfc6a8e46c3cc5633f4a31ca16585f2c48de
irene.tam <irene.tam@itl.com>
2018-10-01 10:29:38 +0700

    commit code

---
+++ WebAPI/MLCOPS/API.Mobile/Resources/LanguageSub.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Mobile.Resources
+{
+    public class LanguageSub
+    {
+        public static readonly string MSG_DATA_NOT_FOUND = "MSG_DATA_NOT_FOUND";
+        public static readonly string MSG_OBJECT_NOT_EXISTS = "MSG_OBJECT_NOT_EXISTS";
+        public static readonly string MSG_OBJECT_DUPLICATED = "MSG_OBJECT_DUPLICATED";
+
+        public static readonly string MSG_OBJECT_RELATION_NOT_VALID = "MSG_OBJECT_RELATION_NOT_VALID";
+
+        public static readonly string MSG_INSERT_SUCCESS = "MSG_INSERT_SUCCESS";
+
+        public static readonly string MSG_UPDATE_SUCCESS = "MSG_UPDATE_SUCCESS";
+
+        public static readonly string MSG_DELETE_SUCCESS = "MSG_DELETE_SUCCESS";
+    }
+}
