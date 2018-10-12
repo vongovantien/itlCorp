d8ae0cb7a8137fd4084b674c7273ef3d678a917e
The Nguyen <nguyendangthe10@gmail.com>
2018-10-12 15:20:17 +0700

    update

--- WebApp/src/app/business-modules/catalogue/stage-management/stage-management.component.ts
+++ WebApp/src/app/business-modules/catalogue/stage-management/stage-management.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import * as lodash from 'lodash';
 import { ActivatedRoute, Router } from '@angular/router';
+import * as SearchHelper from 'src/helper/SearchHelper';
 import { BaseService } from 'src/services-base/base.service';
 import { ToastrService } from 'ngx-toastr';
 import { Ng4LoadingSpinnerService } from 'ng4-loading-spinner';
@@ -16,6 +17,7 @@ export class StageManagementComponent implements OnInit {
 
   constructor(private baseServices:BaseService,private toastr: ToastrService, private spinnerService: Ng4LoadingSpinnerService) { }
   Stages_List: any;
+  Const_Stage_List
   New_Stage:any= {
     "stage_id":"",
     "abbreviation":"",
@@ -27,12 +29,14 @@ export class StageManagementComponent implements OnInit {
     "status:":"",
     "role":""
   };
+  selected_filter= "All";
   ngOnInit() {
     this.getStages();
   }
 
   async getStages() {
     this.Stages_List = await this.baseServices.getAsync('./assets/fake-data/stages-list.json', false,false);
+    this.Const_Stage_List = this.Stages_List.map(x=>Object.assign({},x));
   }
 
   id_stage_remove=null;
@@ -54,26 +58,75 @@ export class StageManagementComponent implements OnInit {
     }
   }
 
-  add_stage(){
-    this.New_Stage = {
-      "stage_id":"",
-      "abbreviation":"",
-      "name":"",
-      "description":"",
-      "name_en":"",
-      "description_en":"",
-      "exp_time":"",
-      "status:":"",
-      "role":""
-    }
+  add_stage(){   
     this.spinnerService.show();
     setTimeout(() => {     
       this.Stages_List.push(this.New_Stage);
       this.spinnerService.hide();
       this.toastr.success("Add Stage Successful !");
-    }, 1500);
-    
- 
+      this.New_Stage = {
+        "stage_id":"",
+        "abbreviation":"",
+        "name":"",
+        "description":"",
+        "name_en":"",
+        "description_en":"",
+        "exp_time":"",
+        "status:":"",
+        "role":""
+      }
+    }, 1500);    
   }
+
+  search_fields:any = ['stage_id','role','name','name_en','abbreviation'];
+  condition = "or";
+  search_key = "";
+  select_filter(filter,event){
+    this.search_fields = [];
+    this.selected_filter = filter;
+    var id_element = document.getElementById(event.target.id);
+    if($(id_element).hasClass("active")==false){      
+      $(id_element).siblings().removeClass('active');
+      id_element.classList.add("active");
+    }
+
+    if(filter == "All"){
+      this.search_fields = ['stage_id','role','name','name_en','abbreviation'];
+    }
+    if(filter == "Stage ID"){
+      this.search_fields = ['stage_id'];
+    }
+    if(filter == "Role"){
+      this.search_fields = ['role'];
+    }
+    if(filter == "Name (EN)"){
+      this.search_fields = ['name_en'];
+    }
+    if(filter == "Name (VI)"){
+      this.search_fields = ['name'];
+    }
+    if(filter == "Abbreviation"){
+      this.search_fields = ['abbreviation'];
+    }    
+  }
+
+   search_stage(){
+    this.search_fields = SearchHelper.PrepareListFieldSearch(null,this.search_fields,this.search_key,this.condition);
+    var source_list = this.Const_Stage_List.map(x=>Object.assign({},x));
+    this.spinnerService.show();
+    setTimeout(() => {
+      this.Stages_List =  SearchHelper.SearchEngine(this.search_fields,source_list,this.condition);
+      this.spinnerService.hide();
+    }, 3000);
+    
+  }
+
+  
+reset_search(){
+  this.search_key = "";
+  this.Stages_List = this.Const_Stage_List.map(x=>Object.assign({},x));  
+  this.search_fields = ['stage_id','role','name','name_en','abbreviation'];
+}
+
 
 }
