55ee0aa207938a43de02b53843955d6c55881d12
irene.tam <irene.tam@itl.com>
2018-10-19 11:00:14 +0700

    commit

--- WebApp/src/app/shared/common/pagination/pagination.component.ts
+++ WebApp/src/app/shared/common/pagination/pagination.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { PagerService } from '../../services/pager.service';
 import { PagerSetting } from '../../models/layout/pager-setting.model';
 import { PagingService } from './paging-service';
 
@@ -9,62 +8,22 @@ import { PagingService } from './paging-service';
   styleUrls: ['./pagination.component.scss']
 })
 export class PaginationComponent implements OnInit {
-  //@Input() totalItems: number = 1;
-  //@Input() numberPageDisplay: number = 3;
-  // @Input() config: PagerSetting = {};
    @Output() pageChange: EventEmitter<number> = new EventEmitter<number>();
-
-  // pager: any = {};
-
-  // constructor(private pagerService: PagerService) { }
-
-  // ngOnInit() {
-  //   console.log(this.config);
-  //   this.getPages(this.config.currentPage, this.config.pageSize, this.config.totalItems);
-  // }
-
-  // ngOnChanges(): void {
-  //   this.getPages(this.config.currentPage, this.config.pageSize, this.config.totalItems);
-  // }
+   @Input() config: PagerSetting = {};
+   @Output() pagerObject = new EventEmitter<any[]>();
+   count = 0;
   
-  // getTotalPages(itemsPerPage: number, totalItems: number): number {
-  //   return Math.ceil(Math.max(totalItems, 1) / Math.max(itemsPerPage, 1));
-  // }
-
-  // getPages(offset: number, itemsPerPage: number, totalItems: number) {
-  //   this.pager = this.pagerService.getPager(totalItems, offset, itemsPerPage, this.config.pageSize );
-  // }
-  // selectPage(page: number, event) {
-  //   if (this.isValidPageNumber(page, this.pager.totalPages)) {
-  //     this.pager.currentPage = page;
-      
-  //   console.log(this.pager);
-  //     this.pageChange.emit(this.pager);
-  //   }
-  // }
-  // isValidPageNumber(page: number, totalPages: number): boolean {
-  //   return page > 0 && page <= totalPages;
-  // }
-  selectPageSize(){
+   selectPageSize(){
     this.pager.pageSize = this.config.pageSize;
     this.setPage(this.pager.currentPage);
     this.pageChange.emit(this.pager);
     
   }
 
-
-
-  @Input() config: PagerSetting = {};
-  @Output() pagerObject = new EventEmitter<any[]>();
-  count = 0;
   constructor(private pagerService: PagingService) { }
-
 
   // pager object
   pager: any = {};
-
-  // paged items
-  pagedItems: any[];
   ngOnInit() {   
     console.log(this.config);
     this.setPage(this.config.currentPage);
@@ -72,7 +31,6 @@ export class PaginationComponent implements OnInit {
 
   sendBackData() {
     this.count += 1;
-    // this.returnData.emit(this.data.slice(0,20));
     console.log(this.count)
   }
 
