dc80db4bc12afdd30c40463ddbc5e98f2cdb949f
ITL\lee.le <chienbinh.0904@gmail.com>
2018-10-31 14:37:31 +0700

    [layout] partner-data-detail

--- WebApp/src/app/business-modules/catalogue/partner-data-detail/partner-data-detail.component.ts
+++ WebApp/src/app/business-modules/catalogue/partner-data-detail/partner-data-detail.component.ts
@@ -11,5 +11,39 @@ export class PartnerDataDetailComponent implements OnInit {
 
   ngOnInit() {
   }
+  /**
+   * ng2-select
+   */
+  public items: Array<string> = ['Amsterdam', 'Antwerp', 'Athens', 'Barcelona',
+  'Berlin', 'Birmingham', 'Bradford', 'Bremen', 'Brussels', 'Bucharest'];
+
+  private value: any = {};
+  private _disabledV: string = '0';
+  private disabled: boolean = false;
+
+  private get disabledV(): string {
+    return this._disabledV;
+  }
+
+  private set disabledV(value: string) {
+    this._disabledV = value;
+    this.disabled = this._disabledV === '1';
+  }
+
+  public selected(value: any): void {
+    console.log('Selected value is: ', value);
+  }
+
+  public removed(value: any): void {
+    console.log('Removed value is: ', value);
+  }
+
+  public typed(value: any): void {
+    console.log('New search input: ', value);
+  }
+
+  public refreshValue(value: any): void {
+    this.value = value;
+  }
 
 }
