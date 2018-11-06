bbd538a092357b6506e83c35ccfc4f7b809b8df5
The Nguyen <nguyendangthe10@gmail.com>
2018-11-06 09:52:50 +0700

    update

--- WebAPI/eFMS.API.SystemWeb/CatalogueManagement/eFMS.API.Catalogue/Controllers/CatUnitController.cs
+++ WebAPI/eFMS.API.SystemWeb/CatalogueManagement/eFMS.API.Catalogue/Controllers/CatUnitController.cs
@@ -104,7 +104,7 @@ namespace eFMS.API.Catalogue.Controllers
             catUnit.DatetimeModified = DateTime.Now;
             CultureInfo currentCulture = Thread.CurrentThread.CurrentCulture;
             var hs = catUnitService.Update(catUnit,x=>x.Id==model.Id);
-            var message = HandleError.GetMessage(hs, Crud.Insert);
+            var message = HandleError.GetMessage(hs, Crud.Update);
             ResultHandle result = new ResultHandle { Status = hs.Success, Message = stringLocalizer[message].Value };
             if (!hs.Success)
             {
@@ -128,7 +128,6 @@ namespace eFMS.API.Catalogue.Controllers
             }
             return Ok(result);
         }
-
 
 
         private string CheckExist(int id, CatUnitModel model)
