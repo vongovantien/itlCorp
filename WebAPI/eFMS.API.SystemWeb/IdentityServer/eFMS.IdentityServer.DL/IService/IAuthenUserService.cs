67ce7bf3d6184efa80ea1b58b0a748fb88043364
The Nguyen <nguyendangthe10@gmail.com>
2018-11-19 09:24:18 +0700

    update

---
+++ WebAPI/eFMS.API.SystemWeb/IdentityServer/eFMS.IdentityServer.DL/IService/IAuthenUserService.cs
@@ -0,0 +1,14 @@
+﻿using eFMS.API.System.DL.Models;
+using eFMS.IdentityServer.Service.Models;
+using ITL.NetCore.Connection.BL;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eFMS.IdentityServer.DL.IService
+{
+    public interface IAuthenUserService : IRepositoryBase<SysUser, SysUserModel>
+    {
+        int Login(string username, string password,out LoginReturnModel modelReturn);
+    }
+}
