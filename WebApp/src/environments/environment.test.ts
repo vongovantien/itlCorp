cdfbc698f805413b8dbbc41ab531a1a77bbffe30
The Nguyen <nguyendangthe10@gmail.com>
2018-11-20 17:18:47 +0700

    update

---
+++ WebApp/src/environments/environment.test.ts
@@ -0,0 +1,12 @@
+
+
+export const environment = {
+    production: false,
+    staging:false,
+    test:true,
+    local:false,
+    HOST:{
+        WEB_URL:"test.api-efms.itlvn.com",
+        INDENTITY_SERVER_URL:"http://test.api-efms.itlvn.com/IdentityServer"
+    }
+  };   
