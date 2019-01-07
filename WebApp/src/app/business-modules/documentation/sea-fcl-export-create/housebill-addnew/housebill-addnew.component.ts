ea103f3126bb94308d043be20dacf3b48c163ded
ITL\lee.le <chienbinh.0904@gmail.com>
2019-01-07 18:22:39 +0700

    [layout] add house bill

---
+++ WebApp/src/app/business-modules/documentation/sea-fcl-export-create/housebill-addnew/housebill-addnew.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-housebill-addnew',
+  templateUrl: './housebill-addnew.component.html',
+  styleUrls: ['./housebill-addnew.component.scss']
+})
+export class HousebillAddnewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
