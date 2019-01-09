d0cf7afd2757ac4adc785d6e1e9d4e8eafd2b3a5
ITL\lee.le <chienbinh.0904@gmail.com>
2019-01-09 11:35:09 +0700

    fix table scroll

--- WebApp/src/app/business-modules/documentation/sea-fcl-export-create/housebill-addnew/housebill-addnew.component.ts
+++ WebApp/src/app/business-modules/documentation/sea-fcl-export-create/housebill-addnew/housebill-addnew.component.ts
@@ -12,4 +12,38 @@ export class HousebillAddnewComponent implements OnInit {
   ngOnInit() {
   }
 
+    /**
+    * ng2-select
+    */
+   public items: Array<string> = ['Option 1', 'Option 2', 'Option 3', 'Option 4',
+   'Option 5', 'Option 6', 'Option 7', 'Option 8', 'Option 9', 'Option 10',];
+
+    private value: any = {};
+    private _disabledV: string = '0';
+    public disabled: boolean = false;
+
+    private get disabledV(): string {
+    return this._disabledV;
+    }
+
+    private set disabledV(value: string) {
+    this._disabledV = value;
+    this.disabled = this._disabledV === '1';
+    }
+
+    public selected(value: any): void {
+    console.log('Selected value is: ', value);
+    }
+
+    public removed(value: any): void {
+    console.log('Removed value is: ', value);
+    }
+
+    public typed(value: any): void {
+    console.log('New search input: ', value);
+    }
+
+    public refreshValue(value: any): void {
+    this.value = value;
+    }
 }
