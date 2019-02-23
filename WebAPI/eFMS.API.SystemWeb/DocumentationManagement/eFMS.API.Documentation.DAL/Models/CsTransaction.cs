bdcdff20186d14227fe540fa99a83de3a2ffe642
The Nguyen <nguyendangthe10@gmail.com>
2019-02-23 11:59:52 +0700

    ssss

--- WebAPI/eFMS.API.SystemWeb/DocumentationManagement/eFMS.API.Documentation.DAL/Models/CsTransaction.cs
+++ WebAPI/eFMS.API.SystemWeb/DocumentationManagement/eFMS.API.Documentation.DAL/Models/CsTransaction.cs
@@ -5,10 +5,10 @@ namespace eFMS.API.Documentation.Service.Models
 {
     public partial class CsTransaction
     {
-        public CsTransaction()
-        {
-            CsTransactionDetail = new HashSet<CsTransactionDetail>();
-        }
+        //public CsTransaction()
+        //{
+        //    CsTransactionDetail = new HashSet<CsTransactionDetail>();
+        //}
 
         public Guid Id { get; set; }
         public Guid BranchId { get; set; }
@@ -58,6 +58,6 @@ namespace eFMS.API.Documentation.Service.Models
         public bool? Inactive { get; set; }
         public DateTime? InactiveOn { get; set; }
 
-        public virtual ICollection<CsTransactionDetail> CsTransactionDetail { get; set; }
+        //public virtual ICollection<CsTransactionDetail> CsTransactionDetail { get; set; }
     }
 }
