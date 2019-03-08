79b49e607719ebf0ceab82c0995a9e5c3d37d9bf
ITL\lee.le <chienbinh.0904@gmail.com>
2019-03-08 21:06:13 +0700

    [layout] CD note

--- WebApp/src/app/business-modules/documentation/sea-fcl-export-create/credit-and-debit-note/credit-and-debit-note.component.ts
+++ WebApp/src/app/business-modules/documentation/sea-fcl-export-create/credit-and-debit-note/credit-and-debit-note.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+declare var $: any;
 
 @Component({
-  selector: 'app-credit-and-debit-note',
-  templateUrl: './credit-and-debit-note.component.html',
-  styleUrls: ['./credit-and-debit-note.component.scss']
+    selector: 'app-credit-and-debit-note',
+    templateUrl: './credit-and-debit-note.component.html',
+    styleUrls: ['./credit-and-debit-note.component.scss']
 })
 export class CreditAndDebitNoteComponent implements OnInit {
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
+
+    createCDNote() {
+        $('#add-credit-debit-note-modal').modal('hide');
+        $('#detail-credit-debit-note-modal').modal('show');
+    }
+    editCDNote() {
+        $('#detail-credit-debit-note-modal').modal('hide');
+        $('#edit-credit-debit-note-modal').modal('show');
+    }
+    viewDetailCDNote() {
+        $('#edit-credit-debit-note-modal').modal('hide');
+        $('#detail-credit-debit-note-modal').modal('show');
+    }
 
 }
