ca9d3aa51ec3f48f96ece0459409604eaece9466
The Nguyen <nguyendangthe10@gmail.com>
2019-03-23 09:41:54 +0700

    update

---
+++ WebAPI/eFMS.API.SystemWeb/DocumentationManagement/eFMS.API.Documentation.DL/Models/AcctSOAModel.cs
@@ -0,0 +1,12 @@
+﻿using eFMS.API.Documentation.Service.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eFMS.API.Documentation.DL.Models
+{
+    public class AcctSOAModel : AcctSoa
+    {
+        public List<CsShipmentSurcharge> listCharges { get; set; }
+    }
+}
