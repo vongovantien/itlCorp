105951dbdb5186f49434779ee1d9146166777937
The Nguyen <nguyendangthe10@gmail.com>
2019-03-29 09:35:56 +0700

    update

--- WebApp/src/app/business-modules/documentation/sea-fcl-export-create/credit-and-debit-note/credit-and-debit-note.component.ts
+++ WebApp/src/app/business-modules/documentation/sea-fcl-export-create/credit-and-debit-note/credit-and-debit-note.component.ts
@@ -4,6 +4,7 @@ import { API_MENU } from 'src/constants/api-menu.const';
 import { ExtendData } from '../../extend-data';
 import cloneDeep from 'lodash/cloneDeep'
 import filter from 'lodash/filter';
+import moment from 'moment/moment';
 
 declare var $: any;
 
@@ -13,52 +14,74 @@ declare var $: any;
     styleUrls: ['./credit-and-debit-note.component.scss']
 })
 export class CreditAndDebitNoteComponent implements OnInit {
-    listCDNotes:any [] = [];
-    constListCDNotes:any [] = [];
+    listCDNotes: any[] = [];
+    constListCDNotes: any[] = [];
 
     constructor(
         private baseServices: BaseService,
         private api_menu: API_MENU
-      ) { }
+    ) { }
 
     ngOnInit() {
         this.getAllCDNote();
     }
 
 
-    
-  getAllCDNote(){
-    this.baseServices.get(this.api_menu.Documentation.AcctSOA.getAll+"?JobId="+ExtendData.currentJobID).subscribe((data:any)=>{
-      this.listCDNotes = cloneDeep(data);
-      this.constListCDNotes = cloneDeep(data);
-    });
-  }
 
-  SearchCDNotes(search_key: string){
-      search_key = search_key.trim();
-      var referenceList:any[] = this.constListCDNotes;
-      this.listCDNotes = filter(referenceList,function(x:any,index:number){
-        var root = false;
-        var branch = false;
-        if(x.partnerNameEn==null?"":x.partnerNameEn.toLowerCase().includes(search_key)){
-            root = true;
-        }
-        
-        const list = filter(x.listSOA,function(o){
-            if(o.soa.type.toLowerCase().includes(search_key)){
-                branch = true;
-            }
-            return branch;
+    getAllCDNote() {
+        this.baseServices.get(this.api_menu.Documentation.AcctSOA.getAll + "?JobId=" + ExtendData.currentJobID).subscribe((data: any) => {
+            this.listCDNotes = cloneDeep(data);
+            this.constListCDNotes = cloneDeep(data);
         });
+    }
+
+    SearchCDNotes(search_key: string) {
+        this.listCDNotes = cloneDeep(this.constListCDNotes)
+        search_key = search_key.trim().toLowerCase();
+        var listBranch: any[] = [];
+        this.listCDNotes = filter(cloneDeep(this.constListCDNotes), function (x: any) {
+            var root = false;
+            var branch = false;
+            if (x.partnerNameEn == null ? "" : x.partnerNameEn.toLowerCase().includes(search_key)) {
+                root = true;
+            }
+            var listSOA: any[] = []
+            for (var i = 0; i < x.listSOA.length; i++) {
+                const date = moment(x.listSOA[i].soa.datetimeCreated).format('DD/MM/YYYY');
+                if (x.listSOA[i].soa.type.toLowerCase().includes(search_key) ||
+                    x.listSOA[i].total_charge.toString().toLowerCase() === search_key ||
+                    x.listSOA[i].soa.total.toString().toLowerCase().includes(search_key) ||
+                    x.listSOA[i].soa.userCreated.toLowerCase().includes(search_key) ||
+                    x.listSOA[i].soa.code.toLowerCase().includes(search_key)||
+                    x.listSOA[i].soa.code.toLowerCase().includes(search_key)||
+                    date.includes(search_key)) {
+                    listSOA.push(x.listSOA[i]);
+                    branch = true;
+                }
+            }
+            if (listSOA.length > 0) {
+                listBranch.push({
+                    partnerID: x.id,
+                    list: listSOA
+                });
+            }
+
+            return (root || branch);
+
+        });
+        for (var i = 0; i < this.listCDNotes.length; i++) {
+            for (var k = 0; k < listBranch.length; k++) {
+                if (this.listCDNotes[i].id === listBranch[k].partnerID) {
+                    this.listCDNotes[i].listSOA = listBranch[k].list;
+                }
+            }
+        }
+    }
 
 
-       return (root || branch);
 
-      });
-  }
+    deleteCharge() {
 
-    deleteCharge(){
-        
     }
 
     /**
@@ -95,18 +118,18 @@ export class CreditAndDebitNoteComponent implements OnInit {
         this.value = value;
     }
 
-    open(id:string){
+    open(id: string) {
         $(id).modal('show');
     }
 
 
-    ListRemainingCharges:any[]=[];
-    addNewRemainingChargesCatcher(event:any){
+    ListRemainingCharges: any[] = [];
+    addNewRemainingChargesCatcher(event: any) {
         this.ListRemainingCharges = event;
     }
 
-    currentPartnerId:string = null;
-    currentPartnerIdCatcher(event:any){
+    currentPartnerId: string = null;
+    currentPartnerIdCatcher(event: any) {
         this.currentPartnerId = event;
     }
 
