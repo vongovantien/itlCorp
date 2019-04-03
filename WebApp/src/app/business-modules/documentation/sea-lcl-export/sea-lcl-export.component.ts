c975075e8c64133b6211a52fcfebeb45d593da43
lee.le <lee.le@itlvn.com>
2019-04-03 17:35:49 +0700

    [layout] sea-lcl-export job list

--- WebApp/src/app/business-modules/documentation/sea-lcl-export/sea-lcl-export.component.ts
+++ WebApp/src/app/business-modules/documentation/sea-lcl-export/sea-lcl-export.component.ts
@@ -1,15 +1,74 @@
 import { Component, OnInit } from '@angular/core';
+import * as moment from 'moment';
 
 @Component({
-  selector: 'app-sea-lcl-export',
-  templateUrl: './sea-lcl-export.component.html',
-  styleUrls: ['./sea-lcl-export.component.sass']
+    selector: 'app-sea-lcl-export',
+    templateUrl: './sea-lcl-export.component.html',
+    styleUrls: ['./sea-lcl-export.component.scss']
 })
 export class SeaLCLExportComponent implements OnInit {
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
+
+    /**
+       * Daterange picker
+       */
+    selectedRange: any;
+    selectedDate: any;
+    keepCalendarOpeningWithRange: true;
+    maxDate: moment.Moment = moment();
+    ranges: any = {
+        Today: [moment(), moment()],
+        Yesterday: [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+        'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+        'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+        'This Month': [moment().startOf('month'), moment().endOf('month')],
+        'Last Month': [
+            moment()
+                .subtract(1, 'month')
+                .startOf('month'),
+            moment()
+                .subtract(1, 'month')
+                .endOf('month')
+        ]
+    };
+
+    /**
+     * ng2-select
+     */
+    searchFilters: Array<string> = ['Job ID', 'MBL No', 'Supplier', 'Agent', 'HBL No'];
+    searchFilterActive = ['Job ID'];
+    private value: any = ['Athens'];
+    private _disabledV: string = '0';
+    public disabled: boolean = false;
+
+    public get disabledV(): string {
+        return this._disabledV;
+    }
+
+    public set disabledV(value: string) {
+        this._disabledV = value;
+        this.disabled = this._disabledV === '1';
+    }
+
+    public selected(value: any): void {
+        console.log('Selected value is: ', value);
+        //this.selectFilter = value.id;
+    }
+
+    public removed(value: any): void {
+        console.log('Removed value is: ', value);
+    }
+
+    public refreshValue(value: any): void {
+        this.value = value;
+    }
+
+    public typed(e){
+
+    }
 
 }
