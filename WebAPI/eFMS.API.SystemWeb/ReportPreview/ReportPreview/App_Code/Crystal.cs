8ce36d5c6fe2c588a80fd04f79efcf902490daf1
irene.tam <irene.tam@itl.com>
2019-04-10 10:12:34 +0700

    add new project

---
+++ WebAPI/eFMS.API.SystemWeb/ReportPreview/ReportPreview/App_Code/Crystal.cs
@@ -0,0 +1,68 @@
+﻿using CrystalDecisions.Shared;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Crystal
+/// </summary>
+public class Crystal
+{
+    public Crystal()
+    {
+        DataSource = new DataTable();
+        SubReports = new List<SubReport>();
+        parameter = new Dictionary<string, object>();
+    }
+    public Crystal(DataTable dataSource, List<SubReport> subReports, Dictionary<string, object> parameter)
+    {
+        this.DataSource = dataSource;
+        this.SubReports = subReports;
+        this.parameter = parameter;
+    }
+    public void AddDataSource<T>(List<T> lst)
+    {
+        DataSource = lst.ToDataTable();
+    }
+    public void AddSubReport<T>(string name, IList<T> lst)
+    {
+        SubReports.Add(new SubReport(name, lst.ToDataTable()));
+    }
+    public void SetParameter<T>(T obj)
+    {
+        PropertyDescriptorCollection props =
+            TypeDescriptor.GetProperties(typeof(T));
+        for (int i = 0; i < props.Count; i++)
+        {
+            PropertyDescriptor prop = props[i];
+            parameter.Add(prop.Name, obj.GetValueBy(prop.Name));
+        }
+    }
+    public string ReportFile { get; set; }
+    public string ReportName { get; set; }
+    public ExportFormatType FormatType { get; set; }
+    public DataTable DataSource { get; set; }
+    public List<SubReport> SubReports { get; set; }
+    public Dictionary<string, object> parameter { get; set; }
+    public bool AllowPrint { get; set; }
+    public bool AllowExport { get; set; }
+}
+public class SubReport
+{
+    public SubReport()
+    {
+        DataSource = new DataTable();
+    }
+
+    public SubReport(string name, DataTable dataSource)
+    {
+        this.Name = name;
+        this.DataSource = dataSource;
+    }
+
+    public string Name { get; set; }
+    public DataTable DataSource { get; set; }
+}
---
+++ WebAPI/eFMS.API.SystemWeb/ReportPreview/ReportPreview/Default.aspx.cs
@@ -0,0 +1,44 @@
+﻿using CrystalDecisions.CrystalReports.Engine;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!Page.IsPostBack)
+        {
+            Crystal crystal = new Crystal();
+            try
+            {
+                crystal = JsonConvert.DeserializeObject<Crystal>(Request.Form.GetValues(0)[0]);
+            }
+            catch (Exception ex)
+            {
+                crystal = null;
+                Response.Redirect("~/NotFound.aspx");
+            }
+            var reportPath = Server.MapPath("~/Reports/rptManifest.rpt");
+            if (File.Exists(reportPath))
+            {
+                ReportDocument rpt = new ReportDocument();
+                rpt.Load(reportPath);
+                rpt.SetDataSource(crystal.DataSource);
+                foreach (SubReport sub in crystal.SubReports)
+                {
+                    if (sub.DataSource.Rows.Count > 0)
+                        rpt.Subreports[sub.Name].SetDataSource(sub.DataSource);
+                }
+                rptViewer.HasPrintButton = false;
+                rptViewer.HasExportButton = false;
+                rptViewer.ReportSource = rpt;
+            }
+        }
+    }
+}
