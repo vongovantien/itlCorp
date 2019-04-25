2efb72886bdd5050306e5eedaf0d8143ff43994c
lee.le <lee.le@itlvn.com>
2019-04-25 16:57:06 +0700

    [layout] charge popups

--- WebApp/src/app/business-modules/documentation/sea-lcl-export-create/sea-lcl-export-housebill-list/sea-lcl-export-housebill-list.component.ts
+++ WebApp/src/app/business-modules/documentation/sea-lcl-export-create/sea-lcl-export-housebill-list/sea-lcl-export-housebill-list.component.ts
@@ -16,4 +16,37 @@ export class SeaLclExportHousebillListComponent implements OnInit {
     ngOnInit() {
     }
 
+    /**
+  * ng2-select
+  */
+    public items: Array<string> = ['option 1', 'option 2', 'option 3', 'option 4', 'option 5', 'option 6', 'option 7'];
+
+    private value: any = {};
+    private _disabledV: string = '0';
+    public disabled: boolean = false;
+
+    private get disabledV(): string {
+        return this._disabledV;
+    }
+
+    private set disabledV(value: string) {
+        this._disabledV = value;
+        this.disabled = this._disabledV === '1';
+    }
+
+    public selected(value: any): void {
+        console.log('Selected value is: ', value);
+    }
+
+    public removed(value: any): void {
+        console.log('Removed value is: ', value);
+    }
+
+    public typed(value: any): void {
+        console.log('New search input: ', value);
+    }
+
+    public refreshValue(value: any): void {
+        this.value = value;
+    }
 }
