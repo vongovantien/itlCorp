d7b7d875b3b4a5bd3f7040f931837992b2da19f8
lee.le <lee.le@itlvn.com>
2019-06-19 15:41:45 +0700

    SOA list

--- WebApp/src/app/business-modules/accounting/accounting.module.ts
+++ WebApp/src/app/business-modules/accounting/accounting.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { AccountingRoutingModule } from './accounting-routing.module';
 import { StatementOfAccountComponent } from './statement-of-account/statement-of-account.component';
@@ -7,13 +8,23 @@ import { AdvancePaymentComponent } from './advance-payment/advance-payment.compo
 import { SettlementPaymentComponent } from './settlement-payment/settlement-payment.component';
 import { AccountReceivablePayableComponent } from './account-receivable-payable/account-receivable-payable.component';
 import { SharedModule } from '../../shared/shared.module';
+import { SelectModule } from 'ng2-select';
+import { NgxDaterangepickerMd } from 'ngx-daterangepicker-material';
 
 @NgModule({
   imports: [
     CommonModule,
     AccountingRoutingModule,
-    SharedModule
+    SharedModule,
+    FormsModule,
+    SelectModule,
+    NgxDaterangepickerMd
   ],
-  declarations: [StatementOfAccountComponent, AdvancePaymentComponent, SettlementPaymentComponent, AccountReceivablePayableComponent]
+  declarations: [
+      StatementOfAccountComponent, 
+      AdvancePaymentComponent, 
+      SettlementPaymentComponent, 
+      AccountReceivablePayableComponent
+    ]
 })
 export class AccountingModule { }
