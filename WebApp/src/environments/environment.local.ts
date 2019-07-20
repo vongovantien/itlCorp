cd9c8680e50ad95bb11008d5a7681de6de595092
irene.tam <irene.tam@itl.com>
2019-07-20 11:42:49 +0700

    commit

--- WebApp/src/environments/environment.local.ts
+++ WebApp/src/environments/environment.local.ts
@@ -3,6 +3,7 @@ export const environment = {
     production: false,
     HOST: {
         WEB_URL: "localhost:",
+        CD_NOTE: "test.api-efms.itlvn.com:44366",
         INDENTITY_SERVER_URL: "https://localhost:44369"
-    }
+    },
 };
--- WebApp/src/environments/environment.ts
+++ WebApp/src/environments/environment.ts
@@ -2,7 +2,8 @@
 export const environment = {
     production: false,
     HOST: {
-        WEB_URL: "test.api-efms.itlvn.com:",
+        WEB_URL: "test.api-efms.itlvn.com",
+        CD_NOTE: "test.api-efms.itlvn.com",
         INDENTITY_SERVER_URL: "http://test.api-efms.itlvn.com/indentityserver"
-    }
+    },
 };
