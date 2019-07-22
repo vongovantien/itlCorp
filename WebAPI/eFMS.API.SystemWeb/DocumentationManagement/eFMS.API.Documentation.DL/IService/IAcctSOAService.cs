682135ccafd7a109e53b496e950ce632ee2f55f5
andy.hoa <andy.hoa@itlvn.com>
2019-07-22 17:55:49 +0700

    commit code - create soa

---
+++ WebAPI/eFMS.API.SystemWeb/DocumentationManagement/eFMS.API.Documentation.DL/IService/IAcctSOAService.cs
@@ -0,0 +1,11 @@
+﻿using eFMS.API.Documentation.DL.Models;
+using eFMS.API.Documentation.Service.Models;
+using ITL.NetCore.Connection.BL;
+
+namespace eFMS.API.Documentation.DL.IService
+{
+    public interface IAcctSOAService : IRepositoryBase<AcctSoa, AcctSoaModel>
+    {
+        object AddSOA(AcctSoaModel model);
+    }
+}
