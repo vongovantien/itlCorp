aa737bed5b8c5a2ed306535c7370a95afc782f21
andy.hoa <andy.hoa@itlvn.com>
2019-07-25 09:06:06 +0700

    commit code - write api get list soa & get details soa

---
+++ WebAPI/eFMS.API.SystemWeb/DocumentationManagement/eFMS.API.Documentation.DL/Models/AcctSOAResult.cs
@@ -0,0 +1,14 @@
+﻿using eFMS.API.Documentation.Service.Models;
+using System.Collections.Generic;
+
+namespace eFMS.API.Documentation.DL.Models
+{
+    public class AcctSOAResult : AcctSoa
+    {
+        public string PartnerName { get; set; }
+        public int Shipment { get; set; }
+        public decimal CreditAmount { get; set; }
+        public decimal DebitAmount { get; set; }
+        public decimal TotalAmount { get { return this.DebitAmount - this.CreditAmount; } }
+    }
+}
