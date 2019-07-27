893690dddbb4debef1053eec0b51566ab886bac6
kenny.thuong <kenny.thuong@itl.com>
2019-07-27 09:28:21 +0700

    ++điều chỉnh sort list charge màn hình detail SOA ++ sửa cột charge name màn hình thêm charge tạo mới SOA

--- WebApp/src/app/business-modules/accounting/statement-of-account/detail/detail-soa.component.ts
+++ WebApp/src/app/business-modules/accounting/statement-of-account/detail/detail-soa.component.ts
@@ -1,18 +1,91 @@
 import { Component } from '@angular/core';
-import { AppPage } from 'src/app/app.base';
+import { ActivatedRoute } from '@angular/router';
+import { AccoutingRepo } from 'src/app/shared/repositories';
+import { catchError, finalize } from 'rxjs/operators';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { HttpErrorResponse } from '@angular/common/http';
+import { ToastrService } from 'ngx-toastr';
+import { SOA } from 'src/app/shared/models';
+import { AppList } from 'src/app/app.list';
+import { SortService } from 'src/app/shared/services';
 
 @Component({
-  selector: 'app-statement-of-account-detail',
-  templateUrl: './detail-soa.component.html',
-  styleUrls: ['./detail-soa.component.scss']
+    selector: 'app-statement-of-account-detail',
+    templateUrl: './detail-soa.component.html',
+    styleUrls: ['./detail-soa.component.scss']
 })
-export class StatementOfAccountDetailComponent extends AppPage {
+export class StatementOfAccountDetailComponent extends AppList {
 
-  constructor() {
-    super();
-  }
+    soaNO: string = '';
+    currencyLocal: string = 'VND';
 
-  ngOnInit() {
-  }
+    soa: SOA = new SOA();
+    headers: any[] = [];
+
+    constructor(
+        private _activedRoute: ActivatedRoute,
+        private _accoutingRepo: AccoutingRepo,
+        private _spinner: NgxSpinnerService,
+        private _toastService: ToastrService,
+        private _sortService: SortService
+    ) {
+        super();
+        this.requestList = this.sortChargeList
+    }
+
+    ngOnInit() {
+        this.headers = [
+            { title: 'Charge Code', field: 'chargeCode', sortable: true },
+            { title: 'Charge Name', field: 'chargeName', sortable: true },
+            { title: 'JobID', field: 'jobId', sortable: true },
+            { title: 'HBL', field: 'hbl', sortable: true },
+            { title: 'MBL', field: 'mbl', sortable: true },
+            { title: 'Custom No', field: 'customNo', sortable: true },
+            { title: 'Debit', field: 'debit', sortable: true },
+            { title: 'Credit', field: 'credit', sortable: true },
+            { title: 'Currency', field: 'currency', sortable: true },
+            { title: 'Invoice No', field: 'invoiceNo', sortable: true },
+            { title: 'Services Date', field: 'serviceDate', sortable: true },
+            { title: 'Note', field: 'note', sortable: true },
+        ];
+        this._activedRoute.queryParams.subscribe((params: any) => {
+            if (!!params.no && params.currency) {
+                this.soaNO = params.no;
+                this.currencyLocal = params.currency;
+                // tslint:disable-next-line: semicolon
+                this.getDetailSOA(this.soaNO, this.currencyLocal)
+            }
+        });
+    }
+
+    getDetailSOA(soaNO: string, currency: string) {
+        this._spinner.show()
+        this._accoutingRepo.getDetaiLSOA(soaNO, currency)
+            .pipe(
+                catchError(this.catchError),
+                finalize(() => { this._spinner.hide(); })
+            )
+            .subscribe(
+                (res: any) => {
+                    this.soa = new SOA(res);
+                    this.totalItems = this.soa.chargeShipments.length;
+                    console.log(this.soa);
+                },
+                (errors: any) => {
+                    let message: string = 'Has Error Please Check Again !';
+                    let title: string = '';
+                    if (errors instanceof HttpErrorResponse) {
+                        message = errors.message;
+                        title = errors.statusText;
+                    }
+                    this._toastService.error(message, title, { positionClass: 'toast-bottom-right' });
+                },
+                () => { }
+            );
+    }
+
+    sortChargeList(sortField?: string, order?: boolean) {
+        this.soa.chargeShipments = this._sortService.sort(this.soa.chargeShipments, sortField, order);
+    }
 
 }
