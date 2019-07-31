18c949bc26580b5f3931d6a0327b9ca33776836a
Ngo Ngoc Toi <lester.toi@itlvn.com>
2019-07-31 10:10:47 +0700

    add missing files

---
+++ WebAPI/eFMS.API.SystemWeb/IdentityServer/eFMS.IdentityServer/Configuration/IAppConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eFMS.IdentityServer.Configuration
+{
+    public interface IAppConfig
+    {
+        string ConnectString { get; }  
+        AuthConfig AuthConfig { get; }
+        CrosConfig CrosConfig { get; }
+    }
+
+    public class AuthConfig
+    {
+        public string Issuer { get; set; }
+        public bool RequireHttps { get; set; }
+        public string[] RedirectUris { get; set; }
+        public int AccessTokenLifetime { get; set; }
+        public int SlidingRefreshTokenLifetime { get; set; }
+    }
+
+    public class CrosConfig {
+        public string[] Urls { get; set; }
+    }
+}
