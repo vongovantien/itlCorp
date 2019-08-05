4ee074710f4ffada17d3480a993fc3836841ecce
irene.tam <irene.tam@itl.com>
2019-08-05 14:00:53 +0700

    fix config service data

--- WebAPI/eFMSWindowService/myserviceinstaller.bat
+++ WebAPI/eFMSWindowService/myserviceinstaller.bat
@@ -1,6 +1,6 @@
 @echo off
 cd C:\Windows\Microsoft.NET\Framework\v4.0.30319
-installutil.exe "D:\Workspace\efms\src\WebAPI\eFMSWindowService\bin\Debug\eFMSWindowService.exe"
+installutil.exe -u "D:\Workspace\efms\src\WebAPI\eFMSWindowService\bin\Debug\eFMSWindowService.exe"
 
 if ERRORLEVEL 1 goto error
 exit
