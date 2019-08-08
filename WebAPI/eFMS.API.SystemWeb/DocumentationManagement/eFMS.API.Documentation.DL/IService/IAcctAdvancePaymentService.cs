a03e94801b04f234c0ba90b84326ab352326e877
andy.hoa <andy.hoa@itlvn.com>
2019-08-08 18:02:53 +0700

    commit code

--- WebAPI/eFMS.API.SystemWeb/DocumentationManagement/eFMS.API.Documentation.DL/IService/IAcctAdvancePaymentService.cs
+++ WebAPI/eFMS.API.SystemWeb/DocumentationManagement/eFMS.API.Documentation.DL/IService/IAcctAdvancePaymentService.cs
@@ -1,5 +1,6 @@
 ﻿using eFMS.API.Documentation.DL.Common;
 using eFMS.API.Documentation.DL.Models;
+using eFMS.API.Documentation.DL.Models.Criteria;
 using eFMS.API.Documentation.Service.Models;
 using ITL.NetCore.Common;
 using ITL.NetCore.Connection.BL;
@@ -12,6 +13,10 @@ namespace eFMS.API.Documentation.DL.IService
     public interface IAcctAdvancePaymentService : IRepositoryBase<AcctAdvancePayment, AcctAdvancePaymentModel>
     {
         List<Shipments> GetShipments();
-        
+
+        HandleState AddAdvancePayment(AcctAdvancePaymentModel model);
+
+        // Kiểm tra lô hàng (JobId, HBL, MBL) đã được add trong advance payment nào hay chưa?
+        bool CheckShipmentsExistInAdvancePayment(ShipmentAdvancePaymentCriteria criteria);
     }
 }
--- WebAPI/eFMS.API.SystemWeb/DocumentationManagement/eFMS.API.Documentation/Controllers/AcctAdvancePaymentController.cs
+++ WebAPI/eFMS.API.SystemWeb/DocumentationManagement/eFMS.API.Documentation/Controllers/AcctAdvancePaymentController.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using eFMS.API.Common;
+using eFMS.API.Common.Globals;
 using eFMS.API.Documentation.DL.Common;
 using eFMS.API.Documentation.DL.IService;
 using eFMS.API.Documentation.DL.Models;
+using eFMS.API.Documentation.DL.Models.Criteria;
+using eFMS.API.Shipment.Infrastructure.Common;
 using eFMS.IdentityServer.DL.UserManager;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Localization;
@@ -49,6 +54,46 @@ namespace eFMS.API.Documentation.Controllers
         {
             var data = acctAdvancePaymentService.GetShipments();
             return Ok(data);
+        }
+
+        /// <summary>
+        /// add new advance payment (include advance request)
+        /// </summary>
+        /// <param name="model">object to add</param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("Add")]
+        //[Authorize]
+        public IActionResult Add(AcctAdvancePaymentModel model)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+            if(model.AdvanceRequests.Count > 0)
+            {
+                var totalAmount = model.AdvanceRequests;
+            }
+
+            var hs = acctAdvancePaymentService.AddAdvancePayment(model);
+
+            var message = HandleError.GetMessage(hs, Crud.Insert);
+            ResultHandle result = new ResultHandle { Status = hs.Success, Message = stringLocalizer[message].Value, Data = model };
+            if (!hs.Success)
+            {
+                return BadRequest(result);
+            }
+            return Ok(result);
+        }
+
+        /// <summary>
+        /// Check shipment exists in advance payment
+        /// </summary>
+        /// <param name="criteria"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("CheckShipmentsExistInAdvancePament")]
+        public IActionResult CheckShipmentsExistInAdvancePayment(ShipmentAdvancePaymentCriteria criteria)
+        {
+            var result = acctAdvancePaymentService.CheckShipmentsExistInAdvancePayment(criteria);
+            return Ok(result);
         }
     }
 }
