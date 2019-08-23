c0f27b7fd0cc875b07540dce8457ac56190e056e
irene.tam <irene.tam@itl.com>
2019-08-23 09:11:17 +0700

    commit

--- WebApp/src/app/shared/repositories/unit.repo.ts
+++ WebApp/src/app/shared/repositories/unit.repo.ts
@@ -12,32 +12,32 @@ export class UnitRepo {
     }
 
     getListUnitByType(data: any = {}) {
-        // return this._api.post(`${environment.HOST.WEB_URL}/Catalogue/api/${this.VERSION}/vi/CatUnit/Query`, data).pipe(
-        //     catchError((error) => throwError(error)),
-        //     map((res: any) => {
-        //         return res;
-        //     })
-        // );
-        return this._api.post(`${environment.HOST.WEB_URL}44361/api/${this.VERSION}/vi/CatUnit/Query`, data).pipe(
+        return this._api.post(`${environment.HOST.WEB_URL}/Catalogue/api/${this.VERSION}/vi/CatUnit/Query`, data).pipe(
             catchError((error) => throwError(error)),
             map((res: any) => {
                 return res;
             })
         );
+        // return this._api.post(`${environment.HOST.WEB_URL}44361/api/${this.VERSION}/vi/CatUnit/Query`, data).pipe(
+        //     catchError((error) => throwError(error)),
+        //     map((res: any) => {
+        //         return res;
+        //     })
+        // );
     }
 
     getAllUnit() {
-        // return this._api.get(`${environment.HOST.WEB_URL}/Catalogue/api/${this.VERSION}/en-US/CatUnit`).pipe(
-        //     catchError((error) => throwError(error)),
-        //     map((data: any) => {
-        //         return data;
-        //     })
-        // );
-        return this._api.get(`${environment.HOST.WEB_URL}44361/api/${this.VERSION}/en-US/CatUnit`).pipe(
+        return this._api.get(`${environment.HOST.WEB_URL}/Catalogue/api/${this.VERSION}/en-US/CatUnit`).pipe(
             catchError((error) => throwError(error)),
             map((data: any) => {
                 return data;
             })
         );
+        // return this._api.get(`${environment.HOST.WEB_URL}44361/api/${this.VERSION}/en-US/CatUnit`).pipe(
+        //     catchError((error) => throwError(error)),
+        //     map((data: any) => {
+        //         return data;
+        //     })
+        // );
     }
 }
