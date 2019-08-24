38f8d0420ca26e5655d00d79678b913364f7fdc3
kenny.thuong <kenny.thuong@itl.com>
2019-08-24 11:48:11 +0700

    + điều chỉnh page not found

--- WebApp/src/app/notfound-page/notfound-page.component.ts
+++ WebApp/src/app/notfound-page/notfound-page.component.ts
@@ -1,15 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-notfound-page',
-  templateUrl: './notfound-page.component.html',
-  styleUrls: ['./notfound-page.component.css']
+    selector: 'app-notfound-page',
+    templateUrl: './notfound-page.component.html',
+    styleUrls: ['./notfound-page.component.css']
 })
 export class NotfoundPageComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+    ngOnInit() {
+    }
 }
