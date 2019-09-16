d0fa70b8eea005f42c6143fdeecec2375ab11560
anpt1902 <phamthienan1902@gmail.com>
2019-09-16 14:07:03 +0700

    update code

---
+++ eFMS.ReportData/eFMS.ReportData.Extension/Criteria/CatCountryCriteria.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eFMS.ReportData.Extension
+{
+    public class CatCountryCriteria
+    {
+       
+        public string Code { get; set; }
+        public string NameVn { get; set; }
+        public string NameEn { get; set; }
+        public bool? Inactive { get; set; }
+        public SearchCondition condition { get; set; }
+    }
+    public enum SearchCondition
+    {
+        AND = 0,
+        OR = 1
+    }
+
+
+}
---
+++ eFMS.ReportData/eFMS.ReportData/Models/CatCommodityModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eFMS.ReportData.Models
+{
+    public class CatCommodityModel: CatCommodity
+    {
+        public string CommodityGroupNameEn { get; set; }
+    }
+}
