0fc695c96670b72cbe6763bd4d0d6c5f41cc3657
kenny.thuong <kenny.thuong@itl.com>
2019-09-17 14:03:27 +0700

    ++ them component app-sub-header

---
+++ WebApp/src/app/shared/common/sub-header/sub-header.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-sub-header',
+    templateUrl: './sub-header.component.html'
+})
+
+export class SubHeaderComponent {
+    constructor() {
+    }
+
+    ngOnInit() { }
+}
