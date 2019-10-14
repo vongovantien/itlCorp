d2e41d30e5b511cc5eab41a9028797adc007b7af
Kenny Thuong <kenny.thuong@itl.com>
2019-10-14 15:43:02 +0700

    ++ them export data exel company

---
+++ WebAPI/eFMS.API.SystemWeb/eFMS.API.ReportData/eFMS.API.ReportData/Controllers/SystemReportController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using eFMS.API.Common;
+using eFMS.API.ReportData.Helpers;
+using eFMS.API.ReportData.HttpServices;
+using eFMS.API.ReportData.Models;
+using eFMS.API.ReportData.Models.Criteria;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace eFMS.API.ReportData.Controllers
+{
+    [ApiVersion("1.0")]
+    [Route("api/v{version:apiVersion}/{lang}/[controller]")]
+    [ApiController]
+    public class SystemReportController : ControllerBase
+    {
+        private readonly APIs aPis;
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="appSettings"></param>
+        /// <param name="fileHelper"></param>
+        public SystemReportController(IOptions<APIs> appSettings)
+        {
+            this.aPis = appSettings.Value;
+
+        }
+
+        [Route("System/ExportCompany")]
+        [HttpPost]
+        public async Task<IActionResult> ExportCompany(SysCompanyCriteria sysCompanyCriteria)
+        {
+            var responseFromApi = await HttpServiceExtension.GetDataFromApi(sysCompanyCriteria, aPis.HostStaging + Urls.System.CompanyUrl);
+
+            var dataObjects = responseFromApi.Content.ReadAsAsync<List<SysCompany>>();
+
+            var stream = new Helper().generateCompanyExcel(dataObjects.Result);
+            if (stream == null)
+            {
+                return null;
+            }
+            FileContentResult fileContent = new FileHelper().ExportExcel(stream, "Company List.xlsx");
+
+            return fileContent;
+
+        }
+
+    }
+}
