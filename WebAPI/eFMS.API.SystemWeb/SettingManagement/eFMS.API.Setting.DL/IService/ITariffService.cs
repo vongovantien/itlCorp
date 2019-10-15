c969cc73d2ba69a26ff033266145e28b735ec9ab
samuel.an <samuel.an@logtechub.com>
2019-10-15 13:52:57 +0700

    update code

---
+++ WebAPI/eFMS.API.SystemWeb/SettingManagement/eFMS.API.Setting.DL/IService/ITariffService.cs
@@ -0,0 +1,14 @@
+﻿using eFMS.API.Setting.DL.Models;
+using ITL.NetCore.Connection.BL;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eFMS.API.Setting.DL.IService
+{
+    public interface ITariffService :  IRepositoryBase<Tariff, TariffModel>
+    {
+
+
+    }
+}
---
+++ WebAPI/eFMS.API.SystemWeb/SettingManagement/eFMS.API.Setting/Controllers/TariffController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using eFMS.API.Setting.DL.IService;
+using eFMS.API.Setting.Infrastructure.Middlewares;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eFMS.API.Setting.Controllers
+{
+    [ApiController]
+    [ApiVersion("1.0")]
+    [MiddlewareFilter(typeof(LocalizationMiddleware))]
+    [Route("api/v{version:apiVersion}/{lang}/[controller]")]
+    public class TariffController : ControllerBase
+    {
+        private readonly ITariffService tariffService;
+        public TariffController(ITariffService service)
+        {
+            tariffService = service;
+        }
+
+
+    }
+}
