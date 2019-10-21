37049162a5b3c1858575af7b2ddf503abb79c9ab
samuel.an <samuel.an@logtechub.com>
2019-10-21 14:52:24 +0700

    update code

---
+++ WebAPI/eFMS.API.SystemWeb/SystemManagement/eFMS.API.System/Controllers/SysEmployeeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using eFMS.API.System.DL.Common;
+using eFMS.API.System.Infrastructure.Middlewares;
+using eFMS.IdentityServer.DL.IService;
+using eFMS.IdentityServer.DL.UserManager;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+
+namespace eFMS.API.System.Controllers
+{
+    [ApiController]
+    [ApiVersion("1.0")]
+    [MiddlewareFilter(typeof(LocalizationMiddleware))]
+    [Route("api/v{version:apiVersion}/{lang}/[controller]")]
+    public class SysEmployeeController : ControllerBase
+    {
+        private readonly ISysEmployeeService  sysEmployeeService;
+
+        public SysEmployeeController(IStringLocalizer<LanguageSub> localizer, ISysEmployeeService isysEmployeeService,
+      IMapper mapper, ICurrentUser currUser
+      )
+        {
+            sysEmployeeService = isysEmployeeService;
+        }
+    }
+}
