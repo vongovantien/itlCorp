703476343fe6d8be926296e6c92d354acd16b695
Kenny Thuong <kenny.thuong@itl.com>
2019-10-31 09:14:55 +0700

    ++update detail

--- WebApp/src/app/business-modules/tool-setting/tariff/detail/detail-tariff.component.ts
+++ WebApp/src/app/business-modules/tool-setting/tariff/detail/detail-tariff.component.ts
@@ -55,47 +55,50 @@ export class TariffDetailComponent extends TariffAddComponent {
         )
             .subscribe((res: CommonInterface.IResult) => {
                 if (res.message) {
-                    this.tariff = new TariffAdd(res.data);
-                    console.log(this.tariff);
-
-                    const objectTariffForm = {
-                        tariffName: this.tariff.setTariff.tariffName,
-                        productService: (this.formAddTariffComponent.productSerices || [].filter(i => i.value === this.tariff.setTariff.productService))[0],
-                        tariffType: this.formAddTariffComponent.tariffTypes.filter(type => type.value === this.tariff.setTariff.tariffType)[0],
-                        status: this.formAddTariffComponent.status.filter(i => i.value === this.tariff.setTariff.status)[0],
-                        effectiveDate: { startDate: new Date(this.tariff.setTariff.effectiveDate), endDate: new Date(this.tariff.setTariff.effectiveDate) },
-                        expiredDate: { startDate: new Date(this.tariff.setTariff.expiredDate), endDate: new Date(this.tariff.setTariff.expiredDate) },
-                        cargoType: this.formAddTariffComponent.cargoTypes.filter(type => type.value === this.tariff.setTariff.cargoType)[0],
-                        serviceMode: this.formAddTariffComponent.serviceModes.filter(mode => mode.value === this.tariff.setTariff.serviceMode)[0],
-                        description: this.tariff.setTariff.description,
-                    };
-
-                    // * Update Form Group.
-                    this.formAddTariffComponent.formAdd.controls['tariff'].setValue(objectTariffForm);
-
-                    // * Update comboGrid.
-                    this.formAddTariffComponent.selectedOffice = <CommonInterface.IComboGridData>{ field: 'id', value: this.tariff.setTariff.officeId };
-
-                    switch (this.tariff.setTariff.tariffType) {
-                        case 'Customer':
-                            this.formAddTariffComponent.selectedCustomer = <CommonInterface.IComboGridData>{ field: 'id', value: this.tariff.setTariff.customerId };
-
-                            break;
-                        case 'Supplier':
-                            this.formAddTariffComponent.selectedSupplier = <CommonInterface.IComboGridData>{ field: 'id', value: this.tariff.setTariff.supplierId };
-
-                            break;
-                        case 'Agent':
-                            this.formAddTariffComponent.selectedAgent = <CommonInterface.IComboGridData>{ field: 'id', value: this.tariff.setTariff.agentId };
-                            break;
-                        default:
-
-                            break;
-                    }
-
-                    this.formAddTariffComponent.onChangeTariffType({ value: this.tariff.setTariff.tariffType, displayName: '' });
+                    this.getTariffDetail(res.data);
                 }
             });
+    }
+
+    getTariffDetail(res: any) {
+        this.tariff = new TariffAdd(res);
+
+        const objectTariffForm = {
+            tariffName: this.tariff.setTariff.tariffName,
+            productService: (this.formAddTariffComponent.productSerices || [].filter(i => i.value === this.tariff.setTariff.productService))[0],
+            tariffType: this.formAddTariffComponent.tariffTypes.filter(type => type.value === this.tariff.setTariff.tariffType)[0],
+            status: this.formAddTariffComponent.status.filter(i => i.value === this.tariff.setTariff.status)[0],
+            effectiveDate: { startDate: new Date(this.tariff.setTariff.effectiveDate), endDate: new Date(this.tariff.setTariff.effectiveDate) },
+            expiredDate: { startDate: new Date(this.tariff.setTariff.expiredDate), endDate: new Date(this.tariff.setTariff.expiredDate) },
+            cargoType: this.formAddTariffComponent.cargoTypes.filter(type => type.value === this.tariff.setTariff.cargoType)[0],
+            serviceMode: this.formAddTariffComponent.serviceModes.filter(mode => mode.value === this.tariff.setTariff.serviceMode)[0],
+            description: this.tariff.setTariff.description,
+        };
+
+        // * Update Form Group.
+        this.formAddTariffComponent.formAdd.controls['tariff'].setValue(objectTariffForm);
+
+        // * Update comboGrid.
+        this.formAddTariffComponent.selectedOffice = <CommonInterface.IComboGridData>{ field: 'id', value: this.tariff.setTariff.officeId };
+
+        switch (this.tariff.setTariff.tariffType) {
+            case 'Customer':
+                this.formAddTariffComponent.selectedCustomer = <CommonInterface.IComboGridData>{ field: 'id', value: this.tariff.setTariff.customerId };
+
+                break;
+            case 'Supplier':
+                this.formAddTariffComponent.selectedSupplier = <CommonInterface.IComboGridData>{ field: 'id', value: this.tariff.setTariff.supplierId };
+
+                break;
+            case 'Agent':
+                this.formAddTariffComponent.selectedAgent = <CommonInterface.IComboGridData>{ field: 'id', value: this.tariff.setTariff.agentId };
+                break;
+            default:
+
+                break;
+        }
+
+        this.formAddTariffComponent.onChangeTariffType({ value: this.tariff.setTariff.tariffType, displayName: '' });
     }
 
     updateTafiff() {
@@ -103,8 +106,6 @@ export class TariffDetailComponent extends TariffAddComponent {
             return;
         }
         this.onSubmitData();
-
-
 
         this._progressRef.start();
         if (this.ACTION === "COPY") {
@@ -118,6 +119,16 @@ export class TariffDetailComponent extends TariffAddComponent {
                     (res: CommonInterface.IResult) => {
                         if (res.status) {
                             this._toastService.success(res.message);
+
+                            // * Get detail
+                            this._settingRepo.getDetailTariff(this.tariffId)
+                                .subscribe(
+                                    (result: CommonInterface.IResult) => {
+                                        if (result.message) {
+                                            this.getTariffDetail(result.data);
+                                        }
+                                    }
+                                );
                         } else {
                             this._toastService.error(res.message);
                         }
