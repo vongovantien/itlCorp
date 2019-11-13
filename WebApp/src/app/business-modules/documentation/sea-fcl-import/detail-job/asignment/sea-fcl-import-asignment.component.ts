bbc7c9f78329928b268897f8a0e72f0b7c8f7d3b
samuel.an <samuel.an@logtechub.com>
2019-11-13 17:42:50 +0700

    update code

--- WebApp/src/app/business-modules/documentation/sea-fcl-import/detail-job/asignment/sea-fcl-import-asignment.component.ts
+++ WebApp/src/app/business-modules/documentation/sea-fcl-import/detail-job/asignment/sea-fcl-import-asignment.component.ts
@@ -4,6 +4,7 @@ import { OperationRepo, DocumentationRepo } from 'src/app/shared/repositories';
 import { ActivatedRoute } from '@angular/router';
 import { SortService } from 'src/app/shared/services';
 import { NgProgress } from '@ngx-progressbar/core';
+import { catchError, finalize } from 'rxjs/operators';
 
 @Component({
     selector: 'app-sea-fcl-import-asignment',
@@ -11,6 +12,9 @@ import { NgProgress } from '@ngx-progressbar/core';
 })
 
 export class SeaFCLImportAsignmentComponent extends AppList {
+    data: any = null;
+    jobId: string = '';
+
     constructor(
         private _operation: OperationRepo,
         private _activedRouter: ActivatedRoute,
@@ -19,7 +23,34 @@ export class SeaFCLImportAsignmentComponent extends AppList {
         private _ngProgressService: NgProgress,
     ) {
         super();
+        this._progressRef = this._ngProgressService.ref();
+
     }
 
-    ngOnInit() { }
+    ngOnInit() {
+        this._activedRouter.params.subscribe((param: any) => {
+            if (param.id) {
+                this.jobId = param.id;
+                this.getCSTransactionDetails(this.jobId);
+
+            }
+        });
+    }
+
+
+    getCSTransactionDetails(id: any) {
+        this._progressRef.start();
+        this._documentRepo.getDetailTransaction(id)
+            .pipe(
+                catchError(this.catchError),
+                finalize(() => this._progressRef.complete()),
+            ).subscribe(
+                (response: any) => {
+
+                    this.data = response;
+                    console.log(this.data);
+                },
+            );
+    }
+
 }
--- WebApp/src/app/business-modules/documentation/sea-fcl-import/detail-job/asignment/sea-fcl-import-asignment.module.ts
+++ WebApp/src/app/business-modules/documentation/sea-fcl-import/detail-job/asignment/sea-fcl-import-asignment.module.ts
@@ -1,9 +1,22 @@
 import { NgModule } from '@angular/core';
 import { SeaFCLImportAsignmentComponent } from './sea-fcl-import-asignment.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { ModalModule } from 'ngx-bootstrap';
+import { SelectModule } from 'ng2-select';
+import { NgxDaterangepickerMd } from 'ngx-daterangepicker-material';
 
 
 @NgModule({
     imports: [
+        CommonModule,
+        FormsModule,
+        SharedModule,
+        ReactiveFormsModule,
+        SelectModule,
+        ModalModule,
+        NgxDaterangepickerMd,
     ],
     exports: [],
     declarations: [
