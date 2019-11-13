ca15b75abb7354a12362e4939efa5b8a5bd42f33
samuel.an <samuel.an@logtechub.com>
2019-11-13 11:46:24 +0700

    update code

--- WebApp/src/app/business-modules/documentation/sea-fcl-import/detail-job/asignment/sea-fcl-import-asignment.component.ts
+++ WebApp/src/app/business-modules/documentation/sea-fcl-import/detail-job/asignment/sea-fcl-import-asignment.component.ts
@@ -1,12 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { AppList } from 'src/app/app.list';
+import { OperationRepo, DocumentationRepo } from 'src/app/shared/repositories';
+import { ActivatedRoute } from '@angular/router';
+import { SortService } from 'src/app/shared/services';
+import { NgProgress } from '@ngx-progressbar/core';
 
 @Component({
     selector: 'app-sea-fcl-import-asignment',
     templateUrl: './sea-fcl-import-asignment.component.html'
 })
 
-export class SeaFCLImportAsignmentComponent {
-    constructor() { }
+export class SeaFCLImportAsignmentComponent extends AppList {
+    constructor(
+        private _operation: OperationRepo,
+        private _activedRouter: ActivatedRoute,
+        private _sortService: SortService,
+        private _documentRepo: DocumentationRepo,
+        private _ngProgressService: NgProgress,
+    ) {
+        super();
+    }
 
     ngOnInit() { }
 }
