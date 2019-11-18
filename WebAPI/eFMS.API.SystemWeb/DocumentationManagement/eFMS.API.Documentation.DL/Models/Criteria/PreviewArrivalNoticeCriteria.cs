47c8efbdd7f6133c61b75e7da5ad6d2380ecc77a
andy.hoa <andy.hoa@itlvn.com>
2019-11-18 15:15:37 +0700

    commit code - add class report arrival notice

---
+++ WebAPI/eFMS.API.SystemWeb/DocumentationManagement/eFMS.API.Documentation.DL/Models/Criteria/PreviewArrivalNoticeCriteria.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eFMS.API.Documentation.DL.Models.Criteria
+{
+    public class PreviewArrivalNoticeCriteria
+    {
+        public Guid HblId { get; set; }
+        public string Currency { get; set; }
+    }
+}
