8b4e9f34a44754b422dc26f618313e06b6c8252e
Kenny Thuong <kenny.thuong@itl.com>
2019-11-26 11:13:44 +0700

    +=fix transattion reducer trong share bussiness

--- WebApp/src/app/business-modules/share-business/store/reducers/transaction.reducer.ts
+++ WebApp/src/app/business-modules/share-business/store/reducers/transaction.reducer.ts
@@ -17,17 +17,39 @@ export interface ITransactionProfit {
 
 export interface ITransactionState {
     profits: ITransactionProfit[];
+    isLoading: boolean;
+    isLoaded: boolean;
 }
 
 export const initState: ITransactionState = {
-    profits: []
+    profits: [],
+    isLoading: false,
+    isLoaded: false
 };
 
 
-export function TransactionReducer(state = initState, action: TransactionActions): ITransactionState | any {
+export function TransactionReducer(state = initState, action: TransactionActions): ITransactionState {
     switch (action.type) {
+        case TransactionActionTypes.GET_PROFIT: {
+            return {
+                ...state,
+                isLoaded: false,
+                isLoading: true
+            };
+        }
         case TransactionActionTypes.GET_PROFIT_SUCCESS: {
-            return { ...state, ...state.profits, profits: [...action.payload] };
+            return {
+                ...state,
+                ...state.profits,
+                profits: [...action.payload],
+                isLoaded: true,
+                isLoading: false
+
+            };
+        }
+
+        default: {
+            return state;
         }
     }
 }
