1e767efbcfba0ad044c8916937a9d8142a69fbbb
Kenny Thuong <kenny.thuong@itl.com>
2019-11-26 17:49:22 +0700

    ++fix sorting srcharge

--- WebApp/src/app/shared/services/sort.service.ts
+++ WebApp/src/app/shared/services/sort.service.ts
@@ -1,41 +1,47 @@
-// import orderBy from 'lodash/orderBy';
-// import deburr from 'lodash/deburr';
+import orderBy from 'lodash/orderBy';
+import deburr from 'lodash/deburr';
 import { Injectable } from '@angular/core';
 
 @Injectable({ providedIn: 'root' })
 export class SortService {
-    // sort(items: any[], property: string, isDesc: boolean) {
+    sort(items: any[], property: string, isDesc: boolean) {
+        if (isDesc) {
+            return orderBy(items, [item => (!item[property] ? null : this.prepareStringToSort(item[property]))], ['asc']);
+        } else {
+            return orderBy(items, [item => (!item[property] ? null : this.prepareStringToSort(item[property]))], ['desc']);
+        }
+    }
+
+    // sort(collection: any[], prop: any, isDesc: boolean) {
+    //     console.log(isDesc);
     //     if (isDesc) {
-    //         return orderBy(items, [item => (!item[property] ? null : this.prepareStringToSort(item[property]))], ['asc']);
+    //         const a = collection.sort(this.compareValues(prop, 'desc'));
+    //         console.log(a);
+    //         return a;
     //     } else {
-    //         return orderBy(items, [item => (!item[property] ? null : this.prepareStringToSort(item[property]))], ['desc']);
+    //         const a = collection.sort(this.compareValues(prop, 'asc'));
+    //         console.log(a);
+    //         return a;
+
     //     }
+
     // }
 
-    sort(collection: any[], prop: any, isDesc: boolean) {
-        if (isDesc) {
-            return collection.sort(this.compareValues(prop, 'desc'));
-        } else {
-            return collection.sort(this.compareValues(prop, 'asc'));
+    private prepareStringToSort(str: any) {
+        if (typeof str === "string") {
+            const x = Number("1000");
+            if (isNaN(x)) {
+                return x;
+            } else {
+                str = str.toString().toLowerCase();
+                return deburr(str);
+            }
+        }
+        if (typeof str === "number") {
+            return str;
         }
 
     }
-
-    // private prepareStringToSort(str: any) {
-    //     if (typeof str === "string") {
-    //         const x = Number("1000");
-    //         if (isNaN(x)) {
-    //             return x;
-    //         } else {
-    //             str = str.toString().toLowerCase();
-    //             return deburr(str);
-    //         }
-    //     }
-    //     if (typeof str === "number") {
-    //         return str;
-    //     }
-
-    // }
 
     resolve(path: string, obj: any) {
         return path.split('.').reduce((prev, curr) => {
@@ -50,10 +56,8 @@ export class SortService {
                 return 0;
             }
 
-            const varA = (typeof a[key] === 'string') ?
-                a[key].toUpperCase() : a[key];
-            const varB = (typeof b[key] === 'string') ?
-                b[key].toUpperCase() : b[key];
+            const varA = (typeof a[key] === 'string') ? a[key] : a[key];
+            const varB = (typeof b[key] === 'string') ? b[key] : b[key];
 
             let comparison = 0;
             if (varA > varB) {
@@ -61,9 +65,7 @@ export class SortService {
             } else if (varA < varB) {
                 comparison = -1;
             }
-            return (
-                (order === 'desc') ? (comparison * -1) : comparison
-            );
+            return ((order === 'desc') ? (comparison * -1) : comparison);
         };
     }
 }
