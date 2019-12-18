128606a277ca1c500e7d53ca7b2d5132f071e10e
Kenny Thuong <kenny.thuong@itl.com>
2019-12-18 09:38:41 +0700

    update settlement payment module

--- WebApp/src/app/business-modules/accounting/settlement-payment/settlement-payment.module.ts
+++ WebApp/src/app/business-modules/accounting/settlement-payment/settlement-payment.module.ts
@@ -1,21 +1,23 @@
 import { NgModule } from '@angular/core';
-import { SettlementPaymentComponent } from './settlement-payment.component';
 import { Routes, RouterModule } from '@angular/router';
-import { SettlementFormSearchComponent } from './components/form-search-settlement/form-search-settlement.component';
 import { CommonModule } from '@angular/common';
-import { SharedModule } from 'src/app/shared/shared.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { NgxDaterangepickerMd } from 'ngx-daterangepicker-material';
 import { PaginationModule, AccordionModule, ModalModule } from 'ngx-bootstrap';
 import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
 import { NgProgressModule } from '@ngx-progressbar/core';
+import { NgxCurrencyModule } from 'ngx-currency';
+
+import { SettlementPaymentComponent } from './settlement-payment.component';
+import { SettlementFormSearchComponent } from './components/form-search-settlement/form-search-settlement.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 import { SettlementPaymentAddNewComponent } from './add/add-settlement-payment.component';
 import { SettlementFormCreateComponent } from './components/form-create-settlement/form-create-settlement.component';
 import { SettlementListChargeComponent } from './components/list-charge-settlement/list-charge-settlement.component';
 import { SettlementPaymentManagementPopupComponent } from './components/popup/payment-management/payment-management.popup';
 import { SettlementExistingChargePopupComponent } from './components/popup/existing-charge/existing-charge.popup';
 import { SettlementFormChargePopupComponent } from './components/popup/form-charge/form-charge.popup';
-import { NgxCurrencyModule } from 'ngx-currency';
 import { SettlementShipmentItemComponent } from './components/shipment-item/shipment-item.component';
 import { SettlementTableSurchargeComponent } from './components/table-surcharge/table-surcharge.component';
 import { SettlementPaymentDetailComponent } from './detail/detail-settlement-payment.component';
