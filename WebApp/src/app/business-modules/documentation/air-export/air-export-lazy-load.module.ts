64103560227cf6434fe95e0f5e11d0c2148f665e
Kenny Thuong <kenny.thuong@itl.com>
2020-01-10 16:13:53 +0700

    ++ them spinner surcharge, ++ thêm delete file

--- WebApp/src/app/business-modules/documentation/air-export/air-export-lazy-load.module.ts
+++ WebApp/src/app/business-modules/documentation/air-export/air-export-lazy-load.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from "@angular/core";
 import { LAZY_MODULES_MAP, ILazyModules } from "src/app/load-module-map";
-import { AirExportAttachFilesModule } from "./detail-job/attach-file/air-export-attach-file.module";
 
 export const lazyModulesFClMap: ILazyModules = {
     AirExport_cdNote: () => import('./detail-job/cd-note/air-export-cd-note.module').then(m => m.AirExportCDNoteModule),
     AirExport_assignment: () => import('./detail-job/assignment/air-export-assigment.module').then(m => m.AirExportAssignmentModule),
-    AirExport_attachFile: () => import('./detail-job/attach-file/air-export-attach-file.module').then(m => AirExportAttachFilesModule)
+    AirExport_attachFile: () => import('./detail-job/attach-file/air-export-attach-file.module').then(m => m.AirExportAttachFilesModule)
 };
 
 @NgModule({
