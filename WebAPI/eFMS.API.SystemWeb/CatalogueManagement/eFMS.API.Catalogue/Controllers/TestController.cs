86ed0afe5e9157201addaac8b4442ba5492a8582
irene.tam <irene.tam@itl.com>
2020-01-20 09:22:36 +0700

    commit

--- WebAPI/eFMS.API.SystemWeb/CatalogueManagement/eFMS.API.Catalogue/Controllers/TestController.cs
+++ WebAPI/eFMS.API.SystemWeb/CatalogueManagement/eFMS.API.Catalogue/Controllers/TestController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using eFMS.API.Catalogue.Authorize;
 using eFMS.API.Catalogue.DL.Common;
 using eFMS.API.Catalogue.DL.IService;
 using eFMS.API.Catalogue.Infrastructure.Middlewares;
@@ -13,26 +14,15 @@ namespace eFMS.API.Catalogue.Controllers
     [Route("api/v{version:apiVersion}/{lang}/[controller]")]
     public class TestController : ControllerBase
     {
-        private readonly IStringLocalizer stringLocalizer;
-        private readonly ICatBranchService catbranchService;
-        public TestController(IStringLocalizer<LanguageSub> localizer, ICatBranchService service)
+        public TestController()
         {
-            stringLocalizer = localizer;
-            catbranchService = service;
-        }
 
+        }
+        
+        [AuthorizeEx("ABC")]
         [HttpGet]
         public IActionResult Get()
         {
-            return Ok(stringLocalizer[LanguageSub.MSG_DATA_NOT_FOUND]);
-        }
-
-        [HttpGet("{id}")]
-        public IActionResult Get(Guid id)
-        {
-            var s = catbranchService.Any(x => x.Id == id);
-            var t = catbranchService.First(x => x.Id == id);
-            var result = catbranchService.Get();
             return Ok();
         }
     }
