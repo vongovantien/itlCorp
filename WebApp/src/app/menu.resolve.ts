8ea5bc05bb98723b64618dfd205fdcc40e338306
DoanVanThuong <doanvanthuong196@gmail.com>
2020-02-19 11:48:28 +0700

    ++ detect menu import

--- WebApp/src/app/menu.resolve.ts
+++ WebApp/src/app/menu.resolve.ts
@@ -20,32 +20,45 @@ export class MenuResolveGuard implements Resolve<any> {
         route: ActivatedRouteSnapshot,
         state: RouterStateSnapshot
     ): Observable<any> | Promise<any> | any {
-        const pathArray = state.url.split("/");
-        if (!!pathArray[3]) {
-            return this._systemRepo.getUserPermissionByMenu(pathArray[3])
+        const pathArray = state.url.split("/").filter(i => Boolean(i));
+        if (!!pathArray[2]) {
+            return this._systemRepo.getUserPermissionByMenu(pathArray[2])
                 .pipe()
                 .subscribe(
                     (res: SystemInterface.IUserPermission) => {
                         console.log(res);
-
-                        // * Dispatch to redux.
                         this._store.dispatch(new MenuUpdatePermissionAction(res));
+
                         if (!!res && !res.access) {
-                            this._router.navigate(["/login"]);
+                            throw 403;
+                        }
+                        if (pathArray[3] === 'new') {
+                            if (!res.allowAdd) {
+                                throw 403;
+                            }
+                        }
+                        if (pathArray[3] === 'import') {
+                            if (!res.import) {
+                                throw 403;
+                            }
                         }
                     },
-                    (err: HttpErrorResponse | any) => {
-                        switch (err.status) {
-                            // case 403:
-                            //     // TODO redirect to forbidden page.
-                            //     break;
-                            default:
-                                this._router.navigate(["/login"]);
-                                break;
+                    (err: HttpErrorResponse | number | any) => {
+                        if (err === 403) {
+                            console.log(err);
+                            // TODO redirect to forbidden page.
+                        } else {
+                            switch (err.status) {
+                                case 403:
+                                    // TODO redirect to forbidden page.
+                                    break;
+                                default:
+                                    this._router.navigate(["/login"]);
+                                    break;
+                            }
                         }
                     }
                 );
         }
-
     }
 }
