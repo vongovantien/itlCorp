7ac477e11486804ab972b481c864aa89c44dce03
irene.tam <irene.tam@itl.com>
2020-02-20 10:52:31 +0700

    modify step by step build

--- WebAPI/@build-helper/1.build.common-all.bat
+++ WebAPI/@build-helper/1.build.common-all.bat
@@ -1,6 +1,5 @@
 @echo off
 
 call build-common.bat
-call build.infrasture.bat
 call build.provider.bat
 pause
---
+++ WebAPI/@build-helper/6.copy-infrasture.bat
@@ -0,0 +1,11 @@
+for %%d in (
+	..\eFMS.API.SystemWeb\AccountingManagement\bin\netcoreapp2.2\,
+	..\eFMS.API.SystemWeb\CatalogueManagement\bin\netcoreapp2.2\,
+	..\eFMS.API.SystemWeb\DocumentationManagement\bin\netcoreapp2.2\,
+	..\eFMS.API.SystemWeb\eFMS.API.ReportData\bin\netcoreapp2.2\,
+	..\eFMS.API.SystemWeb\IdentityServer\bin\netcoreapp2.2\,
+	..\eFMS.API.SystemWeb\OperationManagement\bin\netcoreapp2.2\,	
+	..\eFMS.API.SystemWeb\SettingManagement\bin\netcoreapp2.2\,
+	..\eFMS.API.SystemWeb\SystemManagement\bin\netcoreapp2.2\
+) do xcopy "..\eFMS.API.Common\bin\netcoreapp2.2\eFMS.API.Infrastructure.*" "%%d" /E /Y
+pause
