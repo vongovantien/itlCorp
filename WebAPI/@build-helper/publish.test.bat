34c422630109a1738adc070cdbe0214b85950e8c
irene.tam <irene.tam@itl.com>
2020-02-20 08:51:25 +0700

    commit

--- WebAPI/@build-helper/publish.test.bat
+++ WebAPI/@build-helper/publish.test.bat
@@ -1,2 +1,3 @@
-dotnet publish "..\eFMS.API.SystemWeb\CatalogueManagement\eFMS.API.Catalogue" -c Debug --output="\\192.168.7.31\inetpub\efms-api\Catalogue"
+dotnet build "..\eFMS.API.SystemWeb\CatalogueManagement\eFMS.API.Catalogue" --configuration Release
+call dotnet publish "..\eFMS.API.SystemWeb\CatalogueManagement\eFMS.API.Catalogue" -c Release --output="\\192.168.7.31\inetpub\efms-api\Catalogue"
 pause
