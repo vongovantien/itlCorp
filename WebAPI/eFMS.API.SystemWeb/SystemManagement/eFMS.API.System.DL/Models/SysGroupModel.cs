4ae52fc8caf166db915a7dd25918724b3e378887
samuel.an <samuel.an@logtechub.com>
2020-02-25 14:24:01 +0700

    - update code

--- WebAPI/eFMS.API.SystemWeb/SystemManagement/eFMS.API.System.DL/Models/SysGroupModel.cs
+++ WebAPI/eFMS.API.SystemWeb/SystemManagement/eFMS.API.System.DL/Models/SysGroupModel.cs
@@ -26,5 +26,8 @@ namespace eFMS.API.System.DL.Models
         public string OfficeName { get; set; }
         public Guid? CompanyId { get; set; }
         public Guid? OfficeId { get; set; }
+        public string NameUserCreated { get; set; }
+        public string NameUserModified { get; set; }
+
     }
 }
