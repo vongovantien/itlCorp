dfe47ec5adfa8935be3a3e72213a07f406d3aeb4
irene.tam <irene.tam@itl.com>
2020-03-19 17:27:13 +0700

    commit

---
+++ WebAPI/eFMS.API.SystemWeb/DocumentationManagement/eFMS.API.Documentation/Controllers/SaleReportController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SystemManagementAPI.Infrastructure.Middlewares;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace eFMS.API.Documentation.Controllers
+{
+    [ApiController]
+    [ApiVersion("1.0")]
+    [MiddlewareFilter(typeof(LocalizationMiddleware))]
+    [Route("api/v{version:apiVersion}/{lang}/[controller]")]
+    public class SaleReportController : ControllerBase
+    {
+        public SaleReportController()
+        {
+        }
+
+        public IActionResult MonthlySalereport()
+        {
+            return Ok();
+        }
+    }
+}
