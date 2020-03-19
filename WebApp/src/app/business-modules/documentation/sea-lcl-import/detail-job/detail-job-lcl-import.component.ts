adebb2979249970b8c2d6769c735d7cb758e9a87
DoanVanThuong <doanvanthuong196@gmail.com>
2020-03-19 17:50:54 +0700

    ++ update bug

--- WebApp/src/app/business-modules/documentation/sea-lcl-import/detail-job/detail-job-lcl-import.component.ts
+++ WebApp/src/app/business-modules/documentation/sea-lcl-import/detail-job/detail-job-lcl-import.component.ts
@@ -127,6 +127,8 @@ export class SeaLCLImportDetailJobComponent extends SeaLCLImportCreateJobCompone
         modelAdd.jobNo = this.shipmentDetail.jobNo;
         modelAdd.datetimeCreated = this.shipmentDetail.datetimeCreated;
         modelAdd.userCreated = this.shipmentDetail.userCreated;
+        modelAdd.currentStatus = this.shipmentDetail.currentStatus;
+
         if (this.ACTION === 'COPY') {
             this.duplicateJob(modelAdd);
         } else {
