901a12174a4c7693e607e801ffec942903e0043b
irene.tam <irene.tam@itl.com>
2020-04-06 05:00:03 +0000

    Merge remote-tracking branch 'origin/staging/fixbug/fix-duplicate-charge-invoice' into staging/01-04-2020
    (cherry picked from commit 2092089be802761e518b9f6737761827c6bab872)
    
    d574ef86 ++ update dupilicate charge trong surcharge
    24238af1 ++ hbl abstract base class cho services
    c6dafd84 ++ refactor hbl service theo hbl abstract class

--- WebApp/src/app/business-modules/documentation/air-import/detail-job/hbl/air-import-hbl.component.ts
+++ WebApp/src/app/business-modules/documentation/air-import/detail-job/hbl/air-import-hbl.component.ts
@@ -1,210 +1,35 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { Router, Params } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { ToastrService } from 'ngx-toastr';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { NgProgress } from '@ngx-progressbar/core';
-
-import { AppList } from 'src/app/app.list';
-import { getParamsRouterState } from 'src/app/store';
-import { DocumentationRepo } from 'src/app/shared/repositories';
-import { CsTransactionDetail, HouseBill, CsTransaction } from 'src/app/shared/models';
-import { ConfirmPopupComponent, Permission403PopupComponent, InfoPopupComponent } from 'src/app/shared/common/popup';
-
-import { catchError, finalize, takeUntil, take, skip } from 'rxjs/operators';
-import * as fromShareBussiness from '../../../../share-business/store';
-import { ReportPreviewComponent } from 'src/app/shared/common';
-import { ShareBussinessSellingChargeComponent } from 'src/app/business-modules/share-business/components/selling-charge/selling-charge.component';
-
-import isUUID from 'validator/lib/isUUID';
+import { DocumentationRepo } from '@repositories';
 import { SortService } from '@services';
+import { IShareBussinessState, AppShareHBLBase } from '@share-bussiness';
 
+import { catchError, finalize } from 'rxjs/operators';
 
 @Component({
     selector: 'app-air-import-hbl',
     templateUrl: './air-import-hbl.component.html'
 })
 
-export class AirImportHBLComponent extends AppList implements OnInit {
+export class AirImportHBLComponent extends AppShareHBLBase implements OnInit {
 
-    @ViewChild(ConfirmPopupComponent, { static: false }) confirmDeleteHBLPopup: ConfirmPopupComponent;
-    @ViewChild('confirmDeleteJob', { static: false }) confirmDeleteJobPopup: ConfirmPopupComponent;
-    @ViewChild(ReportPreviewComponent, { static: false }) previewPopup: ReportPreviewComponent;
-    @ViewChild(ShareBussinessSellingChargeComponent, { static: false }) sellingChargeComponent: ShareBussinessSellingChargeComponent;
-    @ViewChild(Permission403PopupComponent, { static: false }) info403Popup: Permission403PopupComponent;
-    @ViewChild(InfoPopupComponent, { static: false }) canNotDeleteJobPopup: InfoPopupComponent;
-
-    jobId: string;
-    headers: CommonInterface.IHeaderTable[];
-    houseBills: HouseBill[] = [];
-
-    selectedHbl: CsTransactionDetail;
-    shipmentDetail: CsTransaction;
-
-
-    selectedTabSurcharge: string = 'BUY';
-
-    dataReport: any = null;
-
-    totalCBM: number;
-    totalGW: number;
-    totalCW: number;
-
-    spinnerSurcharge: string = 'spinnerSurcharge';
+    serviceType: CommonType.SERVICE_TYPE = 'air';
 
     constructor(
         private _router: Router,
-        private _store: Store<fromShareBussiness.IShareBussinessState>,
-        private _documentRepo: DocumentationRepo,
-        private _toastService: ToastrService,
-        private _progressService: NgProgress,
-        private _spinner: NgxSpinnerService,
-        private _sortService: SortService
+        protected _store: Store<IShareBussinessState>,
+        protected _documentRepo: DocumentationRepo,
+        protected _toastService: ToastrService,
+        protected _progressService: NgProgress,
+        protected _spinner: NgxSpinnerService,
+        protected _sortService: SortService
 
     ) {
-        super();
-        this._progressRef = this._progressService.ref();
-        this.requestSort = this.sortLocal;
-    }
-
-    ngOnInit() {
-        this._store.select(getParamsRouterState)
-            .pipe(takeUntil(this.ngUnsubscribe), take(1))
-            .subscribe((param: Params) => {
-                if (param.jobId && isUUID(param.jobId)) {
-                    this.jobId = param.jobId;
-                    this._store.dispatch(new fromShareBussiness.TransactionGetDetailAction(this.jobId));
-                    this.getDetailShipment();
-                    this._store.dispatch(new fromShareBussiness.GetListHBLAction({ jobId: this.jobId }));
-
-                    this.getHouseBills(this.jobId);
-                } else {
-                    this.gotoList();
-                }
-            });
-
-        this.headers = [
-            { title: 'HBL No', field: 'hwbno', sortable: true, width: 100 },
-            { title: 'Customer', field: 'customerName', sortable: true },
-            { title: 'SaleMan', field: 'saleManName', sortable: true },
-            { title: 'Departure', field: 'finalDestinationPlace', sortable: true },
-            { title: 'Destination', field: 'finalDestinationPlace', sortable: true },
-            { title: 'Package', field: 'packages', sortable: true },
-            { title: 'C.W', field: 'cw', sortable: true },
-            { title: 'G.W', field: 'gw', sortable: true },
-            { title: 'CBM', field: 'cbm', sortable: true }
-        ];
-
-        this.isLocked = this._store.select(fromShareBussiness.getTransactionLocked);
-        this.isLoading = this._store.select(fromShareBussiness.getHBLLoadingState);
-        this._store.select(fromShareBussiness.getSurchargeLoadingState).subscribe(
-            (loading: boolean) => {
-                if (loading) {
-                    this._spinner.show(this.spinnerSurcharge);
-                } else {
-                    this._spinner.hide(this.spinnerSurcharge);
-                }
-            }
-        );
-    }
-
-    getHouseBills(id: string) {
-        this._store.select(fromShareBussiness.getHBLSState)
-            .pipe(takeUntil(this.ngUnsubscribe))
-            .subscribe(
-                (hbls: any[]) => {
-                    this.houseBills = hbls;
-                    if (!!this.houseBills.length) {
-                        this.totalGW = this.houseBills.reduce((acc: number, curr: HouseBill) => acc += curr.gw, 0);
-                        this.totalCBM = this.houseBills.reduce((acc: number, curr: HouseBill) => acc += curr.cbm, 0);
-                        this.totalCW = this.houseBills.reduce((acc: number, curr: HouseBill) => acc += curr.cw, 0);
-
-                        this.selectHBL(this.houseBills[0]);
-                    } else {
-                        this.selectedHbl = null;
-                    }
-                }
-            );
-    }
-
-    getDetailShipment() {
-        this._store.select<any>(fromShareBussiness.getTransactionDetailCsTransactionState)
-            .pipe(
-                skip(1),
-                takeUntil(this.ngUnsubscribe)
-            )
-            .subscribe(
-                (res: any) => {
-                    if (!!res) {
-                        this.shipmentDetail = res;
-                    }
-                },
-            );
-    }
-
-    sortLocal(sort: string): void {
-        this.houseBills = this._sortService.sort(this.houseBills, sort, this.order);
-    }
-
-    showDeletePopup(hbl: CsTransactionDetail, event: Event) {
-        event.preventDefault();
-        event.stopImmediatePropagation();
-        event.stopPropagation();
-
-        this.confirmDeleteHBLPopup.show();
-        this.selectedHbl = hbl;
-    }
-
-    onDeleteHbl() {
-        this.confirmDeleteHBLPopup.hide();
-        this.deleteHbl(this.selectedHbl.id);
-    }
-
-    deleteHbl(id: string) {
-        this._progressRef.start();
-        this._documentRepo.deleteHbl(id)
-            .pipe(
-                catchError(this.catchError),
-                finalize(() => { this._progressRef.complete(); }),
-            ).subscribe(
-                (res: CommonInterface.IResult) => {
-                    if (res.status) {
-                        this._toastService.success(res.message, '');
-                        this._store.dispatch(new fromShareBussiness.GetListHBLAction({ jobId: this.jobId }));
-                        this.getHouseBills(this.jobId);
-                    } else {
-                        this._toastService.error(res.message || 'Có lỗi xảy ra', '');
-                    }
-                },
-            );
-    }
-
-    prepareDeleteJob() {
-        this._documentRepo.checkPermissionAllowDeleteShipment(this.jobId)
-            .subscribe((value: boolean) => {
-                if (value) {
-                    this.deleteJob();
-                } else {
-                    this.info403Popup.show();
-                }
-            });
-    }
-
-    deleteJob() {
-        this._progressRef.start();
-        this._documentRepo.checkMasterBillAllowToDelete(this.jobId)
-            .pipe(
-                catchError(this.catchError),
-                finalize(() => this._progressRef.complete())
-            ).subscribe(
-                (res: any) => {
-                    if (res) {
-                        this.confirmDeleteJobPopup.show();
-                    } else {
-                        this.canNotDeleteJobPopup.show();
-                    }
-                },
-            );
+        super(_sortService, _store, _spinner, _progressService, _toastService, _documentRepo);
     }
 
     gotoDetail(id: string) {
@@ -223,27 +48,6 @@ export class AirImportHBLComponent extends AppList implements OnInit {
             );
     }
 
-    onDeleteJob() {
-        this._progressRef.start();
-        this._documentRepo.deleteMasterBill(this.jobId)
-            .pipe(
-                catchError(this.catchError),
-                finalize(() => {
-                    this._progressRef.complete();
-                    this.confirmDeleteJobPopup.hide();
-                })
-            ).subscribe(
-                (respone: CommonInterface.IResult) => {
-                    if (respone.status) {
-
-                        this._toastService.success(respone.message, 'Delete Success !');
-
-                        this.gotoList();
-                    }
-                },
-            );
-    }
-
     gotoList() {
         this._router.navigate(["home/documentation/air-import"]);
     }
@@ -251,52 +55,6 @@ export class AirImportHBLComponent extends AppList implements OnInit {
     gotoCreate() {
         this._router.navigate([`/home/documentation/air-import/${this.jobId}/hbl/new`]);
     }
-
-    selectHBL(hbl: HouseBill) {
-        if (!this.selectedHbl || !!this.selectedHbl && this.selectedHbl.id !== hbl.id) {
-            this.selectedHbl = new CsTransactionDetail(hbl);
-
-            // * Get container, Job detail, Surcharge with hbl id, JobId.
-            this._store.dispatch(new fromShareBussiness.GetDetailHBLSuccessAction(hbl));
-            this._store.dispatch(new fromShareBussiness.GetContainersHBLAction({ hblid: hbl.id }));
-            this._store.dispatch(new fromShareBussiness.GetProfitHBLAction(this.selectedHbl.id));
-
-            switch (this.selectedTabSurcharge) {
-                case 'BUY':
-                    this._store.dispatch(new fromShareBussiness.GetBuyingSurchargeAction({ type: 'BUY', hblId: this.selectedHbl.id }));
-                    break;
-                case 'SELL':
-                    this._store.dispatch(new fromShareBussiness.GetSellingSurchargeAction({ type: 'SELL', hblId: this.selectedHbl.id }));
-                    break;
-                case 'OBH':
-                    this._store.dispatch(new fromShareBussiness.GetOBHSurchargeAction({ type: 'OBH', hblId: this.selectedHbl.id }));
-                    break;
-                default:
-                    break;
-            }
-        }
-    }
-
-    onSelectTabSurcharge(tabName: string) {
-        this.selectedTabSurcharge = tabName;
-
-        if (!!this.selectedHbl) {
-            switch (this.selectedTabSurcharge) {
-                case 'BUY':
-                    this._store.dispatch(new fromShareBussiness.GetBuyingSurchargeAction({ type: 'BUY', hblId: this.selectedHbl.id }));
-                    break;
-                case 'SELL':
-                    this._store.dispatch(new fromShareBussiness.GetSellingSurchargeAction({ type: 'SELL', hblId: this.selectedHbl.id }));
-                    break;
-                case 'OBH':
-                    this._store.dispatch(new fromShareBussiness.GetOBHSurchargeAction({ type: 'OBH', hblId: this.selectedHbl.id }));
-                    break;
-                default:
-                    break;
-            }
-        }
-    }
-
 
     onSelectTab(tabName: string) {
         switch (tabName) {
@@ -313,28 +71,6 @@ export class AirImportHBLComponent extends AppList implements OnInit {
                 this._router.navigate([`home/documentation/air-import/${this.jobId}`], { queryParams: { tab: 'FILES' } });
                 break;
         }
-    }
-
-    previewPLsheet(currency: string) {
-        let hblid = "00000000-0000-0000-0000-000000000000";
-        if (!!this.selectedHbl) {
-            hblid = this.selectedHbl.id;
-        }
-        this._documentRepo.previewSIFPLsheet(this.jobId, hblid, currency)
-            .pipe(catchError(this.catchError))
-            .subscribe(
-                (res: any) => {
-                    this.dataReport = res;
-                    if (this.dataReport != null && res.dataSource.length > 0) {
-                        setTimeout(() => {
-                            this.previewPopup.frm.nativeElement.submit();
-                            this.previewPopup.show();
-                        }, 1000);
-                    } else {
-                        this._toastService.warning('There is no data to display preview');
-                    }
-                },
-            );
     }
 
     duplicateConfirm() {
