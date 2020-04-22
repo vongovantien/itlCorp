1087d4cc4490a5d20fbbfbd9c16670c3ba7d72f8
irene.tam <irene.tam@itl.com>
2020-04-22 14:14:08 +0700

    add profile config

--- WebAPI/@build-helper/publish-service.bat
+++ WebAPI/@build-helper/publish-service.bat
@@ -4,4 +4,5 @@ FOR /f "delims=" %%a IN ('call profile\ini.bat profile\environment.ini %2 %1') D
 
 if not exist "%val%_%3" mkdir "%val%_%3"
 
-call dotnet publish "..\%1\%1"  -c Release --output="%val%_%3"
+call dotnet publish "..\eFMS.API.SystemWeb\%1\%1"  -c Release --output="%val%_%3"
+call echo f | xcopy "profile\%2\appsettings.json" "%val%_%3\appsettings.json" /f /y 
