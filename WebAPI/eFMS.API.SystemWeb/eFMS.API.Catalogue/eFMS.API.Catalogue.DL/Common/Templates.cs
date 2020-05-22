67df81a441aa270cb054536d820aa6ee996aff1f
irene.tam <irene.tam@itl.com>
2020-05-22 17:29:03 +0700

    fix bug import charge voucher

--- WebAPI/eFMS.API.SystemWeb/eFMS.API.Catalogue/eFMS.API.Catalogue.DL/Common/Templates.cs
+++ WebAPI/eFMS.API.SystemWeb/eFMS.API.Catalogue/eFMS.API.Catalogue.DL/Common/Templates.cs
@@ -20,6 +20,7 @@
         }
         public struct CatChargeDefaultAccount
         {
+            public static string ExelImportFileName = "VoucherTypeAccount";
             public struct NameCaching
             {
                 public static string ListName = "ChargeDefaultAccounts";
