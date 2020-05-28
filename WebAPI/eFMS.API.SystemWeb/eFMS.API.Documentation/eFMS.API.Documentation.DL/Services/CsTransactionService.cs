e4a13f01b9584e3a4b191d5928060a53ea86e16e
irene.tam <irene.tam@itl.com>
2020-05-28 15:45:20 +0700

    fix bug sea export not required MBL No

--- WebAPI/eFMS.API.SystemWeb/eFMS.API.Documentation/eFMS.API.Documentation.DL/Services/CsTransactionService.cs
+++ WebAPI/eFMS.API.SystemWeb/eFMS.API.Documentation/eFMS.API.Documentation.DL/Services/CsTransactionService.cs
@@ -1674,7 +1674,7 @@ namespace eFMS.API.Documentation.DL.Services
         public ResultHandle ImportCSTransaction(CsTransactionEditModel model)
         {
             IQueryable<CsTransactionDetail> detailTrans = csTransactionDetailRepo.Get(x => x.JobId == model.Id && x.ParentId == null);
-            if (string.IsNullOrEmpty(model.Mawb) && detailTrans.Select(x => x.Id).Count() > 0)
+            if (string.IsNullOrEmpty(model.Mawb) && detailTrans.Select(x => x.Id).Count() > 0 && model.TransactionType != "SFE" && model.TransactionType != "SLE")
                 return new ResultHandle { Status = false, Message = "This shipment did't have MBL No. You can't import or duplicate it." };
 
             var transaction = GetDefaultJob(model);
