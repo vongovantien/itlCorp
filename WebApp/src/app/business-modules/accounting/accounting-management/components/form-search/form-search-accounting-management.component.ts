faf94c51aaeb58a39d6378301350103104aa4558
DoanVanThuong <doanvanthuong196@gmail.com>
2020-05-28 17:08:15 +0700

    ++ load ds partner, user trong form search

--- WebApp/src/app/business-modules/accounting/accounting-management/components/form-search/form-search-accounting-management.component.ts
+++ WebApp/src/app/business-modules/accounting/accounting-management/components/form-search/form-search-accounting-management.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AppForm } from 'src/app/app.form';
+import { CatalogueRepo, SystemRepo } from '@repositories';
+import { CommonEnum } from '@enums';
+import { Partner, User } from '@models';
 
 @Component({
     selector: 'form-search-accounting-management',
@@ -7,9 +10,37 @@ import { AppForm } from 'src/app/app.form';
 })
 
 export class AccountingManagementFormSearchComponent extends AppForm implements OnInit {
-    constructor() {
-        super()
+
+    status: CommonInterface.INg2Select[] = [
+        { id: 'New', text: 'New' },
+        { id: 'Issued Invoice', text: 'Issued Invoice' },
+        { id: 'Issued Voucher', text: 'Issued Voucher' },
+    ];
+
+    constructor(
+        private _catalogueRepo: CatalogueRepo,
+        private _systemRepo: SystemRepo
+    ) {
+        super();
     }
 
-    ngOnInit() { }
+    ngOnInit() {
+        this.loadPartnerList();
+        this.loadUserList();
+    }
+
+    loadPartnerList() {
+        this._catalogueRepo.getPartnersByType(CommonEnum.PartnerGroupEnum.ALL)
+            .subscribe(
+                (partners: Partner[]) => {
+                }
+            );
+    }
+
+    loadUserList() {
+        this._systemRepo.getSystemUsers({ active: true }).subscribe(
+            (users: User) => {
+            }
+        );
+    }
 }
