b6b74391315c55b556928787fe9883cb157d0b16
irene.tam <irene.tam@itl.com>
2020-06-13 10:41:45 +0700

    commit

--- WebAPI/eFMSWindowService/Program.cs
+++ WebAPI/eFMSWindowService/Program.cs
@@ -16,32 +16,31 @@ namespace eFMSWindowService
         static void Main()
         {
             //#if DEBUG
-            //If the mode is in debugging
-            //create a new service instance
-            //Service1 myService = new Service1();
-            SendMailToARDepartmentService myService = new SendMailToARDepartmentService();
-            //call the start method - this will start the Timer.
-            myService.Start();
-            //Set the Thread to sleep
-            Thread.Sleep(60000);
-            //Call the Stop method-this will stop the Timer.
-            myService.Stop();
+            ////If the mode is in debugging
+            ////create a new service instance
+            ////Service1 myService = new Service1();
+            //SendMailToARDepartmentService myService = new SendMailToARDepartmentService();
+            ////call the start method - this will start the Timer.
+            //myService.Start();
+            ////Set the Thread to sleep
+            //Thread.Sleep(60000);
+            ////Call the Stop method-this will stop the Timer.
+            //myService.Stop();
             //#else
-            //The following is the default code - You may fine tune
-            //the code to create one instance of the service on the top
-            //and use the instance variable in both debug and release mode
+            ////The following is the default code - You may fine tune
+            ////the code to create one instance of the service on the top
+            ////and use the instance variable in both debug and release mode
 
-            //ServiceBase[] ServicesToRun;
-            //ServicesToRun = new ServiceBase[]
-            //{
-            //    //new Service1(),
-            //    new UpdateExchangeRate(),
-            //    new UpdateCurrentStatusOfJobService(),
-            //    new UpdateStatusAuthorization(),
-            //    new SendMailToARDepartmentService()
-            //};
-            //ServiceBase.Run(ServicesToRun);
-//#endif
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new UpdateExchangeRate(),
+                new UpdateCurrentStatusOfJobService(),
+                new UpdateStatusAuthorization(),
+                new SendMailToARDepartmentService()
+            };
+            ServiceBase.Run(ServicesToRun);
+            //#endif
 
         }
     }
