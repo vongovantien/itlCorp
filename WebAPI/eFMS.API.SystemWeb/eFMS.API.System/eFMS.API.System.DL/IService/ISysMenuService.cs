d322d5f6e8ae7cf58e37da81ba1ab0b3ea05ea3d
DoanVanThuong <doanvanthuong196@gmail.com>
2020-07-06 16:10:43 +0700

    ++ get list service theo permission cua user

--- WebAPI/eFMS.API.SystemWeb/eFMS.API.System/eFMS.API.System.DL/IService/ISysMenuService.cs
+++ WebAPI/eFMS.API.SystemWeb/eFMS.API.System/eFMS.API.System.DL/IService/ISysMenuService.cs
@@ -1,4 +1,6 @@
-﻿using eFMS.API.System.DL.Models;
+﻿
+using eFMS.API.Common.Globals;
+using eFMS.API.System.DL.Models;
 using eFMS.API.System.DL.ViewModels;
 using eFMS.API.System.Service.Models;
 using ITL.NetCore.Connection.BL;
@@ -10,5 +12,6 @@ namespace eFMS.API.System.DL.IService
     public interface ISysMenuService : IRepositoryBaseCache<SysMenu, SysMenuModel>
     {
         List<MenuUserModel> GetMenus(string userId, Guid officeId);
+        List<CommonData> GetListService();
     }
 }
