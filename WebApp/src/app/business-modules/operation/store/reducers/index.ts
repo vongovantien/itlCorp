d5d07fbc67098eee678e33a21dafc57b184843cd
DoanVanThuong <kenny.thuong@itlvn.com>
2020-08-19 18:06:26 +0700

    ++ thêm action cho ops

--- WebApp/src/app/business-modules/operation/store/reducers/index.ts
+++ WebApp/src/app/business-modules/operation/store/reducers/index.ts
@@ -9,6 +9,9 @@ export const OperationState = createFeatureSelector<IOperationState>('operations
 
 export const getOperationState = createSelector(OperationState, (state: IOperationState) => state);
 export const getOperationTransationState = createSelector(OperationState, (state: IOperationState) => state && state.transaction);
+export const getOperationTransationListShipment = createSelector(OperationState, (state: IOperationState) => state && state.transaction.opsTransations);
+export const getOperationTransationDataSearch = createSelector(OperationState, (state: IOperationState) => state && state.transaction.dataSearch);
+export const getOperationTransationLoadingState = createSelector(OperationState, (state: IOperationState) => state && state.transaction.isLoading);
 
 export const reducers: ActionReducerMap<IOperationState> = {
     transaction: opsReducer,
