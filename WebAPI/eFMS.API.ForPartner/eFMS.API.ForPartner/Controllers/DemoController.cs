708e1afc8f61b6363992e64e73578d36a5b5a8f8
samuel.an <samuel.an@logtechub.com>
2020-08-26 14:31:25 +0700

    ++ add project

---
+++ WebAPI/eFMS.API.ForPartner/eFMS.API.ForPartner/Controllers/DemoController.cs
@@ -0,0 +1,20 @@
+﻿using eFMS.API.ForPartner.Infrastructure.Middlewares;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eFMS.API.ForPartner.Controllers
+{
+    [ApiController]
+    [ApiVersion("1.0")]
+    [MiddlewareFilter(typeof(LocalizationMiddleware))]
+    [Route("api/v{version:apiVersion}/{lang}/[controller]")]
+    public class DemoController : ControllerBase
+    {
+        /// <summary>
+        /// Get all value
+        /// </summary>
+        public IActionResult GetValue()
+        {
+            return Ok("Ok");
+        }
+    }
+}
---
+++ WebAPI/eFMS.API.ForPartner/eFMS.API.ForPartner/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Linq;
+
+namespace eFMS.API.ForPartner
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var appSettings = JObject.Parse(File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json")));
+            var environmentValue = appSettings["Environment"].ToString();
+
+            var webHostBuilder = CreateWebHostBuilder(args);
+
+            if (!String.IsNullOrEmpty(environmentValue))
+            {
+                webHostBuilder.UseEnvironment(environmentValue);
+            }
+
+            var host = webHostBuilder.Build();
+            host.Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
