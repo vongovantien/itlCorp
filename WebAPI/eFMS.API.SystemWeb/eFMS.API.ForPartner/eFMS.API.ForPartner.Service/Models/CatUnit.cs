9645b7acd41b8fe4460d9554839a5fa287c030dd
DoanVanThuong <kenny.thuong@itlvn.com>
2020-09-03 14:36:55 +0700

    ++ update DataContext cho API ForPartner

---
+++ WebAPI/eFMS.API.SystemWeb/eFMS.API.ForPartner/eFMS.API.ForPartner.Service/Models/CatUnit.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace eFMS.API.ForPartner.Service.Models
+{
+    public partial class CatUnit
+    {
+        public short Id { get; set; }
+        public string Code { get; set; }
+        public string UnitNameVn { get; set; }
+        public string UnitNameEn { get; set; }
+        public string UnitType { get; set; }
+        public string DescriptionEn { get; set; }
+        public string DescriptionVn { get; set; }
+        public string UserCreated { get; set; }
+        public DateTime? DatetimeCreated { get; set; }
+        public string UserModified { get; set; }
+        public DateTime? DatetimeModified { get; set; }
+        public bool? Active { get; set; }
+        public DateTime? InactiveOn { get; set; }
+    }
+}
