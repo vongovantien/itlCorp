d5b703c615c384c2953a7f0fbb17487ae4f6117b
DoanVanThuong <kenny.thuong@itlvn.com>
2020-09-21 09:16:21 +0700

    ++ charge accoutingTotalView model

---
+++ WebAPI/eFMS.API.SystemWeb/eFMS.API.Accounting/eFMS.API.Accounting.DL/Models/ChargeAccountingMngtTotalViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace eFMS.API.Accounting.DL.Models
+{
+    public class ChargeAccountingMngtTotalViewModel
+    {
+        public List<ChargeOfAccountingManagementModel> Charges { get; set; }
+        public decimal? TotalAmountVnd { get; set; }
+        public decimal? TotalAmountVat { get; set; }
+        public decimal? TotalAmount { get; set; } //TotalAmountVND + TotalAmountVatVND
+    }
+}
