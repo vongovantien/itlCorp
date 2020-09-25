9eb36c3e8298343f566e21bd8adbb81faced2d3f
ThienNguyen <intern.lth@itlvn.com>
2020-09-25 10:47:40 +0700

    commit 25-09-2020 uat/fixbug/14511-fix-default-commodities-is-asperbill

--- WebApp/src/app/business-modules/share-business/components/form-create-air/form-create-air.component.ts
+++ WebApp/src/app/business-modules/share-business/components/form-create-air/form-create-air.component.ts
@@ -252,6 +252,12 @@ export class ShareBusinessFormCreateAirComponent extends AppForm implements OnIn
             .subscribe(
                 (commodities: Commodity[]) => {
                     this.commodities = this.utility.prepareNg2SelectData(commodities, 'code', 'commodityNameEn');
+                    if (!!this.commodities.length && this.type === 'import') {
+                        const asPerBill = this.commodities.filter(e => e.text === 'AS PER BILL');
+                        if (!!asPerBill) {
+                            this.commodity.setValue(asPerBill);
+                        }
+                    }
                 },
             );
     }
