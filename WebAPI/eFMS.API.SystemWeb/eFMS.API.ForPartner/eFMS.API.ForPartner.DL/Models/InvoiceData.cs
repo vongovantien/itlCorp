7b60a2abe526693f4533e2c1fb43eec2e93d61c5
DoanVanThuong <kenny.thuong@itlvn.com>
2020-09-30 11:54:49 +0700

    ++ model cho Bravo API

---
+++ WebAPI/eFMS.API.SystemWeb/eFMS.API.ForPartner/eFMS.API.ForPartner.DL/Models/InvoiceData.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace eFMS.API.ForPartner.DL.Models
+{
+    public class InvoiceData
+    {
+        public string InvocieNo { get; set; }
+        public string InvoiceDate { get; set; }
+        public string ReferenceNo { get; set; }
+        public string PartnerCode { get; set; }
+        public string SerieNo { get; set; }
+        List<ChargeInvoice> Charges { get; set; }
+
+    }
+}
