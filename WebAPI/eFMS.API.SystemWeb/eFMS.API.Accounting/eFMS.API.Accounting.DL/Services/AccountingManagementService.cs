4e5bd58f5d903d1eea107a9bc28daf91e1d64264
DoanVanThuong <kenny.thuong@itlvn.com>
2020-10-08 17:17:48 +0700

    -+ remove sync voucher to accounting controller

--- WebAPI/eFMS.API.SystemWeb/eFMS.API.Accounting/eFMS.API.Accounting.DL/Services/AccountingManagementService.cs
+++ WebAPI/eFMS.API.SystemWeb/eFMS.API.Accounting/eFMS.API.Accounting.DL/Services/AccountingManagementService.cs
@@ -1904,25 +1904,5 @@ namespace eFMS.API.Accounting.DL.Services
 
             settlementPaymentRepo.Update(settlement, x => x.Id == settlement.Id,false);
         }
-
-        public HandleState SyncVoucher(Guid voucherId)
-        {
-            HandleState result = new HandleState();
-
-            AccAccountingManagement voucher = DataContext.Get(x => x.Id == voucherId && x.Type == AccountingConstants.ACCOUNTING_VOUCHER_TYPE)?.FirstOrDefault();
-
-            if (voucher == null)
-            {
-                return new HandleState(stringLocalizer[AccountingLanguageSub.MSG_DATA_NOT_FOUND].Value);
-            }
-
-            voucher.UserModified = currentUser.UserID;
-            voucher.DatetimeModified = DateTime.Now;
-            voucher.LastSyncDate = DateTime.Now;
-
-            result = DataContext.Update(voucher, x => x.Id == voucherId);
-
-            return result;
-        }
     }
 }
