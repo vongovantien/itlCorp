8e2a98add0b67fef1787fde261eb3b017999dd80
samuel.an <samuel.an@logtechub.com>
2020-10-09 10:33:01 +0700

    ++ update permission

--- WebAPI/eFMS.API.SystemWeb/eFMS.API.Documentation/eFMS.API.Documentation.DL/Models/ModelUpdate.cs
+++ WebAPI/eFMS.API.SystemWeb/eFMS.API.Documentation/eFMS.API.Documentation.DL/Models/ModelUpdate.cs
@@ -7,5 +7,6 @@ namespace eFMS.API.Documentation.DL.Models
         public string BillingOpsId { get; set; } //Sử dụng cho Service Logistic
         public string PersonInCharge { get; set; } //Sử dụng cho Sevice Documentation
         public string SaleManId { get; set; } //Sử dụng cho Logistic & Documentation        
+        public string[] SalemanIds { get; set; }
     }
 }
