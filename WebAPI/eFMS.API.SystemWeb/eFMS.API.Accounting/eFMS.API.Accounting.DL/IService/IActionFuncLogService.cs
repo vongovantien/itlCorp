b06e161464f841f882e48dc8e7b6ec8c3e4821fb
andy.hoa <andy.hoa@itlvn.com>
2020-10-12 16:46:41 +0700

    add model

---
+++ WebAPI/eFMS.API.SystemWeb/eFMS.API.Accounting/eFMS.API.Accounting.DL/IService/IActionFuncLogService.cs
@@ -0,0 +1,16 @@
+﻿using eFMS.API.Accounting.DL.Models;
+using eFMS.API.Accounting.Service.Models;
+using ITL.NetCore.Common;
+using ITL.NetCore.Connection.BL;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eFMS.API.Accounting.DL.IService
+{
+    public interface IActionFuncLogService : IRepositoryBase<SysActionFuncLog, SysActionFuncLogModel>
+    {
+        HandleState AddActionFuncLog(SysActionFuncLogModel model);
+        HandleState UpdateActionFuncLog(SysActionFuncLogModel model);
+    }
+}
