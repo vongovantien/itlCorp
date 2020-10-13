20250de82520649a02f8751acc95f580acda5469
DoanVanThuong <kenny.thuong@itlvn.com>
2020-10-13 14:50:06 +0700

    + custom bad request, authoriz cho forPartner

---
+++ WebAPI/eFMS.API.SystemWeb/eFMS.API.ForPartner/eFMS.API.ForPartner/Infrastructure/Extensions/CustomUnauthorizedResult.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace eFMS.API.ForPartner.Infrastructure.Extensions
+{
+    public class CustomUnauthorizedResult : JsonResult
+    {
+        public CustomUnauthorizedResult(string message)
+        : base(new CustomError(message))
+        {
+            StatusCode = StatusCodes.Status401Unauthorized;
+        }
+    }
+
+    
+}
