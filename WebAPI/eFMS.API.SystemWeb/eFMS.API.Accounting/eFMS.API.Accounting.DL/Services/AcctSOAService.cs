5dc52b91d3f30ac00f4286ad5dc3200f5de58f12
andy.hoa <andy.hoa@itlvn.com>
2020-10-19 16:08:51 +0700

    fix bug preview SOA bị lỗi

--- WebAPI/eFMS.API.SystemWeb/eFMS.API.Accounting/eFMS.API.Accounting.DL/Services/AcctSOAService.cs
+++ WebAPI/eFMS.API.SystemWeb/eFMS.API.Accounting/eFMS.API.Accounting.DL/Services/AcctSOAService.cs
@@ -2531,7 +2531,7 @@ namespace eFMS.API.Accounting.DL.Services
                 soaCharge.CustomNo = _customNo;
                 soaCharge.JobNo = _jobNo;
                 soaCharge.CdCode = cdNote?.Code;
-                soaCharge.Docs = string.Join("\r\n", grpInvCdNoteByHbl.Where(w => w.Hblid == charge.Hblid).Select(s => !string.IsNullOrEmpty(s.InvoiceNo) ? s.InvoiceNo : s.CdNote).Distinct()); //Ưu tiên: Invoice No >> CD Note Code
+                soaCharge.Docs = string.Join("\r\n", grpInvCdNoteByHbl.ToList().Where(w => w.Hblid == charge.Hblid).Select(s => !string.IsNullOrEmpty(s.InvoiceNo) ? s.InvoiceNo : s.CdNote).Distinct()); //Ưu tiên: Invoice No >> CD Note Code
 
                 soaCharges.Add(soaCharge);
             }
