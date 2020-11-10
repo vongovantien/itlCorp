04abc5ef8b18252045441ce00ec32ea80977ed32
DoanVanThuong <kenny.thuong@itlvn.com>
2020-11-10 14:20:34 +0700

    += update header notifycation

--- WebApp/src/app/master-page/header/header.component.ts
+++ WebApp/src/app/master-page/header/header.component.ts
@@ -63,7 +63,6 @@ export class HeaderComponent implements OnInit, AfterViewInit {
             this.getOfficeDepartGroupCurrentUser(this.currenUser);
         }
 
-
         const indentitySub = this._identity.getUserProfile()
             .subscribe(
                 (user: any) => {
@@ -113,17 +112,18 @@ export class HeaderComponent implements OnInit, AfterViewInit {
         this.getListNotification();
 
         this._signalRService.listenEvent("NotificationWhenChange", (data: SysUserNotification) => {
-            if (data) {
+            if (data && data.userId === this.currenUser.id) {
+                this._toast.info(data.description, data.title, { progressBar: true, positionClass: 'toast-top-right', enableHtml: true, easeTime: 3000 });
                 this.getListNotification();
             }
         });
 
         this._signalRService.listenEvent("SendMessageToAllClient", (data: any) => {
-            this._toast.info(`You have a new message ${data}`, 'Infomation');
+            this._toast.info(`You have a new message ${data}`, 'Infomation', { progressBar: true, positionClass: 'toast-top-right', enableHtml: true });
         });
 
         this._signalRService.listenEvent("SendMessageToClient", (data: any) => {
-            this._toast.info(`You have a new message ${data}`, 'Infomation');
+            this._toast.info(`You have a new message ${data}`, 'Infomation', { progressBar: true, positionClass: 'toast-top-right', enableHtml: true });
         });
 
         this._signalRService.listenEvent("BroadCastMessage", (data: any) => {
