c555ddfb6f485815edf949f4fabdd8616dc7d1ce
luudinhhuy <intern.lth002@itlvn.com>
2020-12-03 16:09:53 +0700

    create receipt

--- WebApp/src/app/business-modules/accounting/account-receivable-payable/customer-payment/components/form-create-receipt/form-create-receipt.component.ts
+++ WebApp/src/app/business-modules/accounting/account-receivable-payable/customer-payment/components/form-create-receipt/form-create-receipt.component.ts
@@ -1,14 +1,99 @@
-import { Component, OnInit } from '@angular/core';
+import {  Component, Output , EventEmitter} from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { SystemConstants } from '@constants';
+import { CommonEnum } from '@enums';
+import { Currency, Customer, User } from '@models';
+import { Store } from '@ngrx/store';
+import {CatalogueRepo, SystemRepo } from '@repositories';
+import { GetCatalogueCurrencyAction, getCatalogueCurrencyState, IAppState } from '@store';
+import { Observable } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+import { AppForm } from 'src/app/app.form';
 
 @Component({
     selector: 'customer-payment-form-create-receipt',
     templateUrl: './form-create-receipt.component.html',
 })
-export class ARCustomerPaymentFormCreateReceiptComponent implements OnInit {
-
-    constructor() { }
-
+export class ARCustomerPaymentFormCreateReceiptComponent extends AppForm {
+    @Output() onChangeCurrency: EventEmitter<Currency> = new EventEmitter<Currency>();
+    form: FormGroup;
+    customerName: AbstractControl;
+    date: AbstractControl;
+    paymentReferenceNo: AbstractControl;
+    salesMan: AbstractControl;
+    agreement: AbstractControl;
+    paidAmount: AbstractControl;
+    type: AbstractControl;
+    cusAdvanceAmount: AbstractControl;
+    finalPaidAmount: AbstractControl;
+    balance: AbstractControl;
+    currency: AbstractControl;
+    paymentDate: AbstractControl;
+    exchangeRate: AbstractControl;
+    bankAcountNo: AbstractControl;
+    currencyList: Currency[];
+    constructor(
+        private _fb: FormBuilder,
+        private _store: Store<IAppState>,
+    ) {
+        super();
+    }
     ngOnInit() {
+        this._store.dispatch(new GetCatalogueCurrencyAction());
+        this.initFormSettlement();
+        this.getCurrency();
+
+    }
+    initFormSettlement() {
+        this.form = this._fb.group({
+            'customerName': [],
+            'date': [{ startDate: new Date(), endDate: new Date() }, Validators.required],
+            'paymentReferenceNo': [],
+            'salesMan': [],
+            'agreement': [],
+            'paidAmount': [],
+            'type': [],
+            'cusAdvanceAmount': [],
+            'finalPaidAmount': [],
+            'balance': [],
+            'currency': [],
+            'paymentDate': [],
+            'exchangeRate': [],
+            'bankAcountNo': [],
+        });
+        this.customerName = this.form.controls['customerName'];
+        this.date = this.form.controls['date'];
+        this.paymentReferenceNo = this.form.controls['paymentReferenceNo'];
+        this.salesMan = this.form.controls['salesMan'];
+        this.agreement = this.form.controls['agreement'];
+        this.paidAmount = this.form.controls['paidAmount'];
+        this.type = this.form.controls['type'];
+        this.cusAdvanceAmount = this.form.controls['cusAdvanceAmount'];
+        this.finalPaidAmount = this.form.controls['finalPaidAmount'];
+        this.balance = this.form.controls['balance'];
+        this.currency = this.form.controls['currency'];
+        // this.currency.valueChanges.pipe(
+        //     // tslint:disable-next-line:no-any
+        //     map((data: any) => data)
+        // ).subscribe((value: Currency) => {
+        //     if (!!value) {
+        //         this.onChangeCurrency.emit(value);
+        //     }
+        // });
+        this.paymentDate = this.form.controls['paymentDate'];
+        this.exchangeRate = this.form.controls['exchangeRate'];
+        this.bankAcountNo = this.form.controls['bankAcountNo'];
+    }
+    getCurrency() {
+        this._store.select(getCatalogueCurrencyState)
+            .pipe(catchError(this.catchError))
+            .subscribe(
+                // tslint:disable-next-line:no-any
+                (data: any) => {
+                    this.currencyList = data || [];
+                    this.currency.setValue("VND");
+                },
+            );
     }
 
 }
--- WebApp/src/app/business-modules/accounting/account-receivable-payable/customer-payment/components/receipt-summary/receipt-summary.component.ts
+++ WebApp/src/app/business-modules/accounting/account-receivable-payable/customer-payment/components/receipt-summary/receipt-summary.component.ts
@@ -1,11 +1,74 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { RoutingConstants } from '@constants';
+import { TrialOfficialOtherModel } from '@models';
+import { NgProgress } from '@ngx-progressbar/core';
+import { AccountingRepo } from '@repositories';
+import { SortService } from '@services';
+import { catchError, finalize } from 'rxjs/operators';
+import { AppList } from 'src/app/app.list';
 
 @Component({
     selector: 'customer-payment-receipt-summary',
     templateUrl: './receipt-summary.component.html',
 })
-export class ARCustomerPaymentReceiptSummaryComponent implements OnInit {
-    constructor() { }
+export class ARCustomerPaymentReceiptSummaryComponent extends AppList implements OnInit {
+    trialOfficialList: TrialOfficialOtherModel[] = [];
 
-    ngOnInit(): void { }
+    constructor(
+        private _sortService: SortService,
+        private _progressService: NgProgress,
+        private _router: Router,
+        private _accountingRepo: AccountingRepo,
+    ) {
+        super();
+        this._progressRef = this._progressService.ref();
+        this.requestSort = this.sortTrialOfficalList;
+        this.requestList = this.getPagingList;
+    }
+    ngOnInit() {
+
+        this.headers = [
+            { title: 'Total Summary', field: 'paymentrefno', sortable: true },
+            { title: 'Total Debit', field: 'customername', sortable: true },
+            { title: 'Total OBH', field: 'paymentamount', sortable: true },
+            { title: 'Total ADV', field: 'currency', sortable: true },
+            { title: 'Total Credit', field: 'billingdate', sortable: true },
+        ];
+
+    }
+
+    sortTrialOfficalList(sortField: string, order: boolean) {
+        this.trialOfficialList = this._sortService.sort(this.trialOfficialList, sortField, order);
+    }
+
+    getPagingList() {
+        this._progressRef.start();
+        this.isLoading = true;
+
+        this._accountingRepo.receivablePaging(this.page, this.pageSize, Object.assign({}, this.dataSearch))
+            .pipe(
+                catchError(this.catchError),
+                finalize(() => {
+                    this._progressRef.complete();
+                    this.isLoading = false;
+                })
+            ).subscribe(
+                (res: CommonInterface.IResponsePaging) => {
+                    this.trialOfficialList = (res.data || []).map((item: TrialOfficialOtherModel) => new TrialOfficialOtherModel(item));
+                    this.totalItems = res.totalItems;
+                },
+            );
+    }
+
+    //
+    viewDetail(agreementId: string) {
+        this._router.navigate([`${RoutingConstants.ACCOUNTING.ACCOUNT_RECEIVABLE_PAYABLE}/`], {
+            queryParams: {
+                agreementId: agreementId,
+
+            }
+        });
+    }
 }
+
