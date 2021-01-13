f6cc7c72d0d42e6d36206d53704d8eaa75964d06
ITL\lynne.loc <lynne.loc@itlvn.com>
2021-01-13 18:29:20 +0700

    #update multi house

--- WebAPI/eFMS.API.SystemWeb/eFMS.API.Documentation/eFMS.API.Documentation.DL/Services/CsTransactionService.cs
+++ WebAPI/eFMS.API.SystemWeb/eFMS.API.Documentation/eFMS.API.Documentation.DL/Services/CsTransactionService.cs
@@ -831,6 +831,7 @@ namespace eFMS.API.Documentation.DL.Services
                         if (masDetail == null)
                         {
                             masDetail = transaction.Where(x => x.BookingNo == mblNo).FirstOrDefault();
+                            masDetail = masDetail == null ? transaction.FirstOrDefault() : masDetail;
                         }
                         jobNo = masDetail?.JobNo.ToString();
                         id = masDetail?.Id.ToString();
