b4a8f17b384e9d1c89e58c1fb480b906fd6c3553
samuel.an <samuel.an@logtechub.com>
2021-01-15 11:50:31 +0700

    ++ fix title

--- WebAPI/eFMS.API.SystemWeb/eFMS.API.Catalogue/eFMS.API.Catalogue.DL/Services/CatPartnerService.cs
+++ WebAPI/eFMS.API.SystemWeb/eFMS.API.Catalogue/eFMS.API.Catalogue.DL/Services/CatPartnerService.cs
@@ -308,6 +308,9 @@ namespace eFMS.API.Catalogue.DL.Services
             string body = string.Empty;
             string address = webUrl.Value.Url + "/en/#/" + url + partner.Id;
 
+            string title = string.Empty;
+            title = partner.PartnerType == "Customer" ? "Customer" : "Agent";
+
 
             linkEn = "You can <a href='" + address + "'> click here </a>" + "to view detail.";
             linkVn = "Bạn click <a href='" + address + "'> vào đây </a>" + "để xem chi tiết.";
@@ -315,7 +318,7 @@ namespace eFMS.API.Catalogue.DL.Services
 
             body = string.Format(@"<div style='font-family: Calibri; font-size: 12pt; color:#004080;'> Dear Accountant/AR Team, " + " </br> </br>" +
 
-              "<i> You have a Customer Approval request from " + EnNameCreatetor + " as info below </i> </br>" +
+              "<i> You have a " + title +" Approval request from " + EnNameCreatetor + " as info below </i> </br>" +
               "<i> Bạn có một yêu cầu xác duyệt khách hàng từ " + EnNameCreatetor + " với thông tin như sau: </i> </br> </br>" +
 
               "\t  Customer ID  / <i> Mã Agent:</i> " + "<b>" + partner.AccountNo + "</b>" + "</br>" +
