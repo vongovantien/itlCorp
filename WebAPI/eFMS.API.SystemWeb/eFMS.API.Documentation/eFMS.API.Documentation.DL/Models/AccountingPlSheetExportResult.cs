58d38f9e533603f965cfbf000074f0777710fbf0
samuel.an <samuel.an@logtechub.com>
2021-01-28 16:54:59 +0700

    ++ fix bug

--- WebAPI/eFMS.API.SystemWeb/eFMS.API.Documentation/eFMS.API.Documentation.DL/Models/AccountingPlSheetExportResult.cs
+++ WebAPI/eFMS.API.SystemWeb/eFMS.API.Documentation/eFMS.API.Documentation.DL/Models/AccountingPlSheetExportResult.cs
@@ -39,7 +39,6 @@ namespace eFMS.API.Documentation.DL.Models
         public string PmVoucherNo { get; set; }
         public string Service { get; set; }
         public string UserExport { get; set; }
-        public Guid ChargeId { get; set; }
         public decimal? FinalExchangeRate { get; set; }
         public DateTime? ExchangeDate { get; set; }
         public string CurrencyId { get; set; }
