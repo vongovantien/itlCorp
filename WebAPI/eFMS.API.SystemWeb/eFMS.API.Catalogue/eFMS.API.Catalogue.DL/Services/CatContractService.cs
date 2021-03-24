3ae21738a58e36bcde57ad1797b448e39d7bd0d5
samuel.an <samuel.an@logtechub.com>
2021-03-24 14:10:26 +0700

    ++ update

--- WebAPI/eFMS.API.SystemWeb/eFMS.API.Catalogue/eFMS.API.Catalogue.DL/Services/CatContractService.cs
+++ WebAPI/eFMS.API.SystemWeb/eFMS.API.Catalogue/eFMS.API.Catalogue.DL/Services/CatContractService.cs
@@ -472,7 +472,7 @@ namespace eFMS.API.Catalogue.DL.Services
             var specialActions = _user.UserMenuPermission.SpecialActions;
             if (specialActions.Count > 0)
             {
-                if (specialActions.FirstOrDefault(x => x.Action == "DetailAgreement").IsAllow == true)
+                if (specialActions.FirstOrDefault(x => x.Action == "DetailAgreement").IsAllow == true || result.UserCreated == currentUser.UserID || result.SaleManId == currentUser.UserID)
                 {
                     data.ViewDetail = true;
                 }
