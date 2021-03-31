b481f433aae6dad34b48bf6af0f14892caf32081
andy.hoa <andy.hoa@itlvn.com>
2021-03-31 17:43:55 +0700

    gỡ bỏ PKG(S) preview form SI; gỡ bỏ tính công nợ Advance/Settlement/SOA; fix bug check exist shipment

--- WebAPI/eFMS.API.SystemWeb/eFMS.API.Accounting/eFMS.API.Accounting/Controllers/AcctAdvancePaymentController.cs
+++ WebAPI/eFMS.API.SystemWeb/eFMS.API.Accounting/eFMS.API.Accounting/Controllers/AcctAdvancePaymentController.cs
@@ -186,11 +186,11 @@ namespace eFMS.API.Accounting.Controllers
                 return BadRequest(new ResultHandle { Status = false, Message = stringLocalizer[LanguageSub.DO_NOT_HAVE_PERMISSION].Value });
             }
 
-            if (hs.Success)
+            /*if (hs.Success)
             {
                 // Tính công nợ
                 CalculatorReceivableAdvancePayment(model.AdvanceRequests);
-            }
+            }*/ //[31/03/2021]
 
             var message = HandleError.GetMessage(hs, Crud.Insert);
             ResultHandle result = new ResultHandle { Status = hs.Success, Message = stringLocalizer[message].Value, Data = model };
@@ -255,11 +255,11 @@ namespace eFMS.API.Accounting.Controllers
                 return BadRequest(new ResultHandle { Status = false, Message = stringLocalizer[LanguageSub.DO_NOT_HAVE_PERMISSION].Value });
             }
 
-            if (hs.Success)
+            /*if (hs.Success)
             {
                 // Sau khi xóa thành công >> tính lại công nợ dựa vào list request của advance no
                 CalculatorReceivableAdvancePayment(advanceRequests);
-            }
+            }*/ //[31/03/2021]
 
             var message = HandleError.GetMessage(hs, Crud.Delete);
             ResultHandle result = new ResultHandle { Status = hs.Success, Message = stringLocalizer[message].Value };
@@ -388,11 +388,11 @@ namespace eFMS.API.Accounting.Controllers
                 return BadRequest(new ResultHandle { Status = false, Message = stringLocalizer[LanguageSub.DO_NOT_HAVE_PERMISSION].Value });
             }
 
-            if (hs.Success)
+            /*if (hs.Success)
             {
                 // Tính công nợ
                 CalculatorReceivableAdvancePayment(model.AdvanceRequests);
-            }
+            }*/ //[31/03/2021]
 
             var message = HandleError.GetMessage(hs, Crud.Update);
             ResultHandle result = new ResultHandle { Status = hs.Success, Message = stringLocalizer[message].Value, Data = model };
@@ -571,7 +571,7 @@ namespace eFMS.API.Accounting.Controllers
                 }
                 
                 // Tính công nợ
-                CalculatorReceivableAdvancePayment(model.AdvanceRequests);
+               // CalculatorReceivableAdvancePayment(model.AdvanceRequests); [31/03/2021]
                 
                 return Ok(result);
             }
