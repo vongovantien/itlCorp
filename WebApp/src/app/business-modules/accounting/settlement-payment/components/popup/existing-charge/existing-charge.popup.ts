bd513e02c3b7b1f95b0c0a9a51a738875eaa2e16
ITL\lynne.loc <lynne.loc@itlvn.com>
2021-04-14 10:59:20 +0700

    #update text align right, cal amount when update amount textbox, update pic when view detail

--- WebApp/src/app/business-modules/accounting/settlement-payment/components/popup/existing-charge/existing-charge.popup.ts
+++ WebApp/src/app/business-modules/accounting/settlement-payment/components/popup/existing-charge/existing-charge.popup.ts
@@ -362,6 +362,23 @@ export class SettlementExistingChargePopupComponent extends PopupBase {
         });
     }
 
+    onBlurAmountCharge(event: any, hblId: string){
+        if(event.target.name === 'amount'){
+            this.shipments.filter((shipment: any) => shipment.hblId === hblId).map((shipment: any) => {
+                shipment.totalNetVND = shipment.chargeSettlements.reduce((net: number, charge: Surcharge) => net += charge.amountVnd, 0);
+                shipment.totalVND = shipment.totalNetVND + shipment.totalVATVND;
+            });
+            this.getTotalAmountVND();
+        }
+        if(event.target.name === 'vat'){
+            this.shipments.filter((shipment: any) => shipment.hblId === hblId).map((shipment: any) => {
+                shipment.totalVATVND = shipment.chargeSettlements.reduce((net: number, charge: Surcharge) => net += charge.vatAmountVnd, 0);
+                shipment.totalVND = shipment.totalNetVND + shipment.totalVATVND;
+            });
+            this.getTotalAmountVND();
+        }
+    }
+
     setAdvanceToSurcharge(shipment: any, advanceNo: string) {
         shipment.chargeSettlements.forEach(element => {
             element.advanceNo = advanceNo;
