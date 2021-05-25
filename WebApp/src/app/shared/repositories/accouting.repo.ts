3fe17cf990f0828af8db70f43ea82425279694b6
kenny.thuong <kenny.thuong@itlvn.com>
2021-05-25 11:09:39 +0700

    += query ds tam ung theo so hbl trog settle

--- WebApp/src/app/shared/repositories/accouting.repo.ts
+++ WebApp/src/app/shared/repositories/accouting.repo.ts
@@ -325,8 +325,8 @@ export class AccountingRepo {
         );
     }
 
-    getAdvanceOfShipment(jobNo: string, settleCode: string = null): Observable<any> {
-        return this._api.get(`${environment.HOST.ACCOUNTING}/api/${this.VERSION}/en-US/AcctAdvancePayment/GetAdvancesOfShipment`, { jobId: jobNo, settleCode: settleCode }).pipe(
+    getAdvanceOfShipment(jobNo: string, hblId: string, settleCode: string = null): Observable<any> {
+        return this._api.get(`${environment.HOST.ACCOUNTING}/api/${this.VERSION}/en-US/AcctAdvancePayment/GetAdvancesOfShipment`, { jobId: jobNo, hblid: hblId, settleCode: settleCode }).pipe(
             map((data: any) => data)
         );
     }
