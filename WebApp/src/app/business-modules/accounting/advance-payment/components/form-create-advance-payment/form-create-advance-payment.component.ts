c7f3d9635012fd565ab4f230c56c482737e5199c
kenny.thuong <kenny.thuong@itlvn.com>
2021-06-23 23:11:40 +0700

    += bank accountName

--- WebApp/src/app/business-modules/accounting/advance-payment/components/form-create-advance-payment/form-create-advance-payment.component.ts
+++ WebApp/src/app/business-modules/accounting/advance-payment/components/form-create-advance-payment/form-create-advance-payment.component.ts
@@ -188,7 +188,7 @@ export class AdvancePaymentFormCreateComponent extends AppForm {
 
     setBankInfoForPayee(payee: Partner) {
         this.bankAccountNo.setValue(payee.bankAccountNo);
-        this.bankAccountNo.setValue(payee.bankAccountName);
+        this.bankAccountName.setValue(payee.bankAccountName);
         this.bankName.setValue(payee.bankName);
     }
 }
