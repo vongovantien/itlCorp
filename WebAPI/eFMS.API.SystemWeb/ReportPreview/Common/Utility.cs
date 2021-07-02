9cee3948460a3fbd9e4ded2158047da787a6f8a8
kenny.thuong <kenny.thuong@itlvn.com>
2021-07-02 15:09:45 +0700

    += ghi log report preview pdf

--- WebAPI/eFMS.API.SystemWeb/ReportPreview/Common/Utility.cs
+++ WebAPI/eFMS.API.SystemWeb/ReportPreview/Common/Utility.cs
@@ -216,5 +216,30 @@ namespace ReportPerview.Common
         {
             return obj.GetType().GetProperty(pro).GetValue(obj, null);
         }
+
+        public static void WriteToFile(string logName, string Message)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory + "\\Logs";
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            string filepath = AppDomain.CurrentDomain.BaseDirectory + "\\Logs\\" + logName + "_" + DateTime.Now.Date.ToShortDateString().Replace('/', '_') + ".txt";
+            if (!File.Exists(filepath))
+            {
+                // Create a file to write to.   
+                using (StreamWriter sw = File.CreateText(filepath))
+                {
+                    sw.WriteLine(Message);
+                }
+            }
+            else
+            {
+                using (StreamWriter sw = File.AppendText(filepath))
+                {
+                    sw.WriteLine(Message);
+                }
+            }
+        }
     }
 }
