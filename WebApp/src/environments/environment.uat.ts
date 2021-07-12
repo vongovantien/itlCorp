c4176c092c0ac6b1d07a60525b51d14ac5598433
kenny.thuong <kenny.thuong@itlvn.com>
2021-07-12 20:20:10 +0700

    += fix env uat

--- WebApp/src/environments/environment.uat.ts
+++ WebApp/src/environments/environment.uat.ts
@@ -5,8 +5,7 @@ export const environment = {
     local: false,
     HOST: {
         WEB_URL: "uat-api-efms.itlvn.com",
-        // ACCOUNTING: "uat-api-efms.itlvn.com/Accounting",
-        ACCOUNTING: "localhost:44373",
+        ACCOUNTING: "uat-api-efms.itlvn.com/Accounting",
         DOCUMENTATION: "uat-api-efms.itlvn.com/Documentation",
         CATALOGUE: "uat-api-efms.itlvn.com/Catalogue",
         OPERATION: "uat-api-efms.itlvn.com/Operation",
