ee734d24d95b9788be288f85e5b20699f6ddc41a
kenny.thuong <kenny.thuong@itlvn.com>
2021-08-03 17:43:50 +0700

    ++ mode option

--- WebApp/src/app/shared/common/permision-button/permission-button.component.ts
+++ WebApp/src/app/shared/common/permision-button/permission-button.component.ts
@@ -12,24 +12,23 @@ import { Observable } from 'rxjs';
 export class AppPermissionButtonComponent extends AppForm implements OnInit {
     @Input() title: string;
     @Input() set type(type: string) { this._type = type; }
-
     @Input() set class(c: string) { this._class = c; }
-
     @Input() set icon(i: string) { this._icon = i; }
+    @Input() user: string;
+    @Input() set mode(mode: string) { this._mode = mode; };
 
     get type() { return this._type; }
-
     get class() { return this._class; }
-
     get icon() { return this._icon; }
+    get mode() { return this._mode };
 
     @Output() onClick: EventEmitter<any> = new EventEmitter<any>();
 
-    @Input() user: string;
 
     private _type: string = "add";
     private _class: string;
     private _icon: string = 'la la-plus';
+    private _mode: string;
 
     menuPermission: Observable<SystemInterface.IUserPermission>;
 
