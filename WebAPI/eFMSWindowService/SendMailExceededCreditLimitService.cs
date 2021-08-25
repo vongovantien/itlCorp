42d3707d11416dc21cc88a629a5d9b47f34ab334
Daniel.Khoa <daniel.khoa@itlvn.com>
2021-08-25 19:21:30 +0700

    prop/16009-AR-Send-Mail-Prealert-Credit-Rate

--- WebAPI/eFMSWindowService/SendMailExceededCreditLimitService.cs
+++ WebAPI/eFMSWindowService/SendMailExceededCreditLimitService.cs
@@ -28,8 +28,8 @@ namespace eFMSWindowService
             // Tạo 1 timer từ libary System.Timers
             _timer = new Timer();
             // Execute mỗi ngày vào lúc 8h sáng
-            //_timer.Interval = _scheduleTime.Subtract(DateTime.Now).TotalSeconds * 1000;
-            _timer.Interval = 30000;
+            _timer.Interval = _scheduleTime.Subtract(DateTime.Now).TotalSeconds * 1000;
+            //_timer.Interval = 30000;
             // Những gì xảy ra khi timer đó dc tick
             _timer.Elapsed += Timer_Elapsed;
             // Enable timer
@@ -48,96 +48,108 @@ namespace eFMSWindowService
                 FileHelper.WriteToFile("SendMailExceededCreditLimit", "Service send mail exceeded credit limit is recall at " + DateTime.Now);
                 using (eFMSTestEntities db = new eFMSTestEntities())
                 {
-                    string subject = "Thông báo Khách hàng VƯỢT HẠN MỨC CÔNG NỢ";
-                    string headerBody = @"<strong>Dear All,</strong> </br></br>"
-                                      + @"<p>Dưới đây là danh sách Khách hàng <strong style='color: red;'>vượt hạn mức công nợ</strong> do anh/chị phụ trách:</p>"
-                                      + @"<p><i>Below is the list of your customers which has been <strong>exceeded credit limit:</strong></i></p>";
-
-                    string footerBody = @"</br></br>"
-                                      + @"<p>* Nếu hạn mức tín dụng vượt quá 120% thì phần mềm sẽ tự động khóa booking</p>"
-                                      + @"<p><i>* If the credit limit has been exceeded 120%, the system will be block booking automatically</i></p>"
-                                      + @"<p>Anh/chị vui lòng liên hệ Khách hàng đề nghị thanh toán ngay phần công nợ <strong>sắp/ đã vượt hạn mức</strong> để tránh ảnh hưởng đến việc nhận booking mới từ khách hàng theo quy định Công ty.</p>"
-                                      + @"<p><i>Please contact your customer to request settle the outstanding which has been <strong>exceeded credit limit</strong> immediately in order to avoid affecting their new booking according to the Company policy.</i></p>"
-                                      + @"</br></br>Many thanks and Best Regards,";
-
-                    string tableBody = @"<table style='width: 100%; border: 1px solid #dddddd; border-collapse: collapse;'>"
-                                     + @"<tr>"
-                                     + @"<th style='border: 1px solid #dddddd; border-collapse: collapse;'>STT <br/> <i style='font-weight: normal'>No.</i></th>"
-                                     + @"<th style='border: 1px solid #dddddd; border-collapse: collapse;'>Chi nhánh <br/> <i style='font-weight: normal'>Branch</i></th>"
-                                     + @"<th style='border: 1px solid #dddddd; border-collapse: collapse;'>Khách hàng <br/> <i style='font-weight: normal'>Customer</i></th>"
-                                     + @"<th style='border: 1px solid #dddddd; border-collapse: collapse;'>Hạn mức được cấp <br/> <i style='font-weight: normal'>Credit Limit</i></th>"
-                                     + @"<th style='border: 1px solid #dddddd; border-collapse: collapse;'>% Hạn mức công nợ hiện tại <br/> <i style='font-weight: normal'>% Current Credit</i></th>"
-                                     + @"<th style='border: 1px solid #dddddd; border-collapse: collapse;'>Số công nợ hiện tại <br/> <i style='font-weight: normal'>Current Debts</i></th>"
-                                     + @"</tr>"
-                                     + @"[content]"
-                                     + @"</table>";
-
-                    var exceededCreditLimits = new List<ExceededCreditLimit>
+                    var dt = db.Database.SqlQuery<sp_GetOverDuePaymentCredit_Result>("[dbo].[sp_GetOverDuePaymentCredit]").ToList();
+                    var dtGrp = dt.GroupBy(x => new
                     {
-                        new ExceededCreditLimit{ Office = "ITL HCM CORP", Customer = "HO GUOM", CreditLimit = 10000, CurrentCredit = 150, CurrentDebts = 19056000 },
-                        new ExceededCreditLimit{ Office = "ITL DAD CORP", Customer = "TAN THIEN DIA", CreditLimit = 20000, CurrentCredit = 570, CurrentDebts = 1190000 },
-                        new ExceededCreditLimit{ Office = "ITL HAN CORP", Customer = "VINATEX", CreditLimit = 30000, CurrentCredit = 90, CurrentDebts = 19005400 },
-                        new ExceededCreditLimit{ Office = "ITL HAN CORP", Customer = "HO GUOM", CreditLimit = 40000, CurrentCredit = 70, CurrentDebts = 1912000 },
-                        new ExceededCreditLimit{ Office = "ITL HAN CORP", Customer = "TRI VIET", CreditLimit = 50000, CurrentCredit = 80, CurrentDebts = 1096000 },
-                        new ExceededCreditLimit{ Office = "ITL DAD CORP", Customer = "VIETJET AIR", CreditLimit = 60000, CurrentCredit = 90, CurrentDebts = 189000 },
-                        new ExceededCreditLimit{ Office = "ITL HCM CORP", Customer = "DELTA CARGO", CreditLimit = 70000, CurrentCredit = 100, CurrentDebts = 790000 },
-                        new ExceededCreditLimit{ Office = "ITL HCM CORP", Customer = "TRANSVIET", CreditLimit = 80000, CurrentCredit = 120, CurrentDebts = 196000 },
-                        new ExceededCreditLimit{ Office = "ITL DAD CORP", Customer = "HO GUOM", CreditLimit = 90000, CurrentCredit = 130, CurrentDebts = 19000670 },
-                        new ExceededCreditLimit{ Office = "ITL DAD CORP", Customer = "TH-Sky Pacific", CreditLimit = 100000, CurrentCredit = 160, CurrentDebts = 13190000 },
-                        new ExceededCreditLimit{ Office = "ITL DAD CORP", Customer = "TH-Sky Pacific", CreditLimit = 110000, CurrentCredit = 280, CurrentDebts = 13590000 },
-                    };
+                        x.UserName,
+                        x.Email
+                    });
 
-                    var exceededCreditsMore70 = exceededCreditLimits.Where(x => x.CurrentCredit >= 70 && x.CurrentCredit < 100).OrderBy(o => o.CurrentCredit).ToList();
-                    var exceededCreditsMore100 = exceededCreditLimits.Where(x => x.CurrentCredit >= 100 && x.CurrentCredit < 120).OrderBy(o => o.CurrentCredit).ToList();
-                    var exceededCreditsMore120 = exceededCreditLimits.Where(x => x.CurrentCredit >= 120).ToList().OrderBy(o => o.CurrentCredit).ToList();
-
-                    StringBuilder content = new StringBuilder();
-                    var no = 1;
-
-                    if (exceededCreditsMore70.Count > 0)
+                    foreach (var item in dtGrp)
                     {
-                        content.Append(GetContentTableGroupCreditLimit(exceededCreditsMore70, 70, no, out no));
-                    }
+                        string subject = item.Key.UserName != null ? $"Thông báo Khách hàng VƯỢT HẠN MỨC CÔNG NỢ của sale {item.Key.UserName}" : "Thông báo Khách hàng VƯỢT HẠN MỨC CÔNG NỢ";
+                        string dear = item.Key.UserName != null ? $"<strong>Dear {item.Key.UserName},</strong> </br></br>" : "<strong>Dear All,</strong> </br></br>";
 
-                    if (exceededCreditsMore100.Count > 0)
-                    {
-                        content.Append(GetContentTableGroupCreditLimit(exceededCreditsMore100, 100, no, out no));
-                    }
+                        string headerBody = @"<strong>Dear All,</strong> </br></br>"
+                                          + @"<p>Dưới đây là danh sách Khách hàng <strong style='color: red;'>vượt hạn mức công nợ</strong> do anh/chị phụ trách:</p>"
+                                          + @"<p><i>Below is the list of your customers which has been <strong>exceeded credit limit:</strong></i></p>";
 
-                    if (exceededCreditsMore120.Count > 0)
-                    {
-                        content.Append(GetContentTableGroupCreditLimit(exceededCreditsMore120, 120, no, out no));
-                    }
+                        string footerBody = @"</br></br>"
+                                          + @"<p>* Nếu hạn mức tín dụng vượt quá 120% thì phần mềm sẽ tự động khóa booking</p>"
+                                          + @"<p><i>* If the credit limit has been exceeded 120%, the system will be block booking automatically</i></p>"
+                                          + @"<p>Anh/chị vui lòng liên hệ Khách hàng đề nghị thanh toán ngay phần công nợ <strong>sắp/ đã vượt hạn mức</strong> để tránh ảnh hưởng đến việc nhận booking mới từ khách hàng theo quy định Công ty.</p>"
+                                          + @"<p><i>Please contact your customer to request settle the outstanding which has been <strong>exceeded credit limit</strong> immediately in order to avoid affecting their new booking according to the Company policy.</i></p>"
+                                          + @"</br></br>Many thanks and Best Regards,";
 
-                    content.Append(@"<tr>");
-                    content.Append(@"<tr colspan='6'>&nbsp;</tr>");
-                    content.Append(@"</tr>");
+                        string tableBody = @"<table style='width: 100%; border: 1px solid #dddddd; border-collapse: collapse;'>"
+                                         + @"<tr>"
+                                         + @"<th style='border: 1px solid #dddddd; border-collapse: collapse;'>STT <br/> <i style='font-weight: normal'>No.</i></th>"
+                                         + @"<th style='border: 1px solid #dddddd; border-collapse: collapse;'>Chi nhánh <br/> <i style='font-weight: normal'>Branch</i></th>"
+                                         + @"<th style='border: 1px solid #dddddd; border-collapse: collapse;'>Khách hàng <br/> <i style='font-weight: normal'>Customer</i></th>"
+                                         + @"<th style='border: 1px solid #dddddd; border-collapse: collapse;'>Hạn mức được cấp <br/> <i style='font-weight: normal'>Credit Limit</i></th>"
+                                         + @"<th style='border: 1px solid #dddddd; border-collapse: collapse;'>% Hạn mức công nợ hiện tại <br/> <i style='font-weight: normal'>% Current Credit</i></th>"
+                                         + @"<th style='border: 1px solid #dddddd; border-collapse: collapse;'>Số công nợ hiện tại <br/> <i style='font-weight: normal'>Current Debts</i></th>"
+                                         + @"</tr>"
+                                         + @"[content]"
+                                         + @"</table>";
 
-                    tableBody = tableBody.Replace("[content]", content.ToString());
-                    string body = headerBody + tableBody + footerBody;
-                    body = string.Format("<div style='font-family: Calibri; font-size: 12pt; color: #004080'>{0}</div>", body);
-                    List<string> mail = new List<string> { "andy.hoa@itlvn.com" };
-                    List<string> emailBCCs = CommonData.EmailBCCs;
-                    if (exceededCreditLimits != null && exceededCreditLimits.Count > 0
-                        && (exceededCreditsMore70.Count > 0 || exceededCreditsMore100.Count > 0 || exceededCreditsMore120.Count > 0)
-                        && mail != null && mail.Count > 0)
-                    {
-                        var s = SendMailHelper.Send(subject, body, mail, null, null, emailBCCs);
-
-                        #region --- Ghi Log Send Mail ---
-                        var logSendMail = new sysSentEmailHistory
+                        var exceededCreditLimits = new List<ExceededCreditLimit>();
+                        foreach (var it in item)
                         {
-                            SentUser = SendMailHelper._emailFrom,
-                            Receivers = string.Join("; ", mail),
-                            Subject = subject,
-                            Sent = s,
-                            SentDateTime = DateTime.Now,
-                            Body = body,
-                            BCCs = string.Join("; ", emailBCCs)
-                        };
-                        var hsLogSendMail = db.sysSentEmailHistories.Add(logSendMail);
-                        var hsSc = db.SaveChanges();
-                        #endregion --- Ghi Log Send Mail ---
+                            var o = new ExceededCreditLimit()
+                            {
+                                Office = it.BranchName_EN,
+                                Customer = it.PartnerName_EN,
+                                CreditLimit = it.CreditLimit != null?it.CreditLimit:0,
+                                CurrentCredit = it.CreditRate,
+                                CurrentDebts = it.DebitAmount
+                            };
+
+                            exceededCreditLimits.Add(o);
+                        }
+
+                        var exceededCreditsMore70 = exceededCreditLimits.Where(x => x.CurrentCredit >= 70 && x.CurrentCredit < 100).OrderBy(o => o.CurrentCredit).ToList();
+                        var exceededCreditsMore100 = exceededCreditLimits.Where(x => x.CurrentCredit >= 100 && x.CurrentCredit < 120).OrderBy(o => o.CurrentCredit).ToList();
+                        var exceededCreditsMore120 = exceededCreditLimits.Where(x => x.CurrentCredit >= 120).ToList().OrderBy(o => o.CurrentCredit).ToList();
+
+                        StringBuilder content = new StringBuilder();
+                        var no = 1;
+
+                        if (exceededCreditsMore70.Count > 0)
+                        {
+                            content.Append(GetContentTableGroupCreditLimit(exceededCreditsMore70, 70, no, out no));
+                        }
+
+                        if (exceededCreditsMore100.Count > 0)
+                        {
+                            content.Append(GetContentTableGroupCreditLimit(exceededCreditsMore100, 100, no, out no));
+                        }
+
+                        if (exceededCreditsMore120.Count > 0)
+                        {
+                            content.Append(GetContentTableGroupCreditLimit(exceededCreditsMore120, 120, no, out no));
+                        }
+
+                        content.Append(@"<tr>");
+                        content.Append(@"<tr colspan='6'>&nbsp;</tr>");
+                        content.Append(@"</tr>");
+
+                        tableBody = tableBody.Replace("[content]", content.ToString());
+                        string body = dear + headerBody + tableBody + footerBody;
+                        body = string.Format("<div style='font-family: Calibri; font-size: 12pt; color: #004080'>{0}</div>", body);
+                        List<string> mail = new List<string> { item.Key.Email };
+                        List<string> emailBCCs = CommonData.EmailBCCs;
+                        if (exceededCreditLimits != null && exceededCreditLimits.Count > 0
+                            && (exceededCreditsMore70.Count > 0 || exceededCreditsMore100.Count > 0 || exceededCreditsMore120.Count > 0)
+                            && mail != null && mail.Count > 0)
+                        {
+                            var s = SendMailHelper.Send(subject, body, mail, null, null, emailBCCs);
+
+                            #region --- Ghi Log Send Mail ---
+                            var logSendMail = new sysSentEmailHistory
+                            {
+                                SentUser = SendMailHelper._emailFrom,
+                                Receivers = string.Join("; ", mail),
+                                Subject = subject,
+                                Sent = s,
+                                SentDateTime = DateTime.Now,
+                                Body = body,
+                                BCCs = string.Join("; ", emailBCCs)
+                            };
+                            var hsLogSendMail = db.sysSentEmailHistories.Add(logSendMail);
+                            var hsSc = db.SaveChanges();
+                            #endregion --- Ghi Log Send Mail ---
+                        }
                     }
                 }
 
