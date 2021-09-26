7127c2b25902b7e416b9e58cb0682a53767b87d6
hngtiendng <hngtiendng@gmail.com>
2021-09-26 22:53:03 +0700

    resolve conflict

--- WebAPI/eFMS.API.SystemWeb/eFMS.API.Accounting/eFMS.API.Accounting.DL/Models/AcctAdvanceRequestModel.cs
+++ WebAPI/eFMS.API.SystemWeb/eFMS.API.Accounting/eFMS.API.Accounting.DL/Models/AcctAdvanceRequestModel.cs
@@ -10,6 +10,7 @@ namespace eFMS.API.Accounting.DL.Models
         public DateTime? SettleDate { get; set; }
         public string Requester { get; set; }
         public string RequesterName { get; set; }
+        public DateTime? ServiceDate { get; set; }
         public string PaymentMethod { get; set; }
         public DateTime? DeadlinePayment { get; set; }    
         public string BankAccountNo { get; set; }
