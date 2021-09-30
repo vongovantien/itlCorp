d8f64e7c7d61e469e5858554b6d73c6fb771f5de
Doan Van Thuong <kenny.thuong@itlvn.com>
2021-09-30 16:11:11 +0700

    Prod/16305 partner mail active agreement and partner

---
+++ WebAPI/eFMS.API.SystemWeb/eFMS.API.Catalogue/eFMS.API.Catalogue.DAL/Models/SysEmailSetting.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace eFMS.API.Catalogue.Service.Models
+{
+    public partial class SysEmailSetting
+    {
+        public int Id { get; set; }
+        public string EmailType { get; set; }
+        public string EmailInfo { get; set; }
+        public DateTime? CreateDate { get; set; }
+        public DateTime? ModifiedDate { get; set; }
+        public int DeptId { get; set; }
+        public string UserModified { get; set; }
+        public string UserCreated { get; set; }
+    }
+}
