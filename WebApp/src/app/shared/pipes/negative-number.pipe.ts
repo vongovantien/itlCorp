542fcaf23116ec0cf1da3d0c95e88571be225d66
kenny.thuong <kenny.thuong@itlvn.com>
2021-10-10 23:18:26 +0700

    += update negative number

--- WebApp/src/app/shared/pipes/negative-number.pipe.ts
+++ WebApp/src/app/shared/pipes/negative-number.pipe.ts
@@ -6,20 +6,24 @@ import { DecimalPipe } from '@angular/common';
 
 export class NegativeNumberePipe extends DecimalPipe implements PipeTransform {
     transform(value: number, ...args: any[]): any {
-        if (!isNaN(value) && value < 0) {
-            if (!args.length) {
-                return `(${Math.abs(value)})`;
+        if (!isNaN(value)) {
+            if (value < 0) {
+                if (!args.length) {
+                    return `(${Math.abs(value)})`;
+                }
+                return `(${super.transform(Math.abs(value), args[0], '')})`;
             }
-            return `(${super.transform(Math.abs(value), args[0], '')})`;
-        }else if (!isNaN(value) && value === 0) {
-            if (!args.length) {
+            else if (value === 0) {
+                if (!args.length) {
+                    return value;
+                }
                 return `-`;
+            } else {
+                if (!args.length)
+                    return `${Math.abs(value)}`;
+                return `${super.transform(Math.abs(value), args[0], '')}`;
             }
-            return `-`;
-        }else {
-            if (!args.length)
-                return `${Math.abs(value)}`;
-            return `${super.transform(Math.abs(value), args[0], '')}`;
         }
+        return value;
     }
 }
