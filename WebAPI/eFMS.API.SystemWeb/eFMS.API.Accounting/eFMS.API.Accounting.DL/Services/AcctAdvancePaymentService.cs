8faaad23c6273b501542fe25792035d1cef240f4
dane.dung <dane.dung@itlvn.com>
2021-10-19 09:31:54 +0700

    update DES

--- WebAPI/eFMS.API.SystemWeb/eFMS.API.Accounting/eFMS.API.Accounting.DL/Services/AcctAdvancePaymentService.cs
+++ WebAPI/eFMS.API.SystemWeb/eFMS.API.Accounting/eFMS.API.Accounting.DL/Services/AcctAdvancePaymentService.cs
@@ -3645,7 +3645,7 @@ namespace eFMS.API.Accounting.DL.Services
                     Hbl = s.First().Hbl,
                     Mbl = s.First().Mbl,
                     CustomNo = s.First().CustomNo,
-                    RequestNote=s.First().RequestNote,
+                    RequestNote= string.Join(";", s.Select(x => x.Description)),
                 });
             foreach (var request in groupJobByHbl)
             {
