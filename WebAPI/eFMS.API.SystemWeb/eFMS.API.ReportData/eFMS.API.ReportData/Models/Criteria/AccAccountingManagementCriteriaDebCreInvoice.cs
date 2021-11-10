084fa2bdf1e4847fca4656cb9a17e230bb0df2d3
ITL\Daniel.khoa <daniel.khoa@itlvn.com>
2021-11-10 14:41:17 +0700

    Update

---
+++ WebAPI/eFMS.API.SystemWeb/eFMS.API.ReportData/eFMS.API.ReportData/Models/Criteria/AccAccountingManagementCriteriaDebCreInvoice.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eFMS.API.ReportData.Models.Criteria
+{
+    public class AccAccountingManagementCriteriaDebCreInvoice
+    {
+        public string ReferenceNos { get; set; }
+        public string PartnerId { get; set; }
+        public string CreatorId { get; set; }
+        public string InvoiceStatus { get; set; }
+        public string VoucherType { get; set; }
+        public string TypeOfAcctManagement { get; set; }
+        public DateTime? FromIssuedDate { get; set; }
+        public DateTime? ToIssuedDate { get; set; }
+
+        public DateTime? FromExportDate { get; set; }
+        public DateTime? ToExportDate { get; set; }
+        public DateTime? FromAccountingDate { get; set; }
+        public DateTime? ToAccountingDate { get; set; }
+    }
+}
