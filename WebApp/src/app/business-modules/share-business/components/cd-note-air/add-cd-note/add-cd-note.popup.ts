29837f395fd15a3943ecb929c91b1326489d676c
Daniel.Khoa <daniel.khoa@itlvn.com>
2021-11-19 13:44:28 +0700

    Update ex rate

--- WebApp/src/app/business-modules/share-business/components/cd-note-air/add-cd-note/add-cd-note.popup.ts
+++ WebApp/src/app/business-modules/share-business/components/cd-note-air/add-cd-note/add-cd-note.popup.ts
@@ -377,6 +377,7 @@ export class ShareBussinessCdNoteAddAirPopupComponent extends PopupBase {
                                 if (res.status) {
                                     this._toastService.success(res.message);
                                     let checkSoa = this.listCharges.find(x=>x.soano !== "" && x.soano !== null);
+                                    if(!checkSoa){checkSoa = this.listCharges.find(x=>x.paySoano !== "" && x.paySoano !== null);}
                                     if(checkSoa){
                                         this._toastService.warning("Vui lòng cập nhật SOA");
                                     }
