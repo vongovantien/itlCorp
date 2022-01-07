7ea78d5bb4306a967d4eabfc4204ca30a8039753
kenny.thuong <kenny.thuong@itlvn.com>
2022-01-07 15:52:30 +0700

    += update link job air sea form tạo mới job

--- WebApp/src/app/business-modules/operation/job-mangement/components/form-create/form-create-job.component.ts
+++ WebApp/src/app/business-modules/operation/job-mangement/components/form-create/form-create-job.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { CatalogueRepo, DocumentationRepo, SystemRepo } from '@repositories';
-import { CommodityGroup, Customer, PortIndex, User } from '@models';
+import { CommodityGroup, Customer, PortIndex, User, LinkAirSeaModel } from '@models';
 import { IShareBussinessState } from '@share-bussiness';
 import { GetCataloguePortAction, getCataloguePortState, GetCatalogueCarrierAction, GetCatalogueAgentAction, getCatalogueCarrierState, getCatalogueAgentState, GetCatalogueCommodityGroupAction, getCatalogueCommodityGroupState } from '@store';
 import { CommonEnum } from '@enums';
@@ -55,8 +55,8 @@ export class JobManagementFormCreateComponent extends AppForm implements OnInit 
     users: Observable<User[]>;
     salesmans: Observable<User[]>;
 
-    shipmentNo: string = null;
-    shipmentInfo: string = '';
+    jobLinkAirSeaNo: string = '';
+    jobLinkAirSeaInfo: LinkAirSeaModel;
 
     displayFieldPort: CommonInterface.IComboGridDisplayField[] = [
         { field: 'code', label: 'Port Code' },
@@ -206,15 +206,23 @@ export class JobManagementFormCreateComponent extends AppForm implements OnInit 
         } else {
             this._documentRepo.getASTransactionInfo(this.mblno.value, this.hwbno.value, this.productService.value, this.serviceMode.value)
                 .pipe(catchError(this.catchError))
-                .subscribe((res: any) => {
-                    if (!!res) {
-                        this.shipmentNo = res.jobNo;
-                        if (!!res.jobNo) {
-                            this.shipmentInfo = res.jobNo;
-                        } else {
-                            this.shipmentInfo = null;
-                            this._toaster.warning("There's no valid Air/Sea Shipment to display. Please check again!");
+                .subscribe((res: LinkAirSeaModel) => {
+                    if (!!res?.jobNo) {
+                        this.jobLinkAirSeaNo = res.jobNo;
+                        this.jobLinkAirSeaInfo = res;
+                        if (!this.customerId.value && !!res.customerId) {
+                            this.customerId.setValue(res.customerId);
                         }
+                        if (!this.salemansId.value && !!res.salemanId) {
+                            this.salemansId.setValue(res.salemanId);
+                        }
+                        if (!this.serviceDate.value?.startDate && !!res.serviceDate) {
+                            this.serviceDate.setValue({ startDate: new Date(res.serviceDate), endDate: new Date(res.serviceDate) });
+                        }
+                    }
+                    else {
+                        this.jobLinkAirSeaNo = null;
+                        this._toaster.warning("There's no valid Air/Sea Shipment to display. Please check again!");
                     }
                 });
         }
