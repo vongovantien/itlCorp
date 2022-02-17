2d4a89a7da34f5c86d2901f21baf64d5e2f62075
kenny.thuong <kenny.thuong@itlvn.com>
2022-02-17 11:48:40 +0700

    ++ ghi log trong acc service middleware

--- WebAPI/eFMS.API.SystemWeb/eFMS.API.Accounting/eFMS.API.Accounting/Infrastructure/Middlewares/ErrorHandlingMiddleware.cs
+++ WebAPI/eFMS.API.SystemWeb/eFMS.API.Accounting/eFMS.API.Accounting/Infrastructure/Middlewares/ErrorHandlingMiddleware.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.IO;
 using System.Net;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using eFMS.API.Accounting.Infrastructure.Common;
+using eFMS.API.Common.Helpers;
 using Microsoft.AspNetCore.Http;
 using Newtonsoft.Json;
 
@@ -30,7 +33,7 @@ namespace eFMS.API.Accounting.Infrastructure.Middlewares
             }
         }
 
-        private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+        private static async Task HandleExceptionAsync(HttpContext context, Exception exception)
         {
             // if it's not one of the expected exception, set it to 500
             var code = HttpStatusCode.InternalServerError;
@@ -40,25 +43,39 @@ namespace eFMS.API.Accounting.Infrastructure.Middlewares
             else if (exception is HttpRequestException) code = HttpStatusCode.BadRequest;
             else if (exception is UnauthorizedAccessException) code = HttpStatusCode.Unauthorized;
 
-
-            return WriteExceptionAsync(context, exception, code);
+            await WriteExceptionAsync(context, exception, code);
         }
 
-        private static Task WriteExceptionAsync(HttpContext context, Exception exception, HttpStatusCode code)
+        private static async Task WriteExceptionAsync(HttpContext context, Exception exception, HttpStatusCode code)
         {
             var response = context.Response;
             response.ContentType = "application/json";
             response.StatusCode = (int)code;
-            return response.WriteAsync(JsonConvert.SerializeObject(new
+
+            if (!context.Request.Body.CanSeek)
+            {
+                context.Request.EnableBuffering();
+            }
+
+            context.Request.Body.Position = 0;
+            var reader = new StreamReader(context.Request.Body,  Encoding.UTF8);
+            string body = await reader.ReadToEndAsync().ConfigureAwait(false);
+            context.Request.Body.Position = 0;
+
+            var log = JsonConvert.SerializeObject(new
             {
                 error = new ResponseModel
                 {
                     Code = (int)code,
-                    Message = exception.Message,
-                    Exception = exception.GetType().Name,
-                    Success = false
+                    Message = exception.GetType().Name + " " + exception.Message + " in " + exception.Source,
+                    Exception = body,
+                    Success = false,
+                    Path = context.Request.Path
                 }
-            }));
+            });
+
+            new LogHelper("eFMS_Log_Exception", log);
+            await response.WriteAsync(log);
         }
     }
 }
