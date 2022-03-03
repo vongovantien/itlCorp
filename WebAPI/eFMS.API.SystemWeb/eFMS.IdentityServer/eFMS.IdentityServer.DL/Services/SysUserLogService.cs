b5821a544edbc83e20a52c6a5ca03abda504b5af
kenny.thuong <kenny.thuong@itlvn.com>
2022-03-03 15:53:25 +0700

    += check ldap inactive

--- WebAPI/eFMS.API.SystemWeb/eFMS.IdentityServer/eFMS.IdentityServer.DL/Services/SysUserLogService.cs
+++ WebAPI/eFMS.API.SystemWeb/eFMS.IdentityServer/eFMS.IdentityServer.DL/Services/SysUserLogService.cs
@@ -1,21 +1,50 @@
 ﻿using AutoMapper;
 using eFMS.API.System.DL.Models;
+using eFMS.IdentityServer.DL.Helpers;
+using eFMS.IdentityServer.DL.Infrastructure;
 using eFMS.IdentityServer.DL.IService;
 using eFMS.IdentityServer.DL.Models;
 using eFMS.IdentityServer.Service.Models;
 using ITL.NetCore.Connection.BL;
 using ITL.NetCore.Connection.EF;
+using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
+using System.DirectoryServices;
 using System.Text;
 
 namespace eFMS.IdentityServer.DL.Services
 {
     public class SysUserLogService : RepositoryBase<SysUserLog, SysUserLogModel>, ISysUserLogService
     {
-        public SysUserLogService(IContextBase<SysUserLog> repository, IMapper mapper) : base(repository, mapper)
-        {
+        private readonly LDAPConfig ldap;
 
+        public SysUserLogService(IContextBase<SysUserLog> repository,
+            IMapper mapper,
+            IOptions<LDAPConfig> ldapConfig
+            ) : base(repository, mapper)
+        {
+            ldap = ldapConfig.Value;
+        }
+
+
+        public SearchResult GetLDAPInfo(List<string> usernames, out List<string> userInactive)
+        {
+            SearchResult ldapInfo = null;
+            LdapAuthentication Ldap = new LdapAuthentication();
+            Ldap.Path = ldap.LdapPaths[0];
+            userInactive = new List<string>();
+            foreach (var username in usernames)
+            {
+                ldapInfo = Ldap.GetLDAPInfo(ldap.Domain, username);
+
+                if(ldapInfo == null)
+                {
+                    userInactive.Add(username);
+                }
+            }
+
+            return ldapInfo;
         }
     }
 }
