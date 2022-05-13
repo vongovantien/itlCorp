358968256d0a0cb4a0e8895cb32f457ff495470b
ITL\Daniel.khoa <daniel.khoa@itlvn.com>
2022-05-13 14:04:12 +0700

    Update logs

--- WebAPI/eFMS.API.SystemWeb/eFMS.API.Documentation/eFMS.API.Documentation.DL/Services/OpsTransactionService.cs
+++ WebAPI/eFMS.API.SystemWeb/eFMS.API.Documentation/eFMS.API.Documentation.DL/Services/OpsTransactionService.cs
@@ -29,6 +29,7 @@ using System.Linq.Expressions;
 using Newtonsoft.Json;
 using eFMS.API.Documentation.DL.Helpers;
 using System.Data.SqlClient;
+using System.Threading;
 
 namespace eFMS.API.Documentation.DL.Services
 {
@@ -2194,8 +2195,6 @@ namespace eFMS.API.Documentation.DL.Services
                 {
                     if (charges != null && charges.Count() > 0)
                     {
-                        logMessage = string.Format(" *  \n [CHARGES]: {0} * ", JsonConvert.SerializeObject(charges));
-                        new LogHelper("[EFMS_OPSTRANSACTIONSERVICE_CHARGEFROMREPLICATE]", logMessage);
                         foreach (var charge in charges)
                         {
                             CsShipmentSurcharge surcharge = new CsShipmentSurcharge();
@@ -2282,7 +2281,7 @@ namespace eFMS.API.Documentation.DL.Services
                     }
                     if (surchargeAdds.Count > 0)
                     {
-                        logMessage = string.Format(" *  \n [SURCHARGE_ADD]: {0} * ", JsonConvert.SerializeObject(surchargeAdds));
+                        logMessage = string.Format(" *  \n [TIME]:{0}[SURCHARGE_ADD]: {1} * ",DateTime.Now.ToString("DD/MM/YYYY hh:mm:ss"), JsonConvert.SerializeObject(surchargeAdds));
                         new LogHelper("[EFMS_OPSTRANSACTIONSERVICE_CHARGEFROMREPLICATE]", logMessage);
                         surchargeRepository.Add(surchargeAdds, false);
                         var result = surchargeRepository.SubmitChanges();
