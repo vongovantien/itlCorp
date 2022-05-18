7ec925d00d511ef45973d433bc4567d6a7ac36a5
kenny.thuong <kenny.thuong@itlvn.com>
2022-05-18 18:31:14 +0700

    += update linq get overdue hóa đơn

--- WebAPI/eFMS.API.SystemWeb/eFMS.API.Accounting/eFMS.API.Accounting.DL/Services/AccAccountReceivableService.cs
+++ WebAPI/eFMS.API.SystemWeb/eFMS.API.Accounting/eFMS.API.Accounting.DL/Services/AccAccountReceivableService.cs
@@ -2633,22 +2633,23 @@ namespace eFMS.API.Accounting.DL.Services
             {
                 query = query.And(x => partnerIds.Contains(x.PartnerId));
             }
+            invoiceOverDue = accountingManagementRepo.Get(query);
+
             switch (type)
             {
                 case 1: // 1 - 15
-                    query = query.And(x => x.PaymentDueDate != null && (DateTime.Now.Date - x.PaymentDueDate.Value.Date).Days < 16 && (DateTime.Now.Date - x.PaymentDueDate.Value.Date).Days > 0);
+                    invoiceOverDue = invoiceOverDue.Where(x => x.PaymentDueDate != null && (DateTime.Now.Date - x.PaymentDueDate.Value.Date).Days < 16 && (DateTime.Now.Date - x.PaymentDueDate.Value.Date).Days > 0);
                     break;
                 case 2: // 15 - 30
-                    query = query.And(x => x.PaymentDueDate != null && (DateTime.Now.Date - x.PaymentDueDate.Value.Date).Days <= 30 && (DateTime.Now.Date - x.PaymentDueDate.Value.Date).Days > 15);
+                    invoiceOverDue = invoiceOverDue.Where(x => x.PaymentDueDate != null && (DateTime.Now.Date - x.PaymentDueDate.Value.Date).Days <= 30 && (DateTime.Now.Date - x.PaymentDueDate.Value.Date).Days > 15);
                     break;
                 case 3: // 30
-                    query = query.And(x => x.PaymentDueDate != null && (DateTime.Now.Date - x.PaymentDueDate.Value.Date).Days > 30);
+                    invoiceOverDue = invoiceOverDue.Where(x => (DateTime.Now.Date - x.PaymentDueDate.Value.Date).TotalDays > 30);
                     break;
                 default:
                     break;
             }
 
-            invoiceOverDue = accountingManagementRepo.Get(query);
             return invoiceOverDue;
         }
 
