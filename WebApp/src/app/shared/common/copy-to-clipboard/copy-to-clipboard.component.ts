9d116a8818ea9bcebc47234fdefafadb6f8bce42
ITL\intern.lth012 <intern.lth012@itlvn.com>
2022-06-15 20:09:01 -0700

    prod/17646 Copy to clipboard

---
+++ WebApp/src/app/shared/common/copy-to-clipboard/copy-to-clipboard.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Clipboard } from '@angular/cdk/clipboard';
+import { ToastrService } from 'ngx-toastr';
+
+@Component({
+  selector: 'app-copy-to-clipboard',
+  template:
+  `
+  <ng-container>
+    <span NoDbClick (click)="onClicked($event)">
+      <i class="la la-{{icon}} text-success"></i>
+    </span>
+  </ng-container>
+  `,
+})
+export class CopyToClipboardComponent implements OnInit {
+  @Input() set notification(alert: string) { this._notification = alert; }
+  @Input() set text(t: string) { this._text = t; }
+  @Input() set icon(i: string) { this._icon = i; }
+
+  get text() { return this._text; }
+  get icon() { return this._icon; }
+  get notification() { return this._notification}
+  private message: string = ' was copied';
+  
+  @Output() onClick: EventEmitter<any> = new EventEmitter<any>();
+
+  private _text: string = "";
+  private _icon: string = 'la la-copy';
+  private _notification: string;
+
+
+  constructor(
+      private clipboard: Clipboard,
+      private _toastService: ToastrService,
+  ){
+  }
+
+  ngOnInit(): void {
+  }
+
+  onClicked(e) {
+      this.onClick.emit(e);
+      this.clipboard.copy(this._text);
+      this._toastService.success(this._notification !== undefined ? this._notification: this._text + this.message);
+  }
+}
