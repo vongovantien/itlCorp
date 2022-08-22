eb58f1139cbdbdf71bcfa81f5eb7dac04fc351cc
Vo Ngo Van Tien <alden.tien@itlvn.com>
2022-08-22 16:24:46 +0700

    updated load SOA for add charge

--- WebAPI/eFMS.API.SystemWeb/eFMS.API.Documentation/eFMS.API.Documentation/Controllers/CsTransactionController.cs
+++ WebAPI/eFMS.API.SystemWeb/eFMS.API.Documentation/eFMS.API.Documentation/Controllers/CsTransactionController.cs
@@ -589,23 +589,6 @@ namespace eFMS.API.Documentation.Controllers
                 return File((MemoryStream)res.Message, "application/zip", m.FileName);
             return BadRequest(res);
         }
-
-        [Authorize]
-        [HttpPut("UpdateJobStatus")]
-        public IActionResult UpdateJobStatus(ChargeShipmentStatusModel model)
-        {
-            HandleState hs = csTransactionService.UpdateJobStatus(model);
-
-            string message = HandleError.GetMessage(hs, Crud.Update);
-
-            ResultHandle result = new ResultHandle { Status = hs.Success, Message = stringLocalizer[message].Value, Data = null };
-            if (!hs.Success)
-            {
-                return BadRequest(result);
-            }
-            return Ok(result);
-        }
-        
         #region -- METHOD PRIVATE --
         private string CheckExist(Guid id, CsTransactionEditModel model)
         {
