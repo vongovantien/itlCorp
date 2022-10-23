d7061a19e4d42242e99ef1a31075e0883dda6fc5
ThuongDoan <doanvanthuong196@gmail.com>
2022-10-23 17:28:53 +0700

    ++ BUTTON confirm

--- WebApp/src/app/shared/common/report-preview/report-preview.component.ts
+++ WebApp/src/app/shared/common/report-preview/report-preview.component.ts
@@ -3,6 +3,8 @@ import { DomSanitizer } from '@angular/platform-browser';
 import { Crystal } from '../../models/report/crystal.model';
 import { PopupBase } from 'src/app/popup.base';
 import { environment } from 'src/environments/environment';
+import { ConfirmPopupComponent } from '../popup';
+import { InjectViewContainerRefDirective } from '../../directives/inject-view-container-ref.directive';
 
 @Component({
     selector: 'app-report-preview',
@@ -14,8 +16,9 @@ export class ReportPreviewComponent extends PopupBase {
     @Input() modalId: any;
     @Input() data: Crystal = null;
     @ViewChild('formReport', { static: true }) frm: ElementRef;
+    @ViewChild(InjectViewContainerRefDirective) viewContainerRef: InjectViewContainerRefDirective;
     @Output() $invisible: EventEmitter<any> = new EventEmitter<any>();
-
+    @Output() onConfirmEdoc: EventEmitter<any> = new EventEmitter<any>();
 
     constructor(private sanitizer: DomSanitizer,
     ) {
@@ -46,5 +49,14 @@ export class ReportPreviewComponent extends PopupBase {
 
     onHide() {
         this.$invisible.emit();
+    }
+
+    onConfirmSaveEdoc() {
+        this.showPopupDynamicRender(ConfirmPopupComponent, this.viewContainerRef.viewContainerRef, {
+            title: 'Confirm save eDOC',
+            body: 'Are you sure to save this file',
+        }, () => {
+            this.onConfirmEdoc.emit();
+        });
     }
 }
