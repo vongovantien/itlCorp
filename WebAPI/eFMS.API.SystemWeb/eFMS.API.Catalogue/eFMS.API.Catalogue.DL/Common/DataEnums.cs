b1fb55d1e6da53e86aa78a068fc1bc14eca42796
An <intern.lth012@itlvn.com>
2022-10-25 16:52:50 +0700

    ++Update send prepaid mail to accountant

--- WebAPI/eFMS.API.SystemWeb/eFMS.API.Catalogue/eFMS.API.Catalogue.DL/Common/DataEnums.cs
+++ WebAPI/eFMS.API.SystemWeb/eFMS.API.Catalogue/eFMS.API.Catalogue.DL/Common/DataEnums.cs
@@ -64,6 +64,7 @@ namespace eFMS.API.Catalogue.DL.Common
         public static readonly string CONTRACT_TRIAL = "Trial";
         public static readonly string CONTRACT_OFFICIAL = "Official";
         public static readonly string CONTRACT_CASH = "Cash";
+        public static readonly string CONTRACT_PREPAID = "Prepaid";
         public static readonly string CONTRACT_GUARANTEE = "Guarantee";
         public static readonly string PARTNER_TYPE_CUSTOMER = "Customer";
         public static readonly string PARTNER_TYPE_AGENT = "Agent";
