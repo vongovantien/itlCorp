f2ae8c5ba3223262347343044529ee6a031500b1
dane.dung <dane.dung@itlvn.com>
2022-11-01 09:22:22 +0700

    reformat aslias

--- WebApp/src/app/business-modules/share-business/components/document-type-attach/document-type-attach.component.ts
+++ WebApp/src/app/business-modules/share-business/components/document-type-attach/document-type-attach.component.ts
@@ -76,7 +76,7 @@ export class ShareDocumentTypeAttachComponent extends PopupBase implements OnIni
                 console.log(event);
                 this.listFile[index].Code = event.code;
                 this.listFile[index].DocumentId = event.id;
-                this.listFile[index].aliasName = this.isUpdate ? event.code + this.listFile[index].name : event.code + this.listFile[index].name.substring(0, this.listFile[index].name.lastIndexOf('.'))
+                this.listFile[index].aliasName = this.isUpdate ? event.code + '_' + this.listFile[index].name : event.code + '_' + this.listFile[index].name.substring(0, this.listFile[index].name.lastIndexOf('.'))
                 this.selectedtDocType = event.id;
                 break;
             case 'aliasName':
