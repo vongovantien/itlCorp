6363ab974a8271bc1a6b4e2ade7f26daead096f9
dane.dung <dane.dung@itlvn.com>
2022-11-02 16:30:28 +0700

    update ưith jobId

--- WebAPI/eFMS.API.SystemWeb/eFMS.API.SystemFileManagement/eFMS.API.SystemFileManagement.DL/Services/EDocService.cs
+++ WebAPI/eFMS.API.SystemWeb/eFMS.API.SystemFileManagement/eFMS.API.SystemFileManagement.DL/Services/EDocService.cs
@@ -632,7 +632,7 @@ namespace eFMS.API.SystemFileManagement.DL.Services
                     edoc.Hblid = edocUpdate.Hblid;
                     edoc.Note = edocUpdate.Note;
                     edoc.DocumentTypeId = edocUpdate.DocumentTypeId;
-                    edoc.JobId = edoc.JobId;    
+                    edoc.JobId = edocUpdate.JobId;    
                     var hs = await _sysImageDetailRepo.UpdateAsync(edoc, x => x.Id == edoc.Id, false);
                 }
                 if (edoc == null)
