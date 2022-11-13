75a349a5bfb76f7c8f55e535abe6e0f699b6e472
ThuongDoan <doanvanthuong196@gmail.com>
2022-11-13 15:03:43 +0700

    update menu resolve

--- WebApp/src/app/business-modules/report/report-routing.module.ts
+++ WebApp/src/app/business-modules/report/report-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MenuResolveGuard } from '@core';
 import { PLReportComponent } from './p-l-report/p-l-report.component';
 import { PerformanceReportComponent } from './performance-report/performance-report.component';
 import { ShipmentOverviewComponent } from './shipment-overview/shipment-overview.component';
@@ -23,19 +24,31 @@ const routes: Routes = [
   },
   {
     path: 'general-report', loadChildren: () => import('./general-report/general-report.module').then(m => m.GeneralReportModule),
-    data: { name: 'General Report', title: "General Report" }
+    data: { name: 'General Report', title: "General Report" },
+    resolve: {
+      checkMenu: MenuResolveGuard
+    },
   },
   {
     path: 'sale-report', loadChildren: () => import('./sale-report/sale-report.module').then(m => m.SaleReportModule),
-    data: { name: 'Sale Report', title: "Sale Report" }
+    data: { name: 'Sale Report', title: "Sale Report" },
+    resolve: {
+      checkMenu: MenuResolveGuard
+    },
   },
   {
     path: 'sheet-debit-report', loadChildren: () => import('./sheet-debit-report/sheet-debit-report.module').then(m => m.SheetDebitReportModule),
-    data: { name: 'Accountant Report', title: "Accountant Report" }
+    data: { name: 'Accountant Report', title: "Accountant Report" },
+    resolve: {
+      checkMenu: MenuResolveGuard
+    },
   },
   {
     path: 'commission-incentive-report', loadChildren: () => import('./commission-incentive-report/commission-incentive-report.module').then(m => m.CommissionIncentiveReportModule),
-    data: { name: 'Commission/ Incentive', title: "Commission/Incentive Report" }
+    data: { name: 'Commission/ Incentive', title: "Commission/Incentive Report" },
+    resolve: {
+      checkMenu: MenuResolveGuard
+    },
   },
 ];
 
--- WebApp/src/app/business-modules/system/system-routing.module.ts
+++ WebApp/src/app/business-modules/system/system-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MenuResolveGuard } from '@core';
 import { RoleComponent } from './role/role.component';
 
 const routes: Routes = [
@@ -11,18 +12,34 @@ const routes: Routes = [
     {
         path: 'company', loadChildren: () => import('./company/company-information.module').then(m => m.CompanyInformationModule),
         data: { name: 'Company', title: 'eFMS Company' }
+        ,
+        resolve: {
+            checkMenu: MenuResolveGuard
+        },
     },
     {
         path: 'department', loadChildren: () => import('./department/department.module').then(m => m.DepartmentModule),
         data: { name: 'Department', title: 'eFMS Department' }
+        ,
+        resolve: {
+            checkMenu: MenuResolveGuard
+        },
     },
     {
         path: 'group', loadChildren: () => import('./group/group.module').then(m => m.GroupModule),
         data: { name: 'Group', title: 'eFMS Group' }
+        ,
+        resolve: {
+            checkMenu: MenuResolveGuard
+        },
     },
     {
         path: 'permission', loadChildren: () => import('./permission/permission.module').then(m => m.PermissionModule),
         data: { name: 'Permission', title: 'eFMS Permission' }
+        ,
+        resolve: {
+            checkMenu: MenuResolveGuard
+        },
     },
     {
         path: 'role',
@@ -31,14 +48,26 @@ const routes: Routes = [
     {
         path: 'user-management', loadChildren: () => import('./user-management/user-management.module').then(m => m.UserManagementModule),
         data: { name: 'User Management', title: 'eFMS User' }
+        ,
+        resolve: {
+            checkMenu: MenuResolveGuard
+        },
     },
     {
         path: 'office', loadChildren: () => import('./office/office.module').then(m => m.OfficeModule),
         data: { name: 'Office', title: 'eFMS Office' }
+        ,
+        resolve: {
+            checkMenu: MenuResolveGuard
+        },
     },
     {
         path: 'authorization', loadChildren: () => import('./authorization/authorization.module').then(m => m.AuthorizationModule),
         data: { name: 'Authorization', title: 'eFMS Authorization' }
+        ,
+        resolve: {
+            checkMenu: MenuResolveGuard
+        },
     },
 ];
 
