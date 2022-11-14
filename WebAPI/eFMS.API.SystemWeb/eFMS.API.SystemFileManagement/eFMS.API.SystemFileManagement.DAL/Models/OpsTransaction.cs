8a261ad8a8ab8482e74f5ebe83dd99003ea62633
ITL\kenny.thuong <kenny.thuong@itlvn.com>
2022-11-14 09:15:41 +0700

    += update context ops

--- WebAPI/eFMS.API.SystemWeb/eFMS.API.SystemFileManagement/eFMS.API.SystemFileManagement.DAL/Models/OpsTransaction.cs
+++ WebAPI/eFMS.API.SystemWeb/eFMS.API.SystemFileManagement/eFMS.API.SystemFileManagement.DAL/Models/OpsTransaction.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace eFMS.API.SystemFileManagement.Service.Models
 {
@@ -68,9 +69,5 @@ namespace eFMS.API.SystemFileManagement.Service.Models
         public DateTime? DateCreatedLinkJob { get; set; }
         public bool? IsReplicate { get; set; }
         public bool? NoProfit { get; set; }
-        public DateTime? Eta { get; set; }
-        public string SuspendTime { get; set; }
-        public DateTime? DeliveryDate { get; set; }
-        public DateTime? ClearanceDate { get; set; }
     }
 }
