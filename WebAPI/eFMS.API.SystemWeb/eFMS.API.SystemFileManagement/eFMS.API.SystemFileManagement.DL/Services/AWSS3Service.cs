66e2c90e7373a451a20b5507a8f3522a24407cec
dane.dung <dane.dung@itlvn.com>
2022-11-23 16:40:04 +0700

    using static

--- WebAPI/eFMS.API.SystemWeb/eFMS.API.SystemFileManagement/eFMS.API.SystemFileManagement.DL/Services/AWSS3Service.cs
+++ WebAPI/eFMS.API.SystemWeb/eFMS.API.SystemFileManagement/eFMS.API.SystemFileManagement.DL/Services/AWSS3Service.cs
@@ -16,6 +16,7 @@ using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using static eFMS.API.Common.Helpers.FileHelper;
 
 namespace eFMS.API.SystemFileManagement.DL.Services
 {
