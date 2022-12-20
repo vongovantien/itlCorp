b89d9c11db2ae01d74dd745d1236b576c19c7b76
An <intern.lth012@itlvn.com>
2022-12-20 18:10:40 +0700

    change notification

--- WebApp/src/app/business-modules/catalogue/partner-data/detail/detail-partner.component.ts
+++ WebApp/src/app/business-modules/catalogue/partner-data/detail/detail-partner.component.ts
@@ -496,7 +496,7 @@ export class PartnerDetailComponent extends AppList {
                         this.formPartnerComponent.isExistedTaxcode = true;
 
                         if (!!res.internalReferenceNo) {
-                            this.deleteMessage = `This Partner is existed, please you check again!`;
+                            this.deleteMessage = `This <b>Taxcode</b> already <b>Existed</b> in <b>${res.shortName}</b>, If you want to Create Internal account, Please change <b>Internal Reference Info</b>.`;;
                             this.confirmDuplicatePartner.show();
                         } else {
                             this.deleteMessage = `This <b>Taxcode</b> already <b>Existed</b> in  <b>${res.shortName}</b>, If you want to Create Internal account, Please fill info to <b>Internal Reference Info</b>.`;
