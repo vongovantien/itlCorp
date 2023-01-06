fd6d58b3951eb18936392461ed98d44a774e56e2
dane.dung <dane.dung@itlvn.com>
2023-01-06 10:53:42 +0700

    update get prefix name

--- WebAPI/eFMS.API.SystemWeb/eFMS.API.SystemFileManagement/eFMS.API.SystemFileManagement.DL/Services/EDocService.cs
+++ WebAPI/eFMS.API.SystemWeb/eFMS.API.SystemFileManagement/eFMS.API.SystemFileManagement.DL/Services/EDocService.cs
@@ -979,8 +979,14 @@ namespace eFMS.API.SystemFileManagement.DL.Services
                 prefixs = _attachFileTemplateRepo.Get(x => x.TransactionType == transactionType).Select(x => x.Code).OrderBy(x => x.Length).ToList(); prefixs = _attachFileTemplateRepo.Get(x => x.TransactionType == transactionType).Select(x => x.Code).OrderBy(x => x.Length).ToList();
             }
             string code = null;
+            bool clearLastChar = false;
             if (fileName.Split('_').Count() > 0)
             {
+                if(fileName.ToList().Last() == '_')
+                {
+                    fileName=fileName.Remove(fileName.Length - 1);
+                    clearLastChar = true;
+                }
                 var fileNameSplit = fileName.Split('_').ToList().Last();
                 var preFixFileName = fileName.Replace(fileNameSplit, "");
                 for (int i = 0; i < prefixs.Count; i++)
@@ -994,6 +1000,10 @@ namespace eFMS.API.SystemFileManagement.DL.Services
             else
             {
                 return '_' + fileName;
+            }
+            if (clearLastChar)
+            {
+                fileName = fileName + '_';
             }
             //prefixs.ForEach(x =>
             //{
