a2e3ba750e5c8f3dc8d711e77dc3c886cee23df7
ITL\kenny.thuong <kenny.thuong@itlvn.com>
2023-01-31 11:37:44 +0700

    ++ update

--- WebAPI/eFMS.API.SystemWeb/eFMS.API.Accounting/eFMS.API.Accounting.DL/Services/AcctReceiptService.cs
+++ WebAPI/eFMS.API.SystemWeb/eFMS.API.Accounting/eFMS.API.Accounting.DL/Services/AcctReceiptService.cs
@@ -3625,7 +3625,7 @@ namespace eFMS.API.Accounting.DL.Services
                 surcharges = surchargeRepository.Get(query);
             }
             var hs = new HandleState();
-            if (surcharges == null) return hs;
+            if (surcharges == null) return new List<ObjectReceivableModel>();
 
             var objectReceivablesModel = accAccountReceivableService.GetObjectReceivableBySurcharges(surcharges);
 
