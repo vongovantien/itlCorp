8f9d11b922f977094e7491084797ec5baa4f4206
ITL\kenny.thuong <kenny.thuong@itlvn.com>
2023-02-01 10:36:49 +0700

    Receive rabbit with exchange, routing key

--- WebAPI/eFMS.API.Common/eFMS.API.Infrastructure/RabbitMQ/IRabbitBus.cs
+++ WebAPI/eFMS.API.Common/eFMS.API.Infrastructure/RabbitMQ/IRabbitBus.cs
@@ -8,5 +8,6 @@ namespace eFMS.API.Infrastructure.RabbitMQ
         Task SendAsync<T>(string queue, T message);
         Task SendAsync<T>(string exchange, string queue, T message);
         Task ReceiveAsync<T>(string queue, Action<T> onMessage);
+        Task ReceiveAsync<T>(string exchange, string queue, Action<T> onMessage);
     }
 }
