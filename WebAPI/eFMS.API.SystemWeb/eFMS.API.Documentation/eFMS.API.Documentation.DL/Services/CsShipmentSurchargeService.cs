e9c567848bb7830bc6531c3a18aaf56589dbaf39
An <intern.lth012@itlvn.com>
2023-03-08 16:11:55 +0700

    Update error msg

--- WebAPI/eFMS.API.SystemWeb/eFMS.API.Documentation/eFMS.API.Documentation.DL/Services/CsShipmentSurchargeService.cs
+++ WebAPI/eFMS.API.SystemWeb/eFMS.API.Documentation/eFMS.API.Documentation.DL/Services/CsShipmentSurchargeService.cs
@@ -1663,7 +1663,7 @@ namespace eFMS.API.Documentation.DL.Services
                 }
                 if (string.IsNullOrEmpty(item.VatPartnerId))
                 {
-                    item.VatPartnerTaxCodeError = string.Format(stringLocalizer[DocumentationLanguageSub.MSG_VAT_PARTER_CODE_NOT_EXIST]);
+                    item.VatPartnerTaxCodeError = string.Format(stringLocalizer[DocumentationLanguageSub.MSG_VAT_PARTER_CODE_EMPTY]);
                     item.IsValid = false;
                 }
                 else
