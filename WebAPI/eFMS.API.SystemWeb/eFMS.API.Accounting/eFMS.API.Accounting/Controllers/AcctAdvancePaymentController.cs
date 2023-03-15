fa17773b1d484f68b663107c004e4551caeb7b37
Dung Hoang <dane.dung@itlvn.com>
2023-03-15 16:49:31 +0700

    update data out async

--- WebAPI/eFMS.API.SystemWeb/eFMS.API.Accounting/eFMS.API.Accounting/Controllers/AcctAdvancePaymentController.cs
+++ WebAPI/eFMS.API.SystemWeb/eFMS.API.Accounting/eFMS.API.Accounting/Controllers/AcctAdvancePaymentController.cs
@@ -462,14 +462,10 @@ namespace eFMS.API.Accounting.Controllers
                 return BadRequest(result);
             }
             else
-            {
-                Response.OnCompleted(async () =>
-                {
+            
                     Uri urlEdoc = new Uri(apiServiceUrl.Value.Url);
                     var edocModel = _edocSevice.MapAdvanceRequest(model.AdvanceNo);
                     var updateEdoc = HttpClientService.PutAPI(urlEdoc + "File/api/v1/vi/EDoc/UpdateEdocByAcc", edocModel, null);
-                });
-
             }
             return Ok(result);
         }
