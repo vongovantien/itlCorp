8954f9287cec8fcb786c2ed5006a45430d4fd650
Thuan <intern.lth012@itlvn.com>
2023-03-24 11:41:47 +0700

    Update remove unuse code, var

--- WebApp/src/app/business-modules/operation/custom-clearance/getecus/get-custom-clearance-from-Ecus.component.ts
+++ WebApp/src/app/business-modules/operation/custom-clearance/getecus/get-custom-clearance-from-Ecus.component.ts
@@ -1,16 +1,13 @@
-import { Component, EventEmitter, Input, OnInit, Output, ViewChild } from "@angular/core";
+import { Component, EventEmitter, OnInit, Output, ViewChild } from "@angular/core";
 import { PopupBase } from 'src/app/popup.base';
 import { SortService } from 'src/app/shared/services';
-import { OpsTransaction } from 'src/app/shared/models/document/OpsTransaction.model';
-import { FormGroup, AbstractControl, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { template } from "lodash";
 import { DocumentationRepo, OperationRepo } from "@repositories";
 import { PagerSetting } from "src/app/shared/models/layout/pager-setting.model";
 import { PAGINGSETTING } from "src/constants/paging.const";
 import { CustomClearanceFormSearchComponent } from "../components/form-search-custom-clearance/form-search-custom-clearance.component";
-import { ModalDirective } from "ngx-bootstrap/modal";
-import { debounceTime, distinctUntilChanged, finalize, takeUntil} from "rxjs/operators";
-import { BehaviorSubject, Observable } from "rxjs";
+import { finalize} from "rxjs/operators";
 import { ToastrService } from "ngx-toastr";
 
 @Component({
@@ -18,27 +15,16 @@ import { ToastrService } from "ngx-toastr";
     templateUrl: './get-custom-clearance-from-Ecus.html'
 })
 export class CustomClearanceFromEcus extends PopupBase implements OnInit {
-    @ViewChild('staticModal') public staticModal: ModalDirective;
     @ViewChild(CustomClearanceFormSearchComponent) CustomClearanceComponent: CustomClearanceFormSearchComponent;
-    // @ViewChild(InjectViewContainerRefDirective) viewContainerRef: InjectViewContainerRefDirective;
-    @Input() currentJob: OpsTransaction;
-    @Input() customer: CustomClearanceFormSearchComponent;
     @Output() isCloseModal = new EventEmitter();
-    @Output() onSearch: EventEmitter<any> = new EventEmitter<any>();
 
     form: FormGroup;
     headers: CommonInterface.IHeaderTable[];
-    customNo: AbstractControl;
     requestList: any = null;
     strKeySearch: string = "";
     pager: PagerSetting = PAGINGSETTING;
-    isShow: boolean = false;
-    sort: string = null;
-    term$ = new BehaviorSubject<string>('');
     dataEcus: any;
-    ;
     checkAllNotImported = false;
-    partnerTaxcode = '';
     constructor(
         private _fb: FormBuilder,
         private _sortService: SortService,
@@ -60,33 +46,12 @@ export class CustomClearanceFromEcus extends PopupBase implements OnInit {
             { title: 'Package Qty', field: 'pkgQty', sortable: true },
             { title: 'Cont Qty', field: 'contQty', sortable: true },
         ];
-
         this.initForm();
-        this.term$.pipe(
-            debounceTime(2000),
-            distinctUntilChanged(),
-            takeUntil(this.ngUnsubscribe)
-        ).subscribe((text: string) => {
-            this.getClearanceNotImported();
-        });
     }
 
 
     initForm() {
         this.pageSize=30;
-        this.form = this._fb.group({
-            customNo: ['', Validators.compose([
-                Validators.required
-            ])]
-        });
-        this.customNo = this.form.controls['customNo'];
-    }
-    onSearchAutoComplete(keyword: string) {
-        console.log(this.customNo);
-        this.term$.next(keyword.trim());
-        if (this.customNo.value === '') {
-            this.getListCleranceNotImported();
-        }
     }
 
     changeAllNotImported() {
@@ -103,11 +68,7 @@ export class CustomClearanceFromEcus extends PopupBase implements OnInit {
 
     getListCleranceNotImported() {
         this.dataEcus = [];
-        if (this.customNo.value !== '') {
-            this.strKeySearch = this.customNo.value;
-        } else {
-            this.getClearanceNotImported();
-        }
+        this.getClearanceNotImported();
     }
 
     getClearanceNotImported() {
@@ -140,17 +101,15 @@ export class CustomClearanceFromEcus extends PopupBase implements OnInit {
         this.keyword = '';
         this.page = 1;
         this.pageSize = this.numberToShow[2];
-        this.customNo.setValue('');
         this.strKeySearch = '';
         this._operationRepo.getUserCustomClearance(this.page, this.pageSize);
     }
 
     updateJobToClearance() {
         const dataToUpdate = this.dataEcus.filter(x => x.isChecked === true);
-        console.log(dataToUpdate);
         if (dataToUpdate.length > 0) {
             this._operationRepo.importCustomClearance(dataToUpdate)
-                .pipe(finalize(() => this.hide()))
+                .pipe()
                 .subscribe(
                     (responses: CommonInterface.IResult | any) => {
                         if (!!responses.message) {
@@ -165,9 +124,7 @@ export class CustomClearanceFromEcus extends PopupBase implements OnInit {
         this.keyword = '';
         this.page = 1;
         this.pageSize = this.numberToShow[2];
-        this.customNo.setValue('');
         this.strKeySearch = '';
-        this._operationRepo.getUserCustomClearance(this.page, this.pageSize);
         this.hide();
     }
 }
