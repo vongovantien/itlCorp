58bf4da5870ad688c285c604ae305a58193ce6fa
Vo Ngo Van Tien <alden.tien@itlvn.com>
2023-04-28 11:02:48 +0700

    update message return

--- WebAPI/eFMS.API.SystemWeb/eFMS.API.Catalogue/eFMS.API.Catalogue/Controllers/CatPartnerBankController.cs
+++ WebAPI/eFMS.API.SystemWeb/eFMS.API.Catalogue/eFMS.API.Catalogue/Controllers/CatPartnerBankController.cs
@@ -211,80 +211,75 @@ namespace eFMS.API.Catalogue.Controllers
 
                 if (loginResponse?.Success != "1")
                 {
-                    // 2. Get Data To Sync.
-                    List<Guid> ids = request.Select(x => x.Id).ToList();
-
-                    List<Guid> idsAdd = request.Where(x => x.Action == ACTION.ADD).Select(x => x.Id).ToList();
-                    List<Guid> idsUpdate = request.Where(x => x.Action == ACTION.UPDATE).Select(x => x.Id).ToList();
-
-                    List<BankSyncModel> listAdd = (idsAdd.Count > 0) ? await catPartnerBankService.GetListPartnerBankInfoToSync(idsAdd) : new List<BankSyncModel>();
-                    List<BankSyncModel> listUpdate = (idsUpdate.Count > 0) ? await catPartnerBankService.GetListPartnerBankInfoToSync(idsUpdate) : new List<BankSyncModel>();
-
-                    HttpResponseMessage resAdd = new HttpResponseMessage();
-                    HttpResponseMessage resUpdate = new HttpResponseMessage();
-                    BravoResponseModel responseAddModel = new BravoResponseModel();
-                    BravoResponseModel responseUpdateModel = new BravoResponseModel();
-
-                    // 3. Call Bravo to SYNC.
-                    if (listAdd.Count > 0)
-                    {
-                        resAdd = await HttpClientService.PostAPI(_webUrl.Value.Url + "/itl-bravo/Accounting/api?func=EFMSBankInfoSyncAdd", listAdd, loginResponse.TokenKey);
-                        responseAddModel = await resAdd.Content.ReadAsAsync<BravoResponseModel>();
-
-                        #region -- Ghi Log --
-                        var modelLog = new SysActionFuncLogModel
-                        {
-                            FuncLocal = "SyncBankAccountToAccountantSystem",
-                            FuncPartner = "EFMSBankInfoSyncAdd",
-                            ObjectRequest = JsonConvert.SerializeObject(listAdd),
-                            ObjectResponse = JsonConvert.SerializeObject(responseAddModel),
-                            Major = "Tạo mới thông tin ngân hàng",
-                            StartDateProgress = _startDateProgress,
-                            EndDateProgress = DateTime.Now
-                        };
-                        var hsAddLog = actionFuncLogService.AddActionFuncLog(modelLog);
-                        #endregion
-                    }
-
-                    if (listUpdate.Count > 0)
-                    {
-                        resUpdate = await HttpClientService.PostAPI(_webUrl.Value.Url + "/itl-bravo/Accounting/api?func=EFMSBankInfoSyncUpdate", listUpdate, loginResponse.TokenKey);
-                        responseUpdateModel = await resUpdate.Content.ReadAsAsync<BravoResponseModel>();
-
-                        #region -- Ghi Log --
-                        var modelLog = new SysActionFuncLogModel
-                        {
-                            FuncLocal = "SyncBankAccountToAccountantSystem",
-                            FuncPartner = "EFMSBankInfoSyncUpdate",
-                            ObjectRequest = JsonConvert.SerializeObject(listUpdate),
-                            ObjectResponse = JsonConvert.SerializeObject(responseUpdateModel),
-                            Major = "Cập nhật thông tin ngân hàng",
-                            StartDateProgress = _startDateProgress,
-                            EndDateProgress = DateTime.Now
-                        };
-                        var hsAddLog = actionFuncLogService.AddActionFuncLog(modelLog);
-                        #endregion
-                    }
-
-                    // 4. Update STATUS
-                    if (responseAddModel.Success == "1"
-                        || responseUpdateModel.Success == "1")
-                    {
-                        var hs = await catPartnerBankService.UpdateByStatus(ids, "Processing");
-                        ResultHandle result = new ResultHandle { Status = true, Message = stringLocalizer["MSG_SYNC_SUCCESS"].Value, Data = null };
-                        return Ok(result);
-                    }
-                    else
-                    {
-                        if (responseAddModel.Success == null || responseUpdateModel.Success == null)
-                        {
-                            return BadRequest(new ResultHandle { Status = true, Message = stringLocalizer["MSG_SYNC_FAIL"].Value, Data = null });
-                        }
-                        ResultHandle result = new ResultHandle { Status = false, Message = responseAddModel.Msg + "\n" + responseUpdateModel.Msg, Data = ids };
-                        return BadRequest(result);
-                    }
+                    return BadRequest(new ResultHandle { Status = true, Message = stringLocalizer["MSG_SYNC_FAIL"].Value, Data = null });
                 }
-                return BadRequest(new ResultHandle { Status = true, Message = stringLocalizer["MSG_SYNC_FAIL"].Value, Data = null });
+                // 2. Get Data To Sync.
+                List<Guid> ids = request.Select(x => x.Id).ToList();
+
+                List<Guid> idsAdd = request.Where(x => x.Action == ACTION.ADD).Select(x => x.Id).ToList();
+                List<Guid> idsUpdate = request.Where(x => x.Action == ACTION.UPDATE).Select(x => x.Id).ToList();
+
+                List<BankSyncModel> listAdd = (idsAdd.Count > 0) ? await catPartnerBankService.GetListPartnerBankInfoToSync(idsAdd) : new List<BankSyncModel>();
+                List<BankSyncModel> listUpdate = (idsUpdate.Count > 0) ? await catPartnerBankService.GetListPartnerBankInfoToSync(idsUpdate) : new List<BankSyncModel>();
+
+                HttpResponseMessage resAdd = new HttpResponseMessage();
+                HttpResponseMessage resUpdate = new HttpResponseMessage();
+
+                BravoResponseModel responseAddModel = new BravoResponseModel();
+                BravoResponseModel responseUpdateModel = new BravoResponseModel();
+
+                // 3. Call Bravo to SYNC.
+                if (listAdd.Count > 0)
+                {
+                    resAdd = await HttpClientService.PostAPI(_webUrl.Value.Url + "/itl-bravo/Accounting/api?func=EFMSBankInfoSyncAdd", listAdd, loginResponse.TokenKey);
+                    responseAddModel = await resAdd.Content.ReadAsAsync<BravoResponseModel>();
+
+                    #region -- Ghi Log --
+                    var modelLog = new SysActionFuncLogModel
+                    {
+                        FuncLocal = "SyncBankAccountToAccountantSystem",
+                        FuncPartner = "EFMSBankInfoSyncAdd",
+                        ObjectRequest = JsonConvert.SerializeObject(listAdd),
+                        ObjectResponse = JsonConvert.SerializeObject(responseAddModel),
+                        Major = "Tạo mới thông tin ngân hàng",
+                        StartDateProgress = _startDateProgress,
+                        EndDateProgress = DateTime.Now
+                    };
+                    var hsAddLog = actionFuncLogService.AddActionFuncLog(modelLog);
+                    #endregion
+                }
+
+                if (listUpdate.Count > 0)
+                {
+                    resUpdate = await HttpClientService.PostAPI(_webUrl.Value.Url + "/itl-bravo/Accounting/api?func=EFMSBankInfoSyncUpdate", listUpdate, loginResponse.TokenKey);
+                    responseUpdateModel = await resUpdate.Content.ReadAsAsync<BravoResponseModel>();
+
+                    #region -- Ghi Log --
+                    var modelLog = new SysActionFuncLogModel
+                    {
+                        FuncLocal = "SyncBankAccountToAccountantSystem",
+                        FuncPartner = "EFMSBankInfoSyncUpdate",
+                        ObjectRequest = JsonConvert.SerializeObject(listUpdate),
+                        ObjectResponse = JsonConvert.SerializeObject(responseUpdateModel),
+                        Major = "Cập nhật thông tin ngân hàng",
+                        StartDateProgress = _startDateProgress,
+                        EndDateProgress = DateTime.Now
+                    };
+                    var hsAddLog = actionFuncLogService.AddActionFuncLog(modelLog);
+                    #endregion
+                }
+                // 4. Update STATUS
+                if (responseAddModel?.Success == "1" || responseUpdateModel?.Success == "1")
+                {
+                    var hs = await catPartnerBankService.UpdateByStatus(ids, "Processing");
+                    ResultHandle result = new ResultHandle { Status = true, Message = stringLocalizer["MSG_SYNC_SUCCESS"].Value, Data = null };
+                    return Ok(result);
+                }
+                else
+                {
+                    ResultHandle result = new ResultHandle { Status = false, Message = responseAddModel.Msg + "\n" + responseUpdateModel.Msg, Data = ids };
+                    return BadRequest(new ResultHandle { Status = true, Message = result.Message, Data = null });
+                }
             }
             catch (Exception)
             {
